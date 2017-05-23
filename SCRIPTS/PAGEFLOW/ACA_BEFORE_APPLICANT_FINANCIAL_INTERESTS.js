1f386d83846f1a439a3d610c6f15683966949208
Lynda Wacht <lwacht@users.noreply.github.com>
2017-05-23 16:06:29 -0600

    financial interest script

---
+++ SCRIPTS/PAGEFLOW/ACA_BEFORE_APPLICANT_FINANCIAL_INTERESTS.js
@@ -0,0 +1,122 @@
+/*------------------------------------------------------------------------------------------------------/
+| Program : ACA_Before_Applicant.js
+| Event   : ACA_BeforeButton Event
+|
+| Usage   : 
+|
+| Client  : N/A
+| Action# : N/A
+|
+| Notes   :
+/------------------------------------------------------------------------------------------------------*/
+var showMessage = false; // Set to true to see results in popup window
+var showDebug = false; // Set to true to see debug messages in popup window
+var useAppSpecificGroupName = false; // Use Group name when populating App Specific Info Values
+var useTaskSpecificGroupName = false; // Use Group name when populating Task Specific Info Values
+var cancel = false;
+var SCRIPT_VERSION = 3;
+/*------------------------------------------------------------------------------------------------------/
+| END User Configurable Parameters
+/------------------------------------------------------------------------------------------------------*/
+var startDate = new Date();
+var startTime = startDate.getTime();
+var debug = ""; // Debug String
+var br = "<BR>"; // Break Tag
+var useSA = false;
+var SA = null;
+var SAScript = null;
+var bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS", "SUPER_AGENCY_FOR_EMSE");
+if (bzr.getSuccess() && bzr.getOutput().getAuditStatus() != "I") {
+	useSA = true;
+	SA = bzr.getOutput().getDescription();
+	bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS", "SUPER_AGENCY_INCLUDE_SCRIPT");
+	if (bzr.getSuccess()) {
+		SAScript = bzr.getOutput().getDescription();
+	}
+}
+
+if (SA) {
+	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", SA,true));
+	eval(getScriptText("INCLUDES_ACCELA_GLOBALS", SA, true));
+	eval(getScriptText(SAScript, SA));
+} else {
+	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS","CALTREES",true));
+	eval(getScriptText("INCLUDES_ACCELA_GLOBALS", "CALTREES",true));
+}
+
+eval(getScriptText("INCLUDES_CUSTOM"));
+
+function getScriptText(vScriptName, servProvCode, useProductScripts) {
+	if (!servProvCode)  servProvCode = aa.getServiceProviderCode();
+	vScriptName = vScriptName.toUpperCase();
+	var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();
+	try {
+		if (useProductScripts) {
+			var emseScript = emseBiz.getMasterScript(aa.getServiceProviderCode(), vScriptName);
+		} else {
+			var emseScript = emseBiz.getScriptByPK(aa.getServiceProviderCode(), vScriptName, "ADMIN");
+		}
+		return emseScript.getScriptText() + "";
+	} catch (err) {
+		return "";
+	}
+}
+
+var cap = aa.env.getValue("CapModel");
+//var parentId = cap.getParentCapID();
+
+// page flow custom code begin
+
+//doStandardChoiceActions(controlString, true, 0);
+
+try {
+	if(AInfo["Producing Dispensary"] == "CHECKED") {
+		var fnd = "N";
+		loadASITables4ACA();
+		if(typeof(CANNABISFINANCIALINTEREST) == "object") {
+			for(x in CANNABISFINANCIALINTEREST) {
+				if(CANNABISFINANCIALINTEREST[x]["Type of License"] == "Producing Dispensary") 
+					fnd ="Y";
+			}
+		}
+		if (fnd == "N") {
+			showMessage = true;
+			cancel = true;
+			comment("When Producing Dispensary is checked then you must list your Producing Dispensary License Number in the Cannabis Financial Interest table.");
+		}
+	}
+}
+catch (err) {
+    logDebug("A JavaScript Error occurred: Licenses/Cultivation/*/Application/: " + err.message);
+	logDebug(err.stack);
+}
+
+/*------------------------------------------------------------------------------------------------------/
+| <===========END=Main=Loop================>
+/-----------------------------------------------------------------------------------------------------*/
+
+if (debug.indexOf("**ERROR") > 0) {
+    aa.env.setValue("ErrorCode", "1");
+    aa.env.setValue("ErrorMessage", debug);
+}
+else {
+    if (cancel) {
+        aa.env.setValue("ErrorCode", "-2");
+        if (showMessage) aa.env.setValue("ErrorMessage", message);
+        if (showDebug) aa.env.setValue("ErrorMessage", debug);
+    }
+    else {
+        aa.env.setValue("ErrorCode", "0");
+        if (showMessage) aa.env.setValue("ErrorMessage", message);
+        if (showDebug) aa.env.setValue("ErrorMessage", debug);
+    }
+}
+
+/*------------------------------------------------------------------------------------------------------/
+| <===========External Functions (used by Action entries)
+/------------------------------------------------------------------------------------------------------*/
+
+
+
+
+
