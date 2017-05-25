a6a721e15454b6a2c0aff676d24a7ab2eed6a4e6
Lynda Wacht <lwacht@users.noreply.github.com>
2017-05-25 15:06:48 -0600

    and testing.

--- SCRIPTS/PAGEFLOW/ACA_ONLOAD_OWNER_APP_CONTACT.js
+++ SCRIPTS/PAGEFLOW/ACA_ONLOAD_OWNER_APP_CONTACT.js
@@ -17,11 +17,11 @@
 |     will no longer be considered a "Master" script and will not be supported in future releases.  If
 |     changes are made, please add notes above.
 /------------------------------------------------------------------------------------------------------*/
-var showMessage = false; // Set to true to see results in popup window
-var showDebug = false; // Set to true to see debug messages in popup window
+var showMessage = true; // Set to true to see results in popup window
+var showDebug = true; // Set to true to see debug messages in popup window
 var useAppSpecificGroupName = false; // Use Group name when populating App Specific Info Values
 var useTaskSpecificGroupName = false; // Use Group name when populating Task Specific Info Values
-var cancel = false;
+var cancel = true;
 var SCRIPT_VERSION = 3;
 /*------------------------------------------------------------------------------------------------------/
 | END User Configurable Parameters
