442d35d26c6cc4bd6eadc78dec35dcd469bdcef6
Lynda Wacht <lwacht@users.noreply.github.com>
2017-05-25 10:42:03 -0600

    new function for ASB

---
+++ INCLUDES_CUSTOM/createRefContactsFromCapContacts_ASB.js
@@ -0,0 +1,194 @@
+function createRefContactsFromCapContacts_ASB(pCapId, contactTypeArray, ignoreAttributeArray, replaceCapContact, overwriteRefContact, refContactExists)
+	{
+
+	// contactTypeArray is either null (all), or an array or contact types to process
+	//
+	// ignoreAttributeArray is either null (none), or an array of attributes to ignore when creating a REF contact
+	//
+	// replaceCapContact not implemented yet
+	//
+	// overwriteRefContact -- if true, will refresh linked ref contact with CAP contact data
+	//
+	// refContactExists is a function for REF contact comparisons.
+	//
+	// Version 2.0 Update:   This function will now check for the presence of a standard choice "REF_CONTACT_CREATION_RULES".
+	// This setting will determine if the reference contact will be created, as well as the contact type that the reference contact will
+	// be created with.  If this setting is configured, the contactTypeArray parameter will be ignored.   The "Default" in this standard
+	// choice determines the default action of all contact types.   Other types can be configured separately.
+	// Each contact type can be set to "I" (create ref as individual), "O" (create ref as organization),
+	// "F" (follow the indiv/org flag on the cap contact), "D" (Do not create a ref contact), and "U" (create ref using transaction contact type).
+
+	var standardChoiceForBusinessRules = "REF_CONTACT_CREATION_RULES";
+
+
+	var ingoreArray = new Array();
+	if (arguments.length > 1) ignoreArray = arguments[1];
+
+	var defaultContactFlag = lookup(standardChoiceForBusinessRules,"Default");
+
+	//var c = aa.people.getCapContactByCapID(pCapId).getOutput()
+	//var cCopy = aa.people.getCapContactByCapID(pCapId).getOutput()  // must have two working datasets
+	var c = aa.env.getValue("ContactList");
+	var cCopy = aa.env.getValue("ContactList");  // must have two working datasets
+
+	for (var i in c)
+	   {
+	   var ruleForRefContactType = "U"; // default behavior is create the ref contact using transaction contact type
+	   var con = c[i];
+
+	   var p = con.getPeople();
+
+	   var contactFlagForType = lookup(standardChoiceForBusinessRules,p.getContactType());
+
+	   if (!defaultContactFlag && !contactFlagForType) // standard choice not used for rules, check the array passed
+	   	{
+	   	if (contactTypeArray && !exists(p.getContactType(),contactTypeArray))
+			continue;  // not in the contact type list.  Move along.
+		}
+
+	   if (!contactFlagForType && defaultContactFlag) // explicit contact type not used, use the default
+	   	{
+	   	ruleForRefContactType = defaultContactFlag;
+	   	}
+
+	   if (contactFlagForType) // explicit contact type is indicated
+	   	{
+	   	ruleForRefContactType = contactFlagForType;
+	   	}
+
+	   if (ruleForRefContactType.equals("D"))
+	   	continue;
+
+	   var refContactType = "";
+
+	   switch(ruleForRefContactType)
+	   	{
+		   case "U":
+		     refContactType = p.getContactType();
+		     break;
+		   case "I":
+		     refContactType = "Individual";
+		     break;
+		   case "O":
+		     refContactType = "Organization";
+		     break;
+		   case "F":
+		     if (p.getContactTypeFlag() && p.getContactTypeFlag().equals("organization"))
+		     	refContactType = "Organization";
+		     else
+		     	refContactType = "Individual";
+		     break;
+		}
+
+	   var refContactNum = con.getCapContactModel().getRefContactNumber();
+
+	   if (refContactNum)  // This is a reference contact.   Let's refresh or overwrite as requested in parms.
+	   	{
+	   	if (overwriteRefContact)
+	   		{
+	   		p.setContactSeqNumber(refContactNum);  // set the ref seq# to refresh
+	   		p.setContactType(refContactType);
+
+	   						var a = p.getAttributes();
+
+							if (a)
+								{
+								var ai = a.iterator();
+								while (ai.hasNext())
+									{
+									var xx = ai.next();
+									xx.setContactNo(refContactNum);
+									}
+					}
+
+	   		var r = aa.people.editPeopleWithAttribute(p,p.getAttributes());
+
+			if (!r.getSuccess())
+				logDebug("WARNING: couldn't refresh reference people : " + r.getErrorMessage());
+			else
+				logDebug("Successfully refreshed ref contact #" + refContactNum + " with CAP contact data");
+			}
+
+	   	if (replaceCapContact)
+	   		{
+				// To Be Implemented later.   Is there a use case?
+			}
+
+	   	}
+	   	else  // user entered the contact freehand.   Let's create or link to ref contact.
+	   	{
+			var ccmSeq = p.getContactSeqNumber();
+
+			var existingContact = refContactExists(p);  // Call the custom function to see if the REF contact exists
+
+			var p = cCopy[i].getPeople();  // get a fresh version, had to mangle the first for the search
+
+			if (existingContact)  // we found a match with our custom function.  Use this one.
+				{
+					refPeopleId = existingContact;
+				}
+			else  // did not find a match, let's create one
+				{
+
+				var a = p.getAttributes();
+
+				if (a)
+					{
+					//
+					// Clear unwanted attributes
+					var ai = a.iterator();
+					while (ai.hasNext())
+						{
+						var xx = ai.next();
+						if (ignoreAttributeArray && exists(xx.getAttributeName().toUpperCase(),ignoreAttributeArray))
+							ai.remove();
+						}
+					}
+
+				p.setContactType(refContactType);
+				var r = aa.people.createPeopleWithAttribute(p,a);
+
+				if (!r.getSuccess())
+					{logDebug("WARNING: couldn't create reference people : " + r.getErrorMessage()); continue; }
+
+				//
+				// createPeople is nice and updates the sequence number to the ref seq
+				//
+
+				var p = cCopy[i].getPeople();
+				var refPeopleId = p.getContactSeqNumber();
+
+				logDebug("Successfully created reference contact #" + refPeopleId);
+
+				// Need to link to an existing public user.
+
+			    var getUserResult = aa.publicUser.getPublicUserByEmail(con.getEmail())
+			    if (getUserResult.getSuccess() && getUserResult.getOutput()) {
+			        var userModel = getUserResult.getOutput();
+			        logDebug("createRefContactsFromCapContactsAndLink: Found an existing public user: " + userModel.getUserID());
+
+					if (refPeopleId)	{
+						logDebug("createRefContactsFromCapContactsAndLink: Linking this public user with new reference contact : " + refPeopleId);
+						aa.licenseScript.associateContactWithPublicUser(userModel.getUserSeqNum(), refPeopleId);
+						}
+					}
+				}
+
+			//
+			// now that we have the reference Id, we can link back to reference
+			//
+
+		    //var ccm = aa.people.getCapContactByPK(pCapId,ccmSeq).getOutput().getCapContactModel();
+
+		    //ccm.setRefContactNumber(refPeopleId);
+		    //r = aa.people.editCapContact(ccm);
+
+		    //if (!r.getSuccess())
+			//	{ logDebug("WARNING: error updating cap contact model : " + r.getErrorMessage()); }
+			//else
+			//	{ logDebug("Successfully linked ref contact " + refPeopleId + " to cap contact " + ccmSeq);}
+
+
+	    }  // end if user hand entered contact
+	}  // end for each CAP contact
+} 
