3995d76381c369b4b35352036bbbf0d7ee99d434
Lynda Wacht <lwacht@users.noreply.github.com>
2017-06-01 14:38:40 -0600

    better error handling (i hope)

--- SCRIPTS/EVENT/DUA;LICENSES!CULTIVATOR!~!APPLICATION.js
+++ SCRIPTS/EVENT/DUA;LICENSES!CULTIVATOR!~!APPLICATION.js
@@ -60,8 +60,8 @@ try{
 		for(i = 0; i<documentModels.length;i++){
 			documentModel = documentModels[i];
 			conditionNumber = documentModel.getConditionNumber();
-			logDebug(" i = " + i);
-			logDebug("Condition Number = " + conditionNumber);
+			//logDebug(" i = " + i);
+			//logDebug("Condition Number = " + conditionNumber);
 			if(conditionNumber != null && conditionNumber != 0){
 				var capConditionResult =
 				aa.capCondition.getCapCondition(capId, conditionNumber);
@@ -90,5 +90,5 @@ try{
 } catch(err){
 	logDebug("An error has occurred in DUA:LICENSES/CULTIVATOR/*/APPLICATION: Required Documents: " + err.message);
 	logDebug(err.stack);
-	aa.sendMail(sysFromEmail, debugEmail, "", "An error has occurred in DUA:LICENSES/CULTIVATOR/*/APPLICATION: Required Documents: "+ startDate, capId + "; " + err.message+ "; "+ err.stack);
+	aa.sendMail(sysFromEmail, debugEmail, "", "An error has occurred in DUA:LICENSES/CULTIVATOR/*/APPLICATION: Required Documents: "+ startDate, capId + "; " + err.message+ "; "+ err.stack +": DOCUMENT ARRAY: " + documentModels);
 }
