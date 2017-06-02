68e7a0518d6c4cf82917fdf8e730e7f9e4060462
Lynda Wacht <lwacht@users.noreply.github.com>
2017-06-01 18:15:09 -0600

    test

--- SCRIPTS/PAGEFLOW/ACA_BEFORE_APPLICANT_FINANCIAL_INTERESTS.js
+++ SCRIPTS/PAGEFLOW/ACA_BEFORE_APPLICANT_FINANCIAL_INTERESTS.js
@@ -22,16 +22,14 @@ var showDebug = false; // Set to true to see debug messages in popup window
 var useAppSpecificGroupName = false; // Use Group name when populating App Specific Info Values
 var useTaskSpecificGroupName = false; // Use Group name when populating Task Specific Info Values
 var cancel = false;
-var useCustomScriptFile = true;  			// if true, use Events->Custom Script, else use Events->Scripts->INCLUDES_CUSTOM
+var SCRIPT_VERSION = 3;
 /*------------------------------------------------------------------------------------------------------/
 | END User Configurable Parameters
 /------------------------------------------------------------------------------------------------------*/
 var startDate = new Date();
 var startTime = startDate.getTime();
-var message = ""; // Message String
 var debug = ""; // Debug String
 var br = "<BR>"; // Break Tag
-
 var useSA = false;
 var SA = null;
 var SAScript = null;
@@ -46,14 +44,15 @@ if (bzr.getSuccess() && bzr.getOutput().getAuditStatus() != "I") {
 }
 
 if (SA) {
-	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", SA, useCustomScriptFile));
+	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", SA,true));
+	eval(getScriptText("INCLUDES_ACCELA_GLOBALS", SA, true));
 	eval(getScriptText(SAScript, SA));
 } else {
-	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS",null,useCustomScriptFile));
+	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS","CALTREES",true));
+	eval(getScriptText("INCLUDES_ACCELA_GLOBALS", "CALTREES",true));
 }
 
-eval(getScriptText("INCLUDES_CUSTOM",null,useCustomScriptFile));
-
+eval(getScriptText("INCLUDES_CUSTOM"));
 
 function getScriptText(vScriptName, servProvCode, useProductScripts) {
 	if (!servProvCode)  servProvCode = aa.getServiceProviderCode();
@@ -71,72 +70,7 @@ function getScriptText(vScriptName, servProvCode, useProductScripts) {
 	}
 }
 
-
 var cap = aa.env.getValue("CapModel");
-var capId = cap.getCapID();
-var servProvCode = capId.getServiceProviderCode()       		// Service Provider Code
-var publicUser = false ;
-var currentUserID = aa.env.getValue("CurrentUserID");
-var publicUserID = aa.env.getValue("CurrentUserID");
-if (currentUserID.indexOf("PUBLICUSER") == 0) { currentUserID = "ADMIN" ; publicUser = true }  // ignore public users
-var capIDString = capId.getCustomID();					// alternate cap id string
-var systemUserObj = aa.person.getUser(currentUserID).getOutput();  	// Current User Object
-var appTypeResult = cap.getCapType();
-var appTypeString = appTypeResult.toString();				// Convert application type to string ("Building/A/B/C")
-var appTypeArray = appTypeString.split("/");				// Array of application type string
-var currentUserGroup;
-var currentUserGroupObj = aa.userright.getUserRight(appTypeArray[0],currentUserID).getOutput()
-if (currentUserGroupObj) currentUserGroup = currentUserGroupObj.getGroupName();
-var capName = cap.getSpecialText();
-var capStatus = cap.getCapStatus();
-var sysDate = aa.date.getCurrentDate();
-var sysDateMMDDYYYY = dateFormatted(sysDate.getMonth(),sysDate.getDayOfMonth(),sysDate.getYear(),"");
-var parcelArea = 0;
-
-var estValue = 0; var calcValue = 0; var feeFactor			// Init Valuations
-var valobj = aa.finance.getContractorSuppliedValuation(capId,null).getOutput();	// Calculated valuation
-if (valobj.length) {
-	estValue = valobj[0].getEstimatedValue();
-	calcValue = valobj[0].getCalculatedValue();
-	feeFactor = valobj[0].getbValuatn().getFeeFactorFlag();
-	}
-
-var balanceDue = 0 ; var houseCount = 0; feesInvoicedTotal = 0;		// Init detail Data
-var capDetail = "";
-var capDetailObjResult = aa.cap.getCapDetail(capId);			// Detail
-if (capDetailObjResult.getSuccess())
-	{
-	capDetail = capDetailObjResult.getOutput();
-	var houseCount = capDetail.getHouseCount();
-	var feesInvoicedTotal = capDetail.getTotalFee();
-	var balanceDue = capDetail.getBalance();
-	}
-
-var AInfo = new Array();						// Create array for tokenized variables
-loadAppSpecific4ACA(AInfo); 						// Add AppSpecific Info
-//loadTaskSpecific(AInfo);						// Add task specific info
-//loadParcelAttributes(AInfo);						// Add parcel attributes
-loadASITables4ACA();
-
-logDebug("<B>EMSE Script Results for " + capIDString + "</B>");
-logDebug("capId = " + capId.getClass());
-logDebug("cap = " + cap.getClass());
-logDebug("currentUserID = " + currentUserID);
-logDebug("currentUserGroup = " + currentUserGroup);
-logDebug("systemUserObj = " + systemUserObj.getClass());
-logDebug("appTypeString = " + appTypeString);
-logDebug("capName = " + capName);
-logDebug("capStatus = " + capStatus);
-logDebug("sysDate = " + sysDate.getClass());
-logDebug("sysDateMMDDYYYY = " + sysDateMMDDYYYY);
-logDebug("parcelArea = " + parcelArea);
-logDebug("estValue = " + estValue);
-logDebug("calcValue = " + calcValue);
-logDebug("feeFactor = " + feeFactor);
-
-logDebug("houseCount = " + houseCount);
-logDebug("feesInvoicedTotal = " + feesInvoicedTotal);
-logDebug("balanceDue = " + balanceDue);
 
 
 // page flow custom code begin
@@ -144,6 +78,9 @@ logDebug("balanceDue = " + balanceDue);
 //doStandardChoiceActions(controlString, true, 0);
 
 try {
+	var AInfo = [];
+	loadAppSpecific4ACA(AInfo);
+	loadASITables4ACA();
 //	cAppl = getContactObj(capId,"Applicant");
 //	showMessage=true;
 //	logDebug("Contact Array" + cAppl);
@@ -168,85 +105,83 @@ try {
 
 // Check for total acreage from all applicant records.  Total must be less than 4 acres 
 // Check no more than one Medium license allowed unless Producing Disensary is checked.
-if(publicUserID == "PUBLICUSER130840") {
-	cancel = true;
-	var totAcre = 0;
-	var mediumLic = "N";
-	var c = new Array();
-    //c = aa.people.getCapContactByCapID(capId).getOutput();
-	var contactList = capModel.getContactsGroup();
-	if(contactList != null && contactList.size() > 0){
-		for(var i=contactList.size(); i > 0; i--){
-		var contactModel = contactList.get(i-1);
-			var contType = contactModel.getCapContactModel().getContactType();
-	//		showMessage=true;
-			logDebug("AContacts " + contType);
-			if(contType =="Applicant") {
-			var crn = contactModel.getCapContactModel().getRefContactNumber();
-			logDebug("ref nbr " + crn);
-				if (crn != null && crn != "") {
-					var p = contactModel.getPeople();
-					var psm = aa.people.createPeopleModel().getOutput();
-					psm.setContactSeqNumber(crn);
-					psm.setServiceProviderCode(contactModel.getServiceProviderCode());
-					var fn=contactModel.getFirstName();
-					if(fn !=null && fn !="") {
-						var cfn = contactModel.getCapContactModel().getFirstName();
-						var cln = contactModel.getCapContactModel().getLastName();
-						psm.setFullName(cfn + " " + cln);
-					}
-					else {
-						var cbn = contactModel.getCapContactModel().getBusinessName()
-						psm.setBusinessName (cbn);
-					}
-
-					var cResult = aa.people.getCapIDsByRefContact(psm);  // needs 7.1
-					if (cResult.getSuccess()) {
-						logDebug("got recs by contact");
-						var cList = cResult.getOutput();
-						for (var j in cList) {
-							var thisCapId = cList[j];
-							var thatCapId = thisCapId.getCapID();
-							logDebug("capId " + thatCapId);
-							var cs = getAppSpecific("Canopy Size",thatCapId);
-							logDebug("cs " + cs);
-							if(cs != "" && cs != null && cs != undefined) {
-								totAcre = totAcre + parseFloat(cs,2);
-							}
-							capLicType = getAppSpecific("License Type",thatCapId);
-							if (matches(AInfo["License Type"], "Medium Outdoor", "Medium Indoor", "Medium Mixed Light")) {
-								mediumLic = "Y";
+	if(publicUserID == "PUBLICUSER130840") {
+		cancel = true;
+		var totAcre = 0;
+		var mediumLic = "N";
+		var c = new Array();
+		//c = aa.people.getCapContactByCapID(capId).getOutput();
+		var contactList = capModel.getContactsGroup();
+		if(contactList != null && contactList.size() > 0){
+			for(var i=contactList.size(); i > 0; i--){
+				var contactModel = contactList.get(i-1);
+				var contType = contactModel.getCapContactModel().getContactType();
+		//		showMessage=true;
+				logDebug("AContacts " + contType);
+				if(contType =="Applicant") {
+					var crn = contactModel.getCapContactModel().getRefContactNumber();
+					logDebug("ref nbr " + crn);
+					if (crn != null && crn != "") {
+						var p = contactModel.getPeople();
+						var psm = aa.people.createPeopleModel().getOutput();
+						psm.setContactSeqNumber(crn);
+						psm.setServiceProviderCode(contactModel.getServiceProviderCode());
+						var fn=contactModel.getFirstName();
+						if(fn !=null && fn !="") {
+							var cfn = contactModel.getCapContactModel().getFirstName();
+							var cln = contactModel.getCapContactModel().getLastName();
+							psm.setFullName(cfn + " " + cln);
+						}
+						else {
+							var cbn = contactModel.getCapContactModel().getBusinessName()
+							psm.setBusinessName (cbn);
+						}
+						var cResult = aa.people.getCapIDsByRefContact(psm);  // needs 7.1
+						if (cResult.getSuccess()) {
+							logDebug("got recs by contact");
+							var cList = cResult.getOutput();
+							for (var j in cList) {
+								var thisCapId = cList[j];
+								var thatCapId = thisCapId.getCapID();
+								logDebug("capId " + thatCapId);
+								var cs = getAppSpecific("Canopy Size",thatCapId);
+								logDebug("cs " + cs);
+								if(cs != "" && cs != null && cs != undefined) {
+									totAcre = totAcre + parseFloat(cs,2);
+								}
+								capLicType = getAppSpecific("License Type",thatCapId);
+								if (matches(AInfo["License Type"], "Medium Outdoor", "Medium Indoor", "Medium Mixed Light")) {
+									mediumLic = "Y";
+								}
 							}
 						}
-					}
-					else{
-						logDebug("error finding cap ids: " + cResult.getErrorMessage());
+						else{
+							logDebug("error finding cap ids: " + cResult.getErrorMessage());
+						}
 					}
 				}
 			}
+		//	showMessage=true;
+			logDebug("Acres " + totAcre + "Medium " + mediumLic);
+			logDebug("lictype " + AInfo["License Type"]);
+			logDebug("prodDisp" + AInfo["Producing Dispensary"]);
+			if(totAcre > 174240) {
+				cancel=true;
+				showMessage=true;
+				logDebug("You cannot apply for anymore cultivator licenses as you will or have exceeded the 4 acre canopy size limit");
+			}
+			if((AInfo["License Type"] == "Medium Outdoor" || AInfo["License Type"] == "Medium Indoor" || AInfo["License Type"] == "Medium Mixed-Light") && AInfo["Producing Dispensary"] != "CHECKED" && mediumLic == "Y") {
+				cancel=true;
+				showMessage=true;
+				logDebug("You cannot apply for a Medium type license as you already have a Medium type license and you do not have a Producing Dispensary License");
+			}
+		  //  aa.sendMail(sysFromEmail, "mhart@trustvip.com", "", "Info: ACA_Applicant: " + "contacts: " + c + " " + "Type: " + ct);
 		}
-//	showMessage=true;
-	logDebug("Acres " + totAcre + "Medium " + mediumLic);
-	logDebug("lictype " + AInfo["License Type"]);
-	logDebug("prodDisp" + AInfo["Producing Dispensary"]);
-	if(totAcre > 174240) {
-		cancel=true;
-		showMessage=true;
-		logDebug("You cannot apply for anymore cultivator licenses as you will or have exceeded the 4 acre canopy size limit");
 	}
-	if((AInfo["License Type"] == "Medium Outdoor" || AInfo["License Type"] == "Medium Indoor" || AInfo["License Type"] == "Medium Mixed-Light") && AInfo["Producing Dispensary"] != "CHECKED" && mediumLic == "Y") {
-		cancel=true;
-		showMessage=true;
-		logDebug("You cannot apply for a Medium type license as you already have a Medium type license and you do not have a Producing Dispensary License");
-	}
-  //  aa.sendMail(sysFromEmail, "mhart@trustvip.com", "", "Info: ACA_Applicant: " + "contacts: " + c + " " + "Type: " + ct);
- 
- 
-}
-}
-catch (err) {
-    logDebug("A JavaScript Error occurred: Licenses/Cultivation/*/Application/: " + err.message);
+}catch (err) {
+    logDebug("A JavaScript Error occurred: ACA_BEFORE_APPLICANT_FINANCIAL_INTEREST: " + err.message);
 	logDebug(err.stack);
+	aa.sendMail(sysFromEmail, debugEmail, "", "An error has occurred in  ACA_BEFORE_APPLICANT_FINANCIAL_INTEREST: Main Loop: "+ startDate, capId + "; " + err.message+ "; "+ err.stack);
 }
 
 
