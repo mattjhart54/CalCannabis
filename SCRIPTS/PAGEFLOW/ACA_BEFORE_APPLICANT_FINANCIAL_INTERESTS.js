503eff421e46b47b19fe0fc61731da27159f86eb
Lynda Wacht <lwacht@users.noreply.github.com>
2017-06-01 20:28:49 -0600

    fingers crossed

--- SCRIPTS/PAGEFLOW/ACA_BEFORE_APPLICANT_FINANCIAL_INTERESTS.js
+++ SCRIPTS/PAGEFLOW/ACA_BEFORE_APPLICANT_FINANCIAL_INTERESTS.js
@@ -17,8 +17,8 @@
 |     will no longer be considered a "Master" script and will not be supported in future releases.  If
 |     changes are made, please add notes above.
 /------------------------------------------------------------------------------------------------------*/
-var showMessage = true; // Set to true to see results in popup window
-var showDebug = true; // Set to true to see debug messages in popup window
+var showMessage = false; // Set to true to see results in popup window
+var showDebug = false; // Set to true to see debug messages in popup window
 var useAppSpecificGroupName = false; // Use Group name when populating App Specific Info Values
 var useTaskSpecificGroupName = false; // Use Group name when populating Task Specific Info Values
 var cancel = false;
@@ -106,6 +106,7 @@ try {
 // Check for total acreage from all applicant records.  Total must be less than 4 acres 
 // Check no more than one Medium license allowed unless Producing Disensary is checked.
 	if(publicUserID == "PUBLICUSER130840") {
+		aa.sendMail(sysFromEmail, debugEmail, "", "INFO ONLY:   ACA_BEFORE_APPLICANT_FINANCIAL_INTEREST: Main Loop: "+ startDate, capId + "; " );
 		cancel = true;
 		var totAcre = 0;
 		var mediumLic = "N";
@@ -183,6 +184,9 @@ try {
     logDebug("A JavaScript Error occurred: ACA_BEFORE_APPLICANT_FINANCIAL_INTEREST: " + err.message);
 	logDebug(err.stack);
 	aa.sendMail(sysFromEmail, debugEmail, "", "An error has occurred in  ACA_BEFORE_APPLICANT_FINANCIAL_INTEREST: Main Loop: "+ startDate, capId + "; " + err.message+ "; "+ err.stack);
+	aa.env.setValue("ErrorCode", "-2");
+	if (showMessage) aa.env.setValue("ErrorMessage", message);
+	if (showDebug) aa.env.setValue("ErrorMessage", debug);
 }
 
 
