15bd4e0d1d02e0ffaab4420b054d6cebd02a6845
Lynda Wacht <lwacht@users.noreply.github.com>
2017-06-05 16:27:52 -0600

    adding corrected version of this.

---
+++ INCLUDES_CUSTOM/loadASITables4ACA.js
@@ -0,0 +1,65 @@
+function loadASITables4ACA() {
+
+ 	//
+ 	// Loads App Specific tables into their own array of arrays.  Creates global array objects
+	//
+	// Optional parameter, cap ID to load from.  If no CAP Id specified, use the capModel
+	//
+	//corrected issue introduced three years ago.
+
+	var itemCap = capId;
+	if (arguments.length == 1)
+		{
+		itemCap = arguments[0]; // use cap ID specified in args
+		var gm = aa.appSpecificTableScript.getAppSpecificTableGroupModel(itemCap).getOutput();
+		}
+	else
+		{
+		var gm = cap.getAppSpecificTableGroupModel()
+		}
+
+	var ta = gm.getTablesMap();
+
+
+	var tai = ta.values().iterator();
+
+	while (tai.hasNext())
+	  {
+	  var tsm = tai.next();
+
+	  if (tsm.rowIndex.isEmpty()) continue;  // empty table
+
+	  var tempObject = new Array();
+	  var tempArray = new Array();
+	  var tn = tsm.getTableName();
+
+	  tn = String(tn).replace(/[^a-zA-Z0-9]+/g,'');
+
+	  if (!isNaN(tn.substring(0,1))) tn = "TBL" + tn  // prepend with TBL if it starts with a number
+
+  	  var tsmfldi = tsm.getTableField().iterator();
+	  var tsmcoli = tsm.getColumns().iterator();
+	  var numrows = 1;
+
+	  while (tsmfldi.hasNext())  // cycle through fields
+		{
+		if (!tsmcoli.hasNext())  // cycle through columns
+			{
+
+			var tsmcoli = tsm.getColumns().iterator();
+			tempArray.push(tempObject);  // end of record
+			var tempObject = new Array();  // clear the temp obj
+			numrows++;
+			}
+		var tcol = tsmcoli.next();
+		//var tval = tsmfldi.next().getInputValue();
+		var tval = tsmfldi.next();
+		tempObject[tcol.getColumnName()] = tval;
+		}
+	  tempArray.push(tempObject);  // end of record
+	  var copyStr = "" + tn + " = tempArray";
+	  logDebug("ASI Table Array : " + tn + " (" + numrows + " Rows)");
+	  eval(copyStr);  // move to table name
+	  }
+
+	}
