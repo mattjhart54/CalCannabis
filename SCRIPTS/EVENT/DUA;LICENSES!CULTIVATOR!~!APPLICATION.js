51684f2652d39159ad8e27c16a4590ba7b9d1a45
Lynda Wacht <lwacht@users.noreply.github.com>
2017-06-13 14:45:04 -0600

    test

--- SCRIPTS/EVENT/DUA;LICENSES!CULTIVATOR!~!APPLICATION.js
+++ SCRIPTS/EVENT/DUA;LICENSES!CULTIVATOR!~!APPLICATION.js
@@ -1,4 +1,44 @@
 //lwacht
+// condition document script to update the doc group/category
+try{
+	if(documentUploadedFrom == "ACA"){
+		var documentModels = documentModelArray.toArray();
+		var documentModel = null;
+		var conditionNumber = 0;
+		for(i = 0; i<documentModels.length;i++){
+			documentModel = documentModels[i];
+			conditionNumber = documentModel.getConditionNumber();
+			//if(conditionNumber != null && conditionNumber != 0){
+			if(!matches(conditionNumber, null, 0){
+				var capConditionResult = aa.capCondition.getCapCondition(capId, conditionNumber);
+				if(capConditionResult.getSuccess()){
+					var capCondition = capConditionResult.getOutput();
+					var conditionGroup = capCondition.getConditionGroup();
+					var conditionName =
+					capCondition.getConditionDescription();
+					documentModel.setDocCategory(conditionName);
+					//documentModel.setDocDepartment(conditionGroup);
+					documentModel.setDocGroup("CALCANNABIS APPLICANT");
+					logDebug("Condition Name - " + conditionName);
+					logDebug("Condition Group - " + conditionGroup);
+					var updateDocumentResult = aa.document.updateDocument(documentModel);
+					if(updateDocumentResult.getSuccess()){
+						logDebug("Update document model successfully - " + 	documentModel.getDocName());
+					}else{
+						logDebug("Update document model failed - " + documentModel.getDocName());
+					}
+				}else{
+					logDebug("No condition number - " + documentModel.getDocName());
+				}
+			}
+		}
+	}
+} catch(err){
+	logDebug("An error has occurred in DUA:LICENSES/CULTIVATOR/*/APPLICATION: Required Documents: " + err.message);
+	logDebug(err.stack);
+	aa.sendMail(sysFromEmail, debugEmail, "", "An error has occurred in DUA:LICENSES/CULTIVATOR/*/APPLICATION: Required Documents: "+ startDate, capId + "; " + err.message+ br + "; STACK: "+ err.stack + br +": DOCUMENT ARRAY: " + documentModel+ br + message);
+}
+//lwacht
 //compare the documents uploaded to the documents required in the "attachment" event
 // if any documents are required, send an email.
 /* not using for now
@@ -50,43 +90,3 @@ try{
 }
 end code not being used
 */ 
-//lwacht
-// condition document script to update the doc group/category
-try{
-	if(documentUploadedFrom == "ACA"){
-		var documentModels = documentModelArray.toArray();
-		var documentModel = null;
-		var conditionNumber = 0;
-		for(i = 0; i<documentModels.length;i++){
-			documentModel = documentModels[i];
-			conditionNumber = documentModel.getConditionNumber();
-			if(conditionNumber != null && conditionNumber != 0){
-				var capConditionResult =
-				aa.capCondition.getCapCondition(capId, conditionNumber);
-				if(capConditionResult.getSuccess()){
-					var capCondition = capConditionResult.getOutput();
-					var conditionGroup = capCondition.getConditionGroup();
-					var conditionName =
-					capCondition.getConditionDescription();
-					documentModel.setDocCategory(conditionName);
-					//documentModel.setDocDepartment(conditionGroup);
-					documentModel.setDocGroup("CALCANNABIS APPLICANT");
-					logDebug("Condition Name - " + conditionName);
-					logDebug("Condition Group - " + conditionGroup);
-					var updateDocumentResult = aa.document.updateDocument(documentModel);
-					if(updateDocumentResult.getSuccess()){
-						logDebug("Update document model successfully - " + 	documentModel.getDocName());
-					}else{
-						logDebug("Update document model failed - " + documentModel.getDocName());
-					}
-				}else{
-					logDebug("No condition number - " + documentModel.getDocName());
-				}
-			}
-		}
-	}
-} catch(err){
-	logDebug("An error has occurred in DUA:LICENSES/CULTIVATOR/*/APPLICATION: Required Documents: " + err.message);
-	logDebug(err.stack);
-	aa.sendMail(sysFromEmail, debugEmail, "", "An error has occurred in DUA:LICENSES/CULTIVATOR/*/APPLICATION: Required Documents: "+ startDate, capId + "; " + err.message+ br + "; STACK: "+ err.stack + br +": DOCUMENT ARRAY: " + documentModel+ br + message);
-}
