2bf7d75f5df952220dd54d44c147e1a5973e6d6a
Lynda Wacht <lwacht@users.noreply.github.com>
2017-06-19 16:54:10 -0600

    conditions

--- SCRIPTS/PAGEFLOW/ACA_ONLOAD_COND_DOCS.js
+++ SCRIPTS/PAGEFLOW/ACA_ONLOAD_COND_DOCS.js
@@ -152,7 +152,7 @@ try{
 	docsMissing = false;
 	showList = true;
 	addConditions = true;
-	addTableRows = false;
+	addTableRows = true;
 	var tblRow = [];
 	var conditionTable = [];
 	dr = "";
@@ -165,7 +165,8 @@ try{
 	}
 	if (r.length > 0 && showList) {
 		for (x in r) { 
-			if(uploadedDocs[r[x].document] == undefined) {	
+			//going to add the condition, even if the document has been added, in case they want to change it
+			//if(uploadedDocs[r[x].document] == undefined) {	
 				showMessage = true; 
 				if (!docsMissing)  {
 					comment("<div class='docList'><span class='fontbold font14px ACA_Title_Color'>The following documents are required based on the information you have provided: </span><ol>"); 	
@@ -193,7 +194,7 @@ try{
 					tblRow["Status"] = new asiTableValObj("Status","Not Submitted", "Y"); ; 
 					conditionTable.push(tblRow);
 				}	
-			}	
+			//}	
 		}
 		if (dr && ccr.length > 0 && addTableRows) {
 			removeASITable("ATTACHMENTS"); 
