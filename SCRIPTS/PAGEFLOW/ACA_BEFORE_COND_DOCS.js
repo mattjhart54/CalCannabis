99587304bba74fc17750974ef2604d0f39324b9f
Lynda Wacht <lwacht@users.noreply.github.com>
2017-06-20 12:06:26 -0600

    test

--- SCRIPTS/PAGEFLOW/ACA_BEFORE_COND_DOCS.js
+++ SCRIPTS/PAGEFLOW/ACA_BEFORE_COND_DOCS.js
@@ -46,12 +46,10 @@ if (bzr.getSuccess() && bzr.getOutput().getAuditStatus() != "I") {
 }
 
 if (SA) {
-	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", SA, true));
-	eval(getScriptText("INCLUDES_ACCELA_GLOBALS", SA, true));
+	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", SA, useCustomScriptFile));
 	eval(getScriptText(SAScript, SA));
 } else {
-	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS",null,true));
-	eval(getScriptText("INCLUDES_ACCELA_GLOBALS", null,true));
+	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS",null,useCustomScriptFile));
 }
 
 eval(getScriptText("INCLUDES_CUSTOM",null,useCustomScriptFile));
