d2b7af86f03c2e467be038ec0f6e0c2aaac070f2
Lynda Wacht <lwacht@users.noreply.github.com>
2017-06-21 09:02:39 -0600

    only run for non ACA

--- SCRIPTS/EVENT/ASA;LICENSES!CULTIVATOR!~!APPLICATION.js
+++ SCRIPTS/EVENT/ASA;LICENSES!CULTIVATOR!~!APPLICATION.js
@@ -44,17 +44,19 @@ try{
 //lwacht
 //remove conditions after documents are uploaded
 try{
-	var docsList = [];
-	var allDocsLoaded = true;
-	//docsList = getDocumentList();//Get all Documents on a Record
-	var capDocResult = aa.document.getDocumentListByEntity(capId,"CAP");
-	var arrMissingDocs = [];
-	for(docInx = 0; docInx < capDocResult.getOutput().size(); docInx++) {
-		var thisDocument = capDocResult.getOutput().get(docInx);
-		//var thisDocument = docsList[dl];
-		var docCategory = thisDocument.getDocCategory();
-		removeCapCondition("License Required Documents", docCategory);
-		//aa.sendMail(sysFromEmail, debugEmail, "", "Info Only: ASA:LICENSES/CULTIVATOR/*/APPLICATION: Required Documents: "+ startDate, capId + br + "docCategory: " + docCategory);
+	if(!publicUser){
+		var docsList = [];
+		var allDocsLoaded = true;
+		//docsList = getDocumentList();//Get all Documents on a Record
+		var capDocResult = aa.document.getDocumentListByEntity(capId,"CAP");
+		var arrMissingDocs = [];
+		for(docInx = 0; docInx < capDocResult.getOutput().size(); docInx++) {
+			var thisDocument = capDocResult.getOutput().get(docInx);
+			//var thisDocument = docsList[dl];
+			var docCategory = thisDocument.getDocCategory();
+			removeCapCondition("License Required Documents", docCategory);
+			//aa.sendMail(sysFromEmail, debugEmail, "", "Info Only: ASA:LICENSES/CULTIVATOR/*/APPLICATION: Required Documents: "+ startDate, capId + br + "docCategory: " + docCategory);
+		}
 	}
 } catch(err){
 	logDebug("An error has occurred in ASA:LICENSES/CULTIVATOR/*/APPLICATION: Required Documents: " + err.message);
