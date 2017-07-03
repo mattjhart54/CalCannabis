bd396be6267c5b5d6c718fc3960bcd208c370bed
Lynda Wacht <lwacht@users.noreply.github.com>
2017-07-03 12:52:54 -0600

    refining

--- SCRIPTS/EVENT/WTUA;LICENSES!CULTIVATOR!~!APPLICATION.js
+++ SCRIPTS/EVENT/WTUA;LICENSES!CULTIVATOR!~!APPLICATION.js
@@ -1,147 +1,175 @@
 //lwacht: send a deficiency email when the status is "Additional Information Needed"
 try{
 	if("Administrative Manager Review".equals(wfTask) && "Deficiency Letter Sent".equals(wfStatus)){
-		var deficFound = false;
-		var childOwner = getChildren("Licenses/Cultivator/*/Owner Application");
-		for (row in childOwner){
-			var ownCap = aa.cap.getCap(childOwner[row]).getOutput();
-			var ownAppStatus = ownCap.getCapStatus();
-			if(ownAppStatus=="Additional Information Needed"){
-				deficFound = true;
-			}
-		}
-		logDebug("DEFICIENCIES.length"+DEFICIENCIES.length);
-		logDebug("deficFound"+deficFound);
-		if(DEFICIENCIES.length>0 || deficFound){
-			var newAppName = "Deficiency: " + capName;
-			//create child amendment record
-			ctm = aa.proxyInvoker.newInstance("com.accela.aa.aamain.cap.CapTypeModel").getOutput();
-			ctm.setGroup("Licenses");
-			ctm.setType("Cultivator");
-			ctm.setSubType("Medical");
-			ctm.setCategory("Amendment");
-			var newDefId = aa.cap.createSimplePartialRecord(ctm,newAppName, "INCOMPLETE CAP").getOutput();
-			if(newDefId){
-				//relate amendment to application
-				var resCreateRelat = aa.cap.createAppHierarchy(capId, newDefId); 
-				if (resCreateRelat.getSuccess()){
-					logDebug("Child application successfully linked");
-				}else{
-					logDebug("Could not link applications: " + resCreateRelat.getErrorMessage());
-				}
-				copyASITables(capId,newDefId,["CANNABIS FINANCIAL INTEREST", "OWNERS", "ATTACHMENTS"]);
-				copyContactsByType(capId, newDefId,"Designated Responsible Party");
-				copyContactsByType(capId, newDefId,"Primary Contact");
-				//find out how many amendment records there have been so we can create an AltId
-				var childAmend = getChildren("Licenses/Cultivator/Medical/Amendment");
-				var cntChild = childAmend.length;
-				cntChild ++;
-				logDebug("cntChild: " + cntChild);
-				if(cntChild<10){
-					cntChild = "0" +cntChild;
-				}
-				var newAltId = capIDString +"-DEF"+ cntChild;
-				logDebug("newAltId: " + newAltId);
-				var updAltId = aa.cap.updateCapAltID(newDefId,newAltId);
-				if(!updAltId.getSuccess()){
-					logDebug("Error updating Alt Id: " + newAltId + ":: " +updAltId.getErrorMessage());
-				}else{
-					logDebug("Deficiency record ID updated to : " + newAltId);
-				}
-				for(rec in childOwner){
-					//now process the child owner applications for any deficiencies
-					var thisOwnCapId = childOwner[rec];
-					var ownCap = aa.cap.getCap(thisOwnCapId).getOutput();
-					var ownAppStatus = ownCap.getCapStatus();
-					var ownAppName = ownCap.getSpecialText();
-					if(ownAppStatus=="Additional Information Needed"){
-						var newOwnAppName = "Deficiency: " + ownAppName;
-						//create child deficiency record for the owner
-						ctm = aa.proxyInvoker.newInstance("com.accela.aa.aamain.cap.CapTypeModel").getOutput();
-						ctm.setGroup("Licenses");
-						ctm.setType("Cultivator");
-						ctm.setSubType("Medical");
-						ctm.setCategory("Amendment");
-						var newODefId = aa.cap.createSimplePartialRecord(ctm,newOwnAppName, "INCOMPLETE CAP").getOutput();
-						if(newODefId){
-							var resOCreateRelat = aa.cap.createAppHierarchy(thisOwnCapId, newODefId); 
-							if (resOCreateRelat.getSuccess()){
-								logDebug("Child application successfully linked");
-							}else{
-								logDebug("Could not link applications: " + resOCreateRelat.getErrorMessage());
-							}
-							copyASITables(thisOwnCapId,newODefId,["CANNABIS FINANCIAL INTEREST", "CONVICTIONS", "ATTACHMENTS"]);
-							copyContacts(capId, newDefId);
-							editContactType("Owner","Primary Contact",newODefId);
-							//get the current number of deficiency children to set the AltId
-							var childOAmend = getChildren("Licenses/Cultivator/Medical/Amendment");
-							var cntOChild = childOAmend.length;
-							cntOChild ++;
-							logDebug("childOAmend.length: " + childOAmend.length);
-							logDebug("cntOChild: " + cntOChild);
-							if(cntOChild<10){
-								cntOChild = "0" +cntOChild;
-							}
-							var newOAltId = thisOwnCapId.getCustomID() +"-DEF"  + cntOChild;
-							logDebug("newOAltId: " + newOAltId);
-							var updOAltId = aa.cap.updateCapAltID(newODefId,newOAltId);
-							if(!updOAltId.getSuccess()){
-								logDebug("Error updating Owner Alt Id: " + newOAltId + ":: " +updOAltId.getErrorMessage());
-							}else{
-								logDebug("Deficiency owner record ID updated to : " + newOAltId);
-							}
-						}
-					}
-				}
-				//populate the email notification that will go to the primary contact
-				var eParams = aa.util.newHashtable(); 
-				addParameter(eParams, "$$wfDateMMDDYYYY$$", wfDateMMDDYYYY);
-				currCapId = capId;
-				capId = newDefId;
-				getACARecordParam4Notification(eParams,acaUrl);
-				capId = currCapId;
-				var staffUser = new userObj(wfStaffUserID);
-				staffUser.getEmailTemplateParams(eParams,"scientist")
-				getWorkflowParams4Notification(eParams);
-				var drpContact = getContactObj(capId,"Designated Responsible Party");
-				var priContact = getContactObj(capId,"Primary Contact");
-				getContactParams4Notification(eParams,"Primary Contact");
-				priAddresses = priContact.addresses;
-				for (x in priAddresses){
-					thisAddr = priAddresses[x];
-					if(thisAddr.getAddressType()=="Mailing"){
-						addParameter(eParams, "$$priAddress1$$", thisAddr.addressLine1);
-						addParameter(eParams, "$$priCity$$", thisAddr.city);
-						addParameter(eParams, "$$priState$$", thisAddr.state);
-						addParameter(eParams, "$$priZip$$", thisAddr.zip);
-					}
-				}
-				//logDebug("eParams: " + eParams);
-				var drpEmail = ""+drpContact.capContact.getEmail();
-				var priEmail = ""+priContact.capContact.getEmail();
-				var priChannel =  lookup("CONTACT_PREFERRED_CHANNEL",""+ priContact.capContact.getPreferredChannel());
-				var rParams = aa.util.newHashMap(); 
-				rParams.put("agencyid", "CALCANNABIS");
-				rParams.put("capid", capId.getCustomID());
-				var capId4Email = aa.cap.createCapIDScriptModel(capId.getID1(), capId.getID2(), capId.getID3());
-				var rFile;
-				rFile = generateReport(capId,"ACA Permit","Licenses",rParams);
-				if (rFile) {
-					var rFiles = [];
-					rFiles.push(rFile);
-					if(priChannel.indexOf("Email") >= 0 || priChannel.indexOf("E-mail") >= 0){
-						if(priContact.capContact.getEmail()==drpContact.capContact.getEmail()){
-							sendNotification(sysFromEmail,drpEmail,"","LCA_DEFICIENCY",eParams, rFile,capId);
-						}else{
-							//sendNotification(sysFromEmail,"lwacht@trustvip.com","","LCA_DEFICIENCY",eParams, null,capId);
-							aa.document.sendEmailAndSaveAsDocument(sysFromEmail, drpEmail + ";" + priEmail, "", "LCA_DEFICIENCY", eParams, capId4Email, rFiles);
-						}
-					}
-				}
-			}
+		var emailPriReport = false;
+		var emailDRPReport = false;
+		var priContact = getContactObj(capId,"Primary Contact");
+		var priChannel =  lookup("CONTACT_PREFERRED_CHANNEL",""+ priContact.capContact.getPreferredChannel());
+		if(priChannel.indexOf("Email") >= 0 || priChannel.indexOf("E-mail") >= 0){
+			emailPriReport = true;
 		}else{
-			showMessage = true; 
-			comment("The Deficiency tables on this table and the child owner applications are empty.  No email will be sent.");
+			showMessage=true;
+			comment("The Primary Contact, " + priContact.capContact.getFirstName() + " " + priContact.capContact.getLastName() + ", has requested all correspondence be mailed.  Please mail the displayed report.");
+		}
+		var drpContact = getContactObj(capId,"Designated Responsible Party");
+		var drptChannel =  lookup("CONTACT_PREFERRED_CHANNEL",""+ drpContact.capContact.getPreferredChannel());
+		if(drptChannel.indexOf("Email") >= 0 || drptChannel.indexOf("E-mail") >= 0){
+			emailDRPReport = true;
+		}else{
+			showMessage=true;
+			comment("The Designated Responsible Party, " + drpContact.capContact.firstName + " " + drpContact.capContact.lastName + ", has requested all correspondence be mailed.  Please mail the displayed report.");
+		}
+		if(emailPriReport || emailDRPReport){
+			var deficFound = false;
+			var childOwner = getChildren("Licenses/Cultivator/*/Owner Application");
+			for (row in childOwner){
+				var ownCap = aa.cap.getCap(childOwner[row]).getOutput();
+				var ownAppStatus = ownCap.getCapStatus();
+				if(ownAppStatus=="Additional Information Needed"){
+					deficFound = true;
+				}
+			}
+			logDebug("DEFICIENCIES.length"+DEFICIENCIES.length);
+			logDebug("deficFound"+deficFound);
+			if(DEFICIENCIES.length>0 || deficFound){
+				var newAppName = "Deficiency: " + capName;
+				//create child amendment record
+				ctm = aa.proxyInvoker.newInstance("com.accela.aa.aamain.cap.CapTypeModel").getOutput();
+				ctm.setGroup("Licenses");
+				ctm.setType("Cultivator");
+				ctm.setSubType("Medical");
+				ctm.setCategory("Amendment");
+				var resDefId = aa.cap.createSimplePartialRecord(ctm,newAppName, "INCOMPLETE CAP");
+				if(resDefId.getSuccess()){
+					var newDefId = resDefId.getOutput();
+					//relate amendment to application
+					var resCreateRelat = aa.cap.createAppHierarchy(capId, newDefId); 
+					if (resCreateRelat.getSuccess()){
+						logDebug("Child application successfully linked");
+					}else{
+						logDebug("Could not link applications: " + resCreateRelat.getErrorMessage());
+					}
+					copyASITables(capId,newDefId,["CANNABIS FINANCIAL INTEREST", "OWNERS", "ATTACHMENTS"]);
+					copyContactsByType(capId, newDefId,"Designated Responsible Party");
+					copyContactsByType(capId, newDefId,"Primary Contact");
+					//find out how many amendment records there have been so we can create an AltId
+					var childAmend = getChildren("Licenses/Cultivator/Medical/Amendment");
+					var cntChild = childAmend.length;
+					cntChild ++;
+					logDebug("cntChild: " + cntChild);
+					if(cntChild<10){
+						cntChild = "0" +cntChild;
+					}
+					var newAltId = capIDString +"-DEF"+ cntChild;
+					logDebug("newAltId: " + newAltId);
+					var updAltId = aa.cap.updateCapAltID(newDefId,newAltId);
+					if(!updAltId.getSuccess()){
+						logDebug("Error updating Alt Id: " + newAltId + ":: " +updAltId.getErrorMessage());
+					}else{
+						logDebug("Deficiency record ID updated to : " + newAltId);
+					}
+					for(rec in childOwner){
+						//now process the child owner applications for any deficiencies
+						var thisOwnCapId = childOwner[rec];
+						var ownCap = aa.cap.getCap(thisOwnCapId).getOutput();
+						var ownAppStatus = ownCap.getCapStatus();
+						var ownAppName = ownCap.getSpecialText();
+						if(ownAppStatus=="Additional Information Needed"){
+							var newOwnAppName = "Deficiency: " + ownAppName;
+							//create child deficiency record for the owner
+							ctm = aa.proxyInvoker.newInstance("com.accela.aa.aamain.cap.CapTypeModel").getOutput();
+							ctm.setGroup("Licenses");
+							ctm.setType("Cultivator");
+							ctm.setSubType("Medical");
+							ctm.setCategory("Amendment");
+							var newODefId = aa.cap.createSimplePartialRecord(ctm,newOwnAppName, "INCOMPLETE CAP").getOutput();
+							if(newODefId){
+								var resOCreateRelat = aa.cap.createAppHierarchy(thisOwnCapId, newODefId); 
+								if (resOCreateRelat.getSuccess()){
+									logDebug("Child application successfully linked");
+								}else{
+									logDebug("Could not link applications: " + resOCreateRelat.getErrorMessage());
+								}
+								copyASITables(thisOwnCapId,newODefId,["CANNABIS FINANCIAL INTEREST", "CONVICTIONS", "ATTACHMENTS"]);
+								copyContacts(capId, newDefId);
+								editContactType("Owner","Primary Contact",newODefId);
+								//get the current number of deficiency children to set the AltId
+								var childOAmend = getChildren("Licenses/Cultivator/Medical/Amendment");
+								var cntOChild = childOAmend.length;
+								cntOChild ++;
+								logDebug("childOAmend.length: " + childOAmend.length);
+								logDebug("cntOChild: " + cntOChild);
+								if(cntOChild<10){
+									cntOChild = "0" +cntOChild;
+								}
+								var newOAltId = thisOwnCapId.getCustomID() +"-DEF"  + cntOChild;
+								logDebug("newOAltId: " + newOAltId);
+								var updOAltId = aa.cap.updateCapAltID(newODefId,newOAltId);
+								if(!updOAltId.getSuccess()){
+									logDebug("Error updating Owner Alt Id: " + newOAltId + ":: " +updOAltId.getErrorMessage());
+								}else{
+									logDebug("Deficiency owner record ID updated to : " + newOAltId);
+								}
+							}
+						}
+					}
+					//populate the email notification that will go to the primary contact
+					var eParams = aa.util.newHashtable(); 
+					addParameter(eParams, "$$wfDateMMDDYYYY$$", wfDateMMDDYYYY);
+					currCapId = capId;
+					capId = newDefId;
+					getACARecordParam4Notification(eParams,acaUrl);
+					capId = currCapId;
+					var staffUser = new userObj(wfStaffUserID);
+					staffUser.getEmailTemplateParams(eParams,"scientist")
+					getWorkflowParams4Notification(eParams);
+					var contPhone = priContact.capContact.phone1;
+					var fmtPhone = contPhone.substr(0,3) + "-" + contPhone.substr(3,3) +"-" + contPhone.substr(6,4);
+					addParameter(eParams, "$$contactPhone1$$", fmtPhone);
+					addParameter(eParams, "$$contactFirstName$$", priContact.capContact.firstName);
+					addParameter(eParams, "$$contactLastName$$", priContact.capContact.lastName);
+					addParameter(eParams, "$$contactEmail$$", priContact.capContact.email);
+					priAddresses = priContact.addresses;
+					for (x in priAddresses){
+						thisAddr = priAddresses[x];
+						if(thisAddr.getAddressType()=="Mailing"){
+							addParameter(eParams, "$$priAddress1$$", thisAddr.addressLine1);
+							addParameter(eParams, "$$priCity$$", thisAddr.city);
+							addParameter(eParams, "$$priState$$", thisAddr.state);
+							addParameter(eParams, "$$priZip$$", thisAddr.zip);
+						}
+					}
+					//logDebug("eParams: " + eParams);
+					var drpEmail = ""+drpContact.capContact.getEmail();
+					var priEmail = ""+priContact.capContact.getEmail();
+					var rParams = aa.util.newHashMap(); 
+					rParams.put("agencyid", servProvCode);
+					rParams.put("capid", capId.getCustomID());
+					var capId4Email = aa.cap.createCapIDScriptModel(capId.getID1(), capId.getID2(), capId.getID3());
+					var rFile;
+					rFile = generateReport(capId,"ACA Permit","Licenses",rParams);
+					if (rFile) {
+						var rFiles = [];
+						rFiles.push(rFile);
+						if(priContact.capContact.getEmail()==drpContact.capContact.getEmail()){
+							sendNotification(sysFromEmail,drpEmail,"","LCA_DEFICIENCY",eParams, rFiles,capId);
+						}else{
+							if(emailPriReport){
+								aa.document.sendEmailAndSaveAsDocument(sysFromEmail, priEmail + ";" + priEmail, "", "LCA_DEFICIENCY", eParams, capId4Email, rFiles,capId);
+							}
+							if(emailDRPReport){
+								aa.document.sendEmailAndSaveAsDocument(sysFromEmail, drpEmail + ";" + priEmail, "", "LCA_DEFICIENCY", eParams, capId4Email, rFiles,capId);
+
+							}
+						}
+					}
+				}else{
+					logDebug("Error creating deficiency record: " +resDefId.getErrorMessage());
+				}
+			}else{
+				showMessage = true; 
+				comment("The Deficiency tables on this table and the child owner applications are empty.  No email will be sent.");
+			}
 		}
 	}
 }catch(err){
--- SCRIPTS/EVENT/WTUB;LICENSES!CULTIVATOR!~!APPLICATION.js
+++ SCRIPTS/EVENT/WTUB;LICENSES!CULTIVATOR!~!APPLICATION.js
@@ -4,11 +4,22 @@ try{
 	if("Administrative Manager Review".equals(wfTask) && "Deficiency Letter Sent".equals(wfStatus)){
 		showDebug=false;
 		var priContact = getContactObj(capId,"Primary Contact");
+		var drpContact = getContactObj(capId,"Designated Responsible Party");
+		var showReport = false;
 		if(priContact){
 			var priChannel =  lookup("CONTACT_PREFERRED_CHANNEL",""+ priContact.capContact.getPreferredChannel());
 			if(priChannel.indexOf("Email") < 0 && priChannel.indexOf("E-mail") < 0){
-				displayReport("ACA Permit", "agencyid", servProvCode,"capid", capId.getCustomID());
+				showReport = true;
 			}
+		}
+		if(drpContact){
+			var priChannel =  lookup("CONTACT_PREFERRED_CHANNEL",""+ drpContact.capContact.getPreferredChannel());
+			if(priChannel.indexOf("Email") < 0 && priChannel.indexOf("E-mail") < 0){
+				showReport = true;
+			}
+		}
+		if(showReport){
+			displayReport("ACA Permit", "agencyid", servProvCode,"capid", capId.getCustomID());
 		}
 	}
 }catch(err){
