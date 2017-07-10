b9ce2cf949bcdaa8910cda3d7559a55236fd7ca9
Lynda Wacht <lwacht@users.noreply.github.com>
2017-07-10 06:35:09 -0600

    more reports

--- SCRIPTS/EVENT/WTUB;LICENSES!CULTIVATOR!~!APPLICATION.js
+++ SCRIPTS/EVENT/WTUB;LICENSES!CULTIVATOR!~!APPLICATION.js
@@ -27,6 +27,35 @@ try{
 	logDebug(err.stack);
 }
 
+//lwacht: when the status is set to a status that requires notification and the preferred channel is *not* email,
+//display the deficiency report for printing
+try{
+	if(matches(wfStatus, "Disqualified", "Withdrawn", "Denied", "Science Manager Review Complete")){
+		showDebug=false;
+		var priContact = getContactObj(capId,"Primary Contact");
+		var drpContact = getContactObj(capId,"Designated Responsible Party");
+		var showReport = false;
+		if(priContact){
+			var priChannel =  lookup("CONTACT_PREFERRED_CHANNEL",""+ priContact.capContact.getPreferredChannel());
+			if(priChannel.indexOf("Email") < 0 && priChannel.indexOf("E-mail") < 0){
+				showReport = true;
+			}
+		}
+		if(drpContact){
+			var priChannel =  lookup("CONTACT_PREFERRED_CHANNEL",""+ drpContact.capContact.getPreferredChannel());
+			if(priChannel.indexOf("Email") < 0 && priChannel.indexOf("E-mail") < 0){
+				showReport = true;
+			}
+		}
+		if(showReport){
+			displayReport("Free-Form RTF", "altid", capIDString, "userid", currentUserID, "today", fileDate);
+		}
+	}
+}catch(err){
+	logDebug("An error has occurred in WTUB:LICENSES/CULTIVATOR/*/APPLICATION: Deficiency Notice: " + err.message);
+	logDebug(err.stack);
+}
+
 //lwacht: all owner records need to be updated before this task can be updated
 try{
 	if("Owner Application Reviews".equals(wfTask)){
