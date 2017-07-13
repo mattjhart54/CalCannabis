87c517739ddb70d542d9b80111c564ef20ed2e51
Lynda Wacht <lwacht@users.noreply.github.com>
2017-07-13 15:26:53 -0600

    test

--- INCLUDES_CUSTOM/emailDrpPriContacts.js
+++ INCLUDES_CUSTOM/emailDrpPriContacts.js
@@ -107,9 +107,9 @@ try{
 			}
 		}else{
 			if(priContact.capContact.getEmail()==drpContact.capContact.getEmail()){
-				sendNotification(sysFromEmail,drpEmail,"",notName,eParams, null,capId);
+				sendNotification(sysFromEmail,drpEmail,"",notName,eParams, false,capId);
 			}else{ 
-				sendNotification(sysFromEmail,drpEmail+";"+priEmail,"",notName,eParams, null,capId);
+				sendNotification(sysFromEmail,drpEmail+";"+priEmail,"",notName,eParams, false,capId);
 			}
 
 		}
