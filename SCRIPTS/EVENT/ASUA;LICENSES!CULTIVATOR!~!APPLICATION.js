202c72b0a720fd4ac37889cf41dc6c4491afd47c
Lynda Wacht <lwacht@users.noreply.github.com>
2017-07-13 11:00:16 -0600

    typo

--- SCRIPTS/EVENT/ASUA;LICENSES!CULTIVATOR!~!APPLICATION.js
+++ SCRIPTS/EVENT/ASUA;LICENSES!CULTIVATOR!~!APPLICATION.js
@@ -3,6 +3,7 @@
 try{
 	if(matches(appStatus, "Disqualified", "Withdrawn", "Denied")){
 		emailDrpPriContacts("ASUA", "LCA_GENERAL_NOTIFICATION", "ACA Permit", appStatus, capId, "agencyid", servProvCode, "capid", capId.getCustomID());
+	}
 }catch(err){
 	logDebug("An error has occurred in ASUA:LICENSES/CULTIVATOR/*/APPLICATION: Generic notifications: " + err.message);
 	logDebug(err.stack);
