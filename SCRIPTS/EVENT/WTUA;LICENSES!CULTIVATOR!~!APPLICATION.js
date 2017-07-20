aaa62f404df57e6768d3ba1374b90f18e9d78389
Lynda Wacht <lwacht@users.noreply.github.com>
2017-07-20 10:11:01 -0600

    updated report name

--- SCRIPTS/EVENT/WTUA;LICENSES!CULTIVATOR!~!APPLICATION.js
+++ SCRIPTS/EVENT/WTUA;LICENSES!CULTIVATOR!~!APPLICATION.js
@@ -31,7 +31,7 @@ try{
 		if(resDefId.getSuccess()){
 			var newDefId = resDefId.getOutput();
 			if(emailReport){
-				emailDrpPriContacts("WTUA", "LCA_DEFICIENCY", "ACA Permit", false, wfStatus, newDefId, "agencyid", servProvCode, "capid", capId.getCustomID());
+				emailDrpPriContacts("WTUA", "LCA_DEFICIENCY", "Deficiency Letter", false, wfStatus, newDefId, "agencyid", servProvCode, "capid", capId.getCustomID());
 			}
 			//relate amendment to application
 			var resCreateRelat = aa.cap.createAppHierarchy(capId, newDefId); 
