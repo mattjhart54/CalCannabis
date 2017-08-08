0f771199e473d927d076edaa5ac0f7345584e867
mjhart54 <mjhart54@yahoo.com>
2017-08-08 13:29:08 -0700

    MJH: changed function to get Legal Business Name from ASI field instead of Contact.

--- INCLUDES_CUSTOM/updateLegalBusinessName.js
+++ INCLUDES_CUSTOM/updateLegalBusinessName.js
@@ -1,18 +1,8 @@
 function updateLegalBusinessName() {
-	cList = getContactArray();
-	for(c in cList) {
-		if(cList[c]["contactType"] == "Applicant") {
-			if(!matches*(cList[c]["businessName"], null, "", undefined)) {
-				updateWorkDesc(cList[c]["businessName"]);
-			}
-			else {
-				if(!matches*(cList[c]["middleName"], null, "", undefined)) {
-					updateWorkDesc(cList[c]["middleName"]);
-				}
-				else {
-					updateWorkDesc("No legal business name provided");
-				}
-			}
-		}
+	if(!matches(AInfo["Legal Business Name"], null, "", undefined)) {
+		updateWorkDesc(AInfo["Legal Business Name"]);
+	}
+	else {
+		updateWorkDesc("No legal business name provided");
 	}
 }
