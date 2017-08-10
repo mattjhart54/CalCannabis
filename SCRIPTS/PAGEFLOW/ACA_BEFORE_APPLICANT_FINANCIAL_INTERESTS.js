6b8f732e46ac749d9e812d43aae30d7515217efe
mjhart54 <mjhart54@yahoo.com>
2017-08-10 08:34:24 -0700

    MJH: added check to validate the Legal Business name is entered if Business Entity Structure is not Sole Proprietor

--- SCRIPTS/PAGEFLOW/ACA_BEFORE_APPLICANT_FINANCIAL_INTERESTS.js
+++ SCRIPTS/PAGEFLOW/ACA_BEFORE_APPLICANT_FINANCIAL_INTERESTS.js
@@ -117,6 +117,12 @@ try {
 				var thisCont = arrContacts[i];
 				var contType = thisCont.contactType;
 				if(contType =="Applicant") {
+					//check for legal business name if not a Sole Proprietor
+					if(AInfo["Business Entity Structure"] != "Sole Proprietor" && thisCont.middleName(matches("",null,undefined) {
+						showMessage = true;
+						cancel = true;
+						logMessage("Warning: Legal Business Name must be entered if the Business Entity Structure is not Sole Proprietor");
+					}
 					var refContNrb = thisCont.refContactNumber;
 					if (!matches(refContNrb,null, "", "undefined")) {
 						var pplMdl = aa.people.createPeopleModel().getOutput();
