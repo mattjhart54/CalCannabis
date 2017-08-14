b3060f4c273e828eb03e068df11fa77393569c11
mjhart54 <mjhart54@yahoo.com>
2017-08-14 13:21:36 -0700

    MJH: added debufg statements.

--- SCRIPTS/PAGEFLOW/ACA_BEFORE_OWNER_APP_CONTACT_DETAIL.js
+++ SCRIPTS/PAGEFLOW/ACA_BEFORE_OWNER_APP_CONTACT_DETAIL.js
@@ -79,6 +79,8 @@ try{
 	var capId = cap.getCapID();
 	var emailText="Information: " + br;
 	var contactList = cap.getContactsGroup(); 
+	showMessage = true; 
+	describeObject(contactList);
 	if(contactList != null && contactList.size() > 0){ 
 		var arrContacts = contactList.toArray(); 
 		for(var i in arrContacts) { 
