7dd9e6b64c35b4bd5a4a0b1bc8d5b9aec0d5b759
Lynda Wacht <lwacht@users.noreply.github.com>
2017-08-15 16:40:35 -0600

    test

--- SCRIPTS/PAGEFLOW/ACA_ONLOAD_COND_DOCS.js
+++ SCRIPTS/PAGEFLOW/ACA_ONLOAD_COND_DOCS.js
@@ -17,7 +17,7 @@
 |     will no longer be considered a "Master" script and will not be supported in future releases.  If
 |     changes are made, please add notes above.
 /------------------------------------------------------------------------------------------------------*/
-var showMessage = false; // Set to true to see results in popup window
+var showMessage = true; // Set to true to see results in popup window
 var showDebug = true; // Set to true to see debug messages in popup window
 var useAppSpecificGroupName = false; // Use Group name when populating App Specific Info Values
 var useTaskSpecificGroupName = false; // Use Group name when populating Task Specific Info Values
