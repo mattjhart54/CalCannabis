409a3ae2634f633a68719038a832ce48ee58db9b
Lynda Wacht <lwacht@users.noreply.github.com>
2017-08-23 12:49:50 -0600

    error handling

--- SCRIPTS/PAGEFLOW/ACA_BEFORE_OWNER_APP_CONTACT.js
+++ SCRIPTS/PAGEFLOW/ACA_BEFORE_OWNER_APP_CONTACT.js
@@ -90,10 +90,12 @@ try{
 			if(resCurUser.getSuccess()){
 				var currUser = resCurUser.getOutput();
 				var currEmail = currUser.email;
-				if(ownerEmail.toUpperCase() != currEmail.toUpperCase()){
-					showMessage = true;
-					cancel = true;
-					comment("Error: Only " + ownerName + " can submit this application.");
+				if(!matches(ownerEmail,"",null,"undefined")){
+					if(ownerEmail.toUpperCase() != currEmail.toUpperCase()){
+						showMessage = true;
+						cancel = true;
+						comment("Error: Only " + ownerName + " can submit this application.");
+					}
 				}
 			}else{
 				logDebug("An error occurred retrieving the current user: " + resCurUser.getErrorMessage());
