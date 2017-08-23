19d234d8f421cc8bcb575029df81e6861d528961
Lynda Wacht <lwacht@users.noreply.github.com>
2017-08-23 12:58:41 -0600

    error handling

--- SCRIPTS/EVENT/ASIUA;LICENSES!CULTIVATOR!~!OWNER APPLICATION.js
+++ SCRIPTS/EVENT/ASIUA;LICENSES!CULTIVATOR!~!OWNER APPLICATION.js
@@ -11,5 +11,5 @@ try {
 }catch (err){
 	logDebug("A JavaScript Error occurred: ASIUA: Licenses/Cultivation/*/Application: " + err.message);
 	logDebug(err.stack);
-	aa.sendMail(sysFromEmail, debugEmail, "", "A JavaScript Error occurred: ASIUA:Licenses/Cultivation/*/Application: " + startDate, "capId: " + capId + ": " + err.message + ": " + err.stack);
+	aa.sendMail(sysFromEmail, debugEmail, "", "A JavaScript Error occurred: ASIUA:Licenses/Cultivation/*/Owner Application: " + startDate, "capId: " + capId + ": " + err.message + ": " + err.stack);
 }
