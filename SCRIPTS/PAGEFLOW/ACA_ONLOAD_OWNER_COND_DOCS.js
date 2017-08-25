124aba02e64a70fb843a350e6e60ce5f334af298
Lynda Wacht <lwacht@users.noreply.github.com>
2017-08-25 10:38:35 -0600

    test

--- SCRIPTS/PAGEFLOW/ACA_ONLOAD_OWNER_COND_DOCS.js
+++ SCRIPTS/PAGEFLOW/ACA_ONLOAD_OWNER_COND_DOCS.js
@@ -17,8 +17,8 @@
 |     will no longer be considered a "Master" script and will not be supported in future releases.  If
 |     changes are made, please add notes above.
 /------------------------------------------------------------------------------------------------------*/
-var showMessage = false; // Set to true to see results in popup window
-var showDebug = false; // Set to true to see debug messages in popup window
+var showMessage = true; // Set to true to see results in popup window
+var showDebug = true; // Set to true to see debug messages in popup window
 var useAppSpecificGroupName = false; // Use Group name when populating App Specific Info Values
 var useTaskSpecificGroupName = false; // Use Group name when populating Task Specific Info Values
 var cancel = false;
@@ -145,7 +145,6 @@ try{
 		comment("</ol></div>");
 	}
 } catch (err) {
-	showDebug =true;
 	logDebug("An error has occurred in ACA_AFTER_OWNER_COND_DOCS: Main function: " + err.message);
 	logDebug(err.stack);
 	aa.sendMail(sysFromEmail, debugEmail, "", "A JavaScript Error occurred: ACA_AFTER_OWNER_COND_DOCS: Complete Contact" + startDate, "capId: " + capId + br + err.message + br + err.stack + br + currEnv);
