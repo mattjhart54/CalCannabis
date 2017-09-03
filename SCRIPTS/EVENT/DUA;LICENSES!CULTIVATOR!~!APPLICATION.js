fda9219a26cce0f9302da06a31b97b3b2cd57396
Lynda Wacht <lwacht@users.noreply.github.com>
2017-09-03 12:55:18 -0600

    test

--- SCRIPTS/EVENT/DUA;LICENSES!CULTIVATOR!~!APPLICATION.js
+++ SCRIPTS/EVENT/DUA;LICENSES!CULTIVATOR!~!APPLICATION.js
@@ -9,10 +9,12 @@ try{
 		if(fTask.getActiveFlag()=="Y") {
 			var caseMgr = wf[x].getAssignedStaff().getFirstName()+ " " +wf[x].getAssignedStaff().getLastName();
 			var assignedUserID = aa.person.getUser(wf[x].getAssignedStaff().getFirstName(),wf[x].getAssignedStaff().getMiddleName(),wf[x].getAssignedStaff().getLastName()).getOutput();
-			var staffEmail = assignedUserID.getEmail();
-	aa.sendMail(sysFromEmail, debugEmail, "", "INFO ONLY DUA:LICENSES/CULTIVATOR/*/Application: Notify of Uploaded Document: "+ startDate, capId + br +  caseMgr + br + staffEmail);
-			if(staffEmail){
-				email(staffEmail, sysFromEmail, "A new document has been uploaded.", "TA new document has been uploaded for license application " + capIDString + ". Please review this new document before taking any action on the application.");
+			if(assignedUserID!=null){
+				var staffEmail = assignedUserID.getEmail();
+				aa.sendMail(sysFromEmail, debugEmail, "", "INFO ONLY DUA:LICENSES/CULTIVATOR/*/Application: Notify of Uploaded Document: "+ startDate, capId + br +  caseMgr + br + staffEmail);
+				if(staffEmail){
+					email(staffEmail, sysFromEmail, "A new document has been uploaded.", "TA new document has been uploaded for license application " + capIDString + ". Please review this new document before taking any action on the application.");
+				}
 			}
 		}
 	}
@@ -21,5 +23,4 @@ try{
 	logDebug(err.stack);
 	aa.sendMail(sysFromEmail, debugEmail, "", "An error has occurred in DUA:LICENSES/CULTIVATOR/*/Application: Notify of Uploaded Document: "+ startDate, capId + br + err.message+ br+ err.stack + br + currEnv);
 }
-
 
