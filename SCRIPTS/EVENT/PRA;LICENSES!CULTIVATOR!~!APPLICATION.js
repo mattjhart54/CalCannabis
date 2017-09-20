b1c0a8eb51d6c2cfae498490a2995d29a064ad5a
Lynda Wacht <lwacht@users.noreply.github.com>
2017-09-20 17:01:07 -0600

    license update

--- SCRIPTS/EVENT/PRA;LICENSES!CULTIVATOR!~!APPLICATION.js
+++ SCRIPTS/EVENT/PRA;LICENSES!CULTIVATOR!~!APPLICATION.js
@@ -1,11 +1,12 @@
 //lwacht: 170817: moved record creation to before script
 //lwacht
 //send the application fee notification letter
+//lwacht 170920: paid application fee notice is not going to be sent per CDFA decision
 try{
 	if(balanceDue<=0 && isTaskActive("Administrative Review")){
-		runReportAttach(capId,"Paid Application Fee", "p1value", capId.getCustomID()); 
+		//runReportAttach(capId,"Paid Application Fee", "p1value", capId.getCustomID()); 
 		//emailDrpPriContacts("PRA", "LCA_GENERAL_NOTIFICATION", "", false, "Application Fee Paid", capId, "RECORD_ID", capId.getCustomID());
-		emailRptContact("PRA", "LCA_GENERAL_NOTIFICATION", "", false, capStatus, capId, "Designated Responsible Party", "RECORD_ID", capId.getCustomID());
+		//emailRptContact("PRA", "LCA_GENERAL_NOTIFICATION", "", false, capStatus, capId, "Designated Responsible Party", "RECORD_ID", capId.getCustomID());
 		//emailRptContact("PRA", "LCA_GENERAL_NOTIFICATION", "", false, capStatus, capId, "Primary Contact", "RECORD_ID", capId.getCustomID());
 	}
 }catch(err){
@@ -17,7 +18,10 @@ try{
 //create official license record
 try{
 	if(balanceDue<=0 && capStatus == "License Issued"){
-		runReportAttach(capId,"Official License", "p1value", capId.getCustomID());
+		var parCapId = getParent();
+		if(parCapId){
+			runReportAttach(parCapId,"Official License Certificate", "altId", parCapId.getCustomID());
+		}
 		runReportAttach(capId,"Approval Letter", "p1value", capId.getCustomID());
 		emailRptContact("PRA", "LCA_APP_APPROVAL_PAID", "", false, capStatus, capId, "Designated Responsible Party", "RECORD_ID", capId.getCustomID());
 		//emailRptContact("PRA", "LCA_APP_APPROVAL_PAID", "", false, capStatus, capId, "Primary Contact", "RECORD_ID", capId.getCustomID());
@@ -26,3 +30,4 @@ try{
 	logDebug("An error has occurred in PRA:LICENSES/CULTIVATOR/*/APPLICATION: License Fee Paid: " + err.message);
 	logDebug(err.stack);
 }
+
