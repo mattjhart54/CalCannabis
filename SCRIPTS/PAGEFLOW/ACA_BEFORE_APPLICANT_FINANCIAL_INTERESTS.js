912e445924ee29d263260c68a14c5c59558f3a64
Lynda Wacht <lwacht@users.noreply.github.com>
2017-10-05 14:27:00 -0600

    test

--- SCRIPTS/PAGEFLOW/ACA_BEFORE_APPLICANT_FINANCIAL_INTERESTS.js
+++ SCRIPTS/PAGEFLOW/ACA_BEFORE_APPLICANT_FINANCIAL_INTERESTS.js
@@ -225,7 +225,7 @@ try {
 										maxAcres = licTbl[0];
 										totAcre += parseInt(maxAcres);
 									}
-									emMesg += "capId: " + capId.getCustomID() + "; licType: " + capLicType;
+									emMesg += "capId: " + capId.getCustomID() + "; licType: " + capLicType + br;
 									if (matches(capLicType, "Medium Outdoor", "Medium Indoor", "Medium Mixed-Light Tier 1", "Medium Mixed-Light Tier 2")) {
 										mediumLic = true;
 									}
