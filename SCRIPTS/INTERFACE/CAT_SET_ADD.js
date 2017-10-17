575dae7fab138119e39d0a5e18d2756e399cb376
John Towell <john.towell@gmail.com>
2017-10-17 11:31:03 -0700

    create set if it doesn't exist (#13)

--- SCRIPTS/INTERFACE/CAT_SET_ADD.js
+++ SCRIPTS/INTERFACE/CAT_SET_ADD.js
@@ -5,16 +5,31 @@
 | Usage   : Adds the given capId to the CAT SET
 | By: John Towell
 /------------------------------------------------------------------------------------------------------*/
-showMessage = true
-showDebug = true
+showMessage = true;
+showDebug = true;
+var SET_ID = 'CAT_UPDATES';
+var SCRIPT_VERSION = '1.0';
 
-var SCRIPT_VERSION = '1.0'
 
 function addToCATSet(licenseNumberString) {
+    var createResult = createSetIfNeeded();
+    if(!createResult.getSuccess()) {
+        return createResult;
+    }
     var capId = aa.cap.getCapID(licenseNumberString).getOutput();
     var addResult = aa.set.add('CAT_UPDATES', capId);
 
     return addResult;
 }
+
+function createSetIfNeeded() {
+    var theSetResult = aa.set.getSetByPK(SET_ID);
+    if (!theSetResult.getSuccess()) {
+        theSetResult = aa.set.createSet(SET_ID, SET_ID, null, null);
+    }
+
+    return theSetResult;
+}
+
 //testing
 //addToCATSet('TAL17-0000039');
