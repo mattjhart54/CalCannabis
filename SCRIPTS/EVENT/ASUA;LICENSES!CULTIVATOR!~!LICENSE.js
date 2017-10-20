3786d3df3320e38cfc8f8c6191d6cb03c5bb0704
John Towell <john.towell@gmail.com>
2017-10-20 13:35:18 -0700

    add to CAT when status becomes inactive (#17)

--- SCRIPTS/EVENT/ASUA;LICENSES!CULTIVATOR!~!LICENSE.js
+++ SCRIPTS/EVENT/ASUA;LICENSES!CULTIVATOR!~!LICENSE.js
@@ -1,1 +1,14 @@
-//John Towell  Put code here
+/*===========================================
+Title: ASUA;LICENSES!CULTIVATOR!~!LICENSE
+Purpose: Sends license to CAT when status becomes 'Inactive'
+Author: John Towell
+============================================== */
+
+try{
+    if(matches(appStatus, "Revoked", "Suspended", "Inactive")){
+        addToCat(capId);
+    }
+}catch(err){
+    logDebug("An error has occurred in ASUA;LICENSES!CULTIVATOR!~!LICENSE: Adding to CAT Set: " + err.message);
+    logDebug(err.stack);
+}
