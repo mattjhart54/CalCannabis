7251478ddcb5dcef70fd483a863bba85f3da7d62
Lynda Wacht <lwacht@users.noreply.github.com>
2017-10-31 12:03:22 -0600

    defer payment bug workaround

--- SCRIPTS/EVENT/CTRCA;LICENSES!CULTIVATOR!~!APPLICATION.js
+++ SCRIPTS/EVENT/CTRCA;LICENSES!CULTIVATOR!~!APPLICATION.js
@@ -62,3 +62,19 @@ try{
 	logDebug(err.stack);
 	aa.sendMail(sysFromEmail, debugEmail, "", "An error has occurred in CTRCA:LICENSES/CULTIVATOR/*/APPLICATION: Relate Temp Record: "+ startDate, capId + br + err.message + br + err.stack + br + currEnv);
 }
+
+//lwacht: if defer payment is used, then turn the associated forms into real records
+try{
+	var chIds = getChildren("Licenses/Cultivator/*/*",capId);
+	for(rec in chIds){
+		var chCapId = chIds[rec]
+		if(getCapIdStatusClass(chCapId) == "INCOMPLETE EST"){
+			var chCapModel = aa.cap.getCapViewBySingle4ACA(chCapId);
+			convert2RealCAP(chCapModel);
+		}
+	}
+} catch(err){
+	logDebug("An error has occurred in CTRCA:LICENSES/CULTIVATOR/*/APPLICATION: Convert Assoc Forms: " + err.message);
+	logDebug(err.stack);
+	aa.sendMail(sysFromEmail, debugEmail, "", "An error has occurred in CTRCA:LICENSES/CULTIVATOR/*/APPLICATION: Convert Assoc Forms: "+ startDate, capId + br + err.message + br + err.stack + br + currEnv);
+}
