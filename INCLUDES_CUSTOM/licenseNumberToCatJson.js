7bbc4d8f49aa719037fddcd7887f6ae6cab0f92d
John Towell <john.towell@gmail.com>
2017-10-31 11:17:27 -0700

    refactor global helper functions to nested/private functions

--- INCLUDES_CUSTOM/licenseNumberToCatJson.js
+++ INCLUDES_CUSTOM/licenseNumberToCatJson.js
@@ -65,102 +65,110 @@ function licenseNumberToCatJson(licenseNumber) {
 
     return jsonResult;
 
-}
+    /**
+     * ======================= PRIVATE FUNCTIONS ===========================
+     *
+     * Nested functions to reduce global namespace pollution
+     */
 
-/**
- * Returns the CAT license status based on this license status
- */
-function getLicenseStatus(licenseStatus) {
-    if(licenseStatus === 'Active') {
-        return 'Active';
-    } else  {
-        return 'Inactive';
-    }
-}
-
-/**
- * Returns the CAT license type based on license number and license Type
- */
-function getLicenseType(licenseNumber, licenseType) {
-    var firstThree = licenseNumber.substring(0, 3);
-    if(firstThree === 'CAL' || firstThree === "TAL") {
-        return "A-"+licenseType;
-    } else {
-        return "M-"+licenseType;
-    }
-}
-
-/**
- * Returns information from the DRP contact array
- */
-function getDRPInfo(name) {
-    var contactArray = getContactArrayLocal();
-    for (var i = 0, len = contactArray.length; i < len; i++) {
-        if (contactArray[i]['contactType'] = 'Designated Responsible Party') {
-            return '' + contactArray[i][name];
-        }
-    }
-}
-
-function getContactArrayLocal() {
-    // Returns an array of associative arrays with contact attributes.  Attributes are UPPER CASE
-    // optional capid
-    // added check for ApplicationSubmitAfter event since the contactsgroup array is only on pageflow,
-    // on ASA it should still be pulled normal way even though still partial cap
-    var thisCap = capId;
-    if (arguments.length == 1) thisCap = arguments[0];
-
-    var cArray = new Array();
-
-    if (arguments.length == 0 && !cap.isCompleteCap() && controlString != "ApplicationSubmitAfter") // we are in a page flow script so use the capModel to get contacts
-    {
-        capContactArray = cap.getContactsGroup().toArray();
-    }
-    else {
-        var capContactResult = aa.people.getCapContactByCapID(thisCap);
-        if (capContactResult.getSuccess()) {
-            var capContactArray = capContactResult.getOutput();
+    /**
+     * Returns the CAT license status based on this license status
+     */
+    function getLicenseStatus(licenseStatus) {
+        if(licenseStatus === 'Active') {
+            return 'Active';
+        } else  {
+            return 'Inactive';
         }
     }
 
-    if (capContactArray) {
-        for (yy in capContactArray) {
-            var aArray = new Array();
-            aArray["lastName"] = capContactArray[yy].getPeople().lastName;
-            aArray["refSeqNumber"] = capContactArray[yy].getCapContactModel().getRefContactNumber();
-            aArray["firstName"] = capContactArray[yy].getPeople().firstName;
-            aArray["middleName"] = capContactArray[yy].getPeople().middleName;
-            aArray["businessName"] = capContactArray[yy].getPeople().businessName;
-            aArray["contactSeqNumber"] = capContactArray[yy].getPeople().contactSeqNumber;
-            aArray["contactType"] = capContactArray[yy].getPeople().contactType;
-            aArray["relation"] = capContactArray[yy].getPeople().relation;
-            aArray["phone1"] = capContactArray[yy].getPeople().phone1;
-            aArray["phone3"] = capContactArray[yy].getPeople().phone3;
-            aArray["email"] = capContactArray[yy].getPeople().email;
-            aArray["addressLine1"] = capContactArray[yy].getPeople().getCompactAddress().getAddressLine1();
-            aArray["addressLine2"] = capContactArray[yy].getPeople().getCompactAddress().getAddressLine2();
-            aArray["city"] = capContactArray[yy].getPeople().getCompactAddress().getCity();
-            aArray["state"] = capContactArray[yy].getPeople().getCompactAddress().getState();
-            aArray["zip"] = capContactArray[yy].getPeople().getCompactAddress().getZip();
-            aArray["fax"] = capContactArray[yy].getPeople().fax;
-            aArray["notes"] = capContactArray[yy].getPeople().notes;
-            aArray["country"] = capContactArray[yy].getPeople().getCompactAddress().getCountry();
-            aArray["fullName"] = capContactArray[yy].getPeople().fullName;
-            aArray["peopleModel"] = capContactArray[yy].getPeople();
+    /**
+     * Returns the CAT license type based on license number and license Type
+     */
+    function getLicenseType(licenseNumber, licenseType) {
+        var firstThree = licenseNumber.substring(0, 3);
+        if(firstThree === 'CAL' || firstThree === "TAL") {
+            return "A-"+licenseType;
+        } else {
+            return "M-"+licenseType;
+        }
+    }
 
-            var pa = new Array();
-
-            if (arguments.length == 0 && !cap.isCompleteCap()) {
-                var paR = capContactArray[yy].getPeople().getAttributes();
-                if (paR) pa = paR.toArray();
+    /**
+     * Returns information from the DRP contact array
+     */
+    function getDRPInfo(name) {
+        var contactArray = getContactArrayLocal();
+        for (var i = 0, len = contactArray.length; i < len; i++) {
+            if (contactArray[i]['contactType'] = 'Designated Responsible Party') {
+                return '' + contactArray[i][name];
             }
-            else
-                var pa = capContactArray[yy].getCapContactModel().getPeople().getAttributes().toArray();
-            for (xx1 in pa)
-                aArray[pa[xx1].attributeName] = pa[xx1].attributeValue;
-
-            cArray.push(aArray);
         }
     }
-    return cArray;
+
+    /**
+     * Similar to getContactArray() global function except it adds phone3 which we need.
+     */
+    function getContactArrayLocal() {
+        // Returns an array of associative arrays with contact attributes.  Attributes are UPPER CASE
+        // optional capid
+        // added check for ApplicationSubmitAfter event since the contactsgroup array is only on pageflow,
+        // on ASA it should still be pulled normal way even though still partial cap
+        var thisCap = capId;
+        if (arguments.length == 1) thisCap = arguments[0];
+
+        var cArray = new Array();
+
+        if (arguments.length == 0 && !cap.isCompleteCap() && controlString != "ApplicationSubmitAfter") // we are in a page flow script so use the capModel to get contacts
+        {
+            capContactArray = cap.getContactsGroup().toArray();
+        }
+        else {
+            var capContactResult = aa.people.getCapContactByCapID(thisCap);
+            if (capContactResult.getSuccess()) {
+                var capContactArray = capContactResult.getOutput();
+            }
+        }
+
+        if (capContactArray) {
+            for (yy in capContactArray) {
+                var aArray = new Array();
+                aArray["lastName"] = capContactArray[yy].getPeople().lastName;
+                aArray["refSeqNumber"] = capContactArray[yy].getCapContactModel().getRefContactNumber();
+                aArray["firstName"] = capContactArray[yy].getPeople().firstName;
+                aArray["middleName"] = capContactArray[yy].getPeople().middleName;
+                aArray["businessName"] = capContactArray[yy].getPeople().businessName;
+                aArray["contactSeqNumber"] = capContactArray[yy].getPeople().contactSeqNumber;
+                aArray["contactType"] = capContactArray[yy].getPeople().contactType;
+                aArray["relation"] = capContactArray[yy].getPeople().relation;
+                aArray["phone1"] = capContactArray[yy].getPeople().phone1;
+                aArray["phone3"] = capContactArray[yy].getPeople().phone3;
+                aArray["email"] = capContactArray[yy].getPeople().email;
+                aArray["addressLine1"] = capContactArray[yy].getPeople().getCompactAddress().getAddressLine1();
+                aArray["addressLine2"] = capContactArray[yy].getPeople().getCompactAddress().getAddressLine2();
+                aArray["city"] = capContactArray[yy].getPeople().getCompactAddress().getCity();
+                aArray["state"] = capContactArray[yy].getPeople().getCompactAddress().getState();
+                aArray["zip"] = capContactArray[yy].getPeople().getCompactAddress().getZip();
+                aArray["fax"] = capContactArray[yy].getPeople().fax;
+                aArray["notes"] = capContactArray[yy].getPeople().notes;
+                aArray["country"] = capContactArray[yy].getPeople().getCompactAddress().getCountry();
+                aArray["fullName"] = capContactArray[yy].getPeople().fullName;
+                aArray["peopleModel"] = capContactArray[yy].getPeople();
+
+                var pa = new Array();
+
+                if (arguments.length == 0 && !cap.isCompleteCap()) {
+                    var paR = capContactArray[yy].getPeople().getAttributes();
+                    if (paR) pa = paR.toArray();
+                }
+                else
+                    var pa = capContactArray[yy].getCapContactModel().getPeople().getAttributes().toArray();
+                for (xx1 in pa)
+                    aArray[pa[xx1].attributeName] = pa[xx1].attributeValue;
+
+                cArray.push(aArray);
+            }
+        }
+        return cArray;
+    }
 }
