57eaf5308397e2b28f0a99060f3a55ecf0c08bf8
Lynda Wacht <lwacht@users.noreply.github.com>
2017-11-13 07:14:20 -0700

    letter edit

--- SCRIPTS/EVENT/CTRCA;LICENSES!CULTIVATOR!~!APPLICATION.js
+++ SCRIPTS/EVENT/CTRCA;LICENSES!CULTIVATOR!~!APPLICATION.js
@@ -87,7 +87,7 @@ try{
 		}
 		//do not put this in CTRCB
 		runReportAttach(capId,"CDFA_Invoice_Params", "capID", capId, "invoiceNbr", invNbr, "agencyid","CALCANNABIS");
-		runReportAttach(capId,"Cash Payment Due Letter", "altId", capId.getCustomID());
+		runReportAttach(capId,"Cash Payment Due Letter", "altId", capId.getCustomID(), "contactType", "Designated Responsible Party");
 		emailRptContact("CTRCA", "LCA_GENERAL_NOTIFICATION", "", false, capStatus, capId, "Designated Responsible Party", "p1value", capId.getCustomID());
 		updateAppStatus("Application Fee Due", "Updated via CTRCA:LICENSES/CULTIVATOR/* /APPLICATION.");
 		deactivateTask("Owner Application Reviews");
