a4e7cfe1803f870e5fccfa04e4974f7f866bd4c2
Lynda Wacht <lwacht@users.noreply.github.com>
2017-12-26 11:13:52 -0700

    voided logic

---
+++ SCRIPTS/EVENT/ASUA;LICENSES!CULTIVATOR!TEMPORARY!APPLICATION.js
@@ -0,0 +1,11 @@
+//lwacht: 171226: adding logic for a voided temp application
+try{
+	// Run withdrawal report for each contact and either email notice or send message to mail notice.
+	if(matches(appStatus, "Voided")){
+		taskCloseAllActive("Voided","Task Closed by script. Record status was updated to Voided");
+	}
+}catch(err){
+	logDebug("An error has occurred in ASUA:LICENSES/CULTIVATOR/*/APPLICATION: Voided Notifications: " + err.message);
+	logDebug(err.stack);
+}
+//lwacht 171226: end
