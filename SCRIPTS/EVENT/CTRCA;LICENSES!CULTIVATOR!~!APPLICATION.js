965b97231fd95986e9827726001d339e6c3d8d69
Lynda Wacht <lwacht@users.noreply.github.com>
2018-01-08 14:47:43 -0700

    defect 5120

--- SCRIPTS/EVENT/CTRCA;LICENSES!CULTIVATOR!~!APPLICATION.js
+++ SCRIPTS/EVENT/CTRCA;LICENSES!CULTIVATOR!~!APPLICATION.js
@@ -65,7 +65,11 @@ try{
 
 //lwacht: create submission report
 try{
-	runReportAttach(capId,"Completed Application", "altId", capId.getCustomID());
+	//lwacht: 180108: defect 5120: don't run for temporary
+	if(appTypeArray[2]!="Temporary"){
+		runReportAttach(capId,"Completed Application", "altId", capId.getCustomID());
+	}
+	//lwacht: 180108: defect 5120: end
 } catch(err){
 	logDebug("An error has occurred in ASA:LICENSES/CULTIVATOR/*/APPLICATION: Submission Report: " + err.message);
 	logDebug(err.stack);
