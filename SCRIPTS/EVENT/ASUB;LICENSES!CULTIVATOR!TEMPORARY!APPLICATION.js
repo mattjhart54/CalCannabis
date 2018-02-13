3b63ba616a32b1c4112cf8a956eee50b596a4ef7
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2018-02-13 12:05:45 -0800

    mhart removed code as there is a bug that  script does not prevent update.  Changing to User Group security.

--- SCRIPTS/EVENT/ASUB;LICENSES!CULTIVATOR!TEMPORARY!APPLICATION.js
+++ SCRIPTS/EVENT/ASUB;LICENSES!CULTIVATOR!TEMPORARY!APPLICATION.js
@@ -1,5 +1,7 @@
+// mhart story 5261 02/13/2018 Commenting out code as there is a bug in scripting that does not stop the update.  Changing security to User Group
 //lwacht
 //send other notifications
+/*
 try{
 	if(appStatus =="Voided" && currentUserGroup!="LicensesAdminMgr"){
 		showMessage = true;
@@ -10,4 +12,4 @@ try{
 	logDebug("An error has occurred in ASUB:LICENSES/CULTIVATOR/TEMPORARY/APPLICATION: Voided Security: " + err.message);
 	logDebug(err.stack);
 }
-
+*/
