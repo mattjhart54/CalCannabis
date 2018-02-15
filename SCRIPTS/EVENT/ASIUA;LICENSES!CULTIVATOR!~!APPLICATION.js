94748ebc98dfc3ddd50a81d27e258ae061d87e45
Lynda Wacht <lwacht@users.noreply.github.com>
2018-02-15 11:57:43 -0700

    story 4796

--- SCRIPTS/EVENT/ASIUA;LICENSES!CULTIVATOR!~!APPLICATION.js
+++ SCRIPTS/EVENT/ASIUA;LICENSES!CULTIVATOR!~!APPLICATION.js
@@ -6,6 +6,16 @@ try {
 				defDesc = lookup("LIC_CC_DEFICIENCY_TYPE",DEFICIENCIES[x]["Deficiency Type"]);
 				DEFICIENCIES[x]["Deficiency Details"] = defDesc;
 			}
+			//lwacht: 180215: story 4796: populated a UID so that rows can be matched to child records
+			if(matches(DEFICIENCIES[x]["UID"],"",null,"undefined")){
+				var thisDate = new Date();
+				var thisTime = ""+thisDate.getTime();
+				DEFICIENCIES[x]["UID"] = thisTime;
+				var date = new Date();
+				var curDate = null;
+				do { curDate = new Date(); } 
+					while(curDate-date < 10);			}
+			//lwacht: 180215: story 4796: end
 		}
 		addASITable("DEFICIENCIES", DEFICIENCIES)
 	}
@@ -19,7 +29,13 @@ try {
 		}
 		addASITable("DENIAL REASONS", DENIALREASONS)
 	}
+}catch (err){
+	logDebug("A JavaScript Error occurred: ASIUA: Licenses/Cultivation/*/Application: Send Local Auth: " + err.message);
+	logDebug(err.stack);
+	aa.sendMail(sysFromEmail, debugEmail, "", "A JavaScript Error occurred: ASIUA:Licenses/Cultivation/*/Application: Update deficiency table: " + startDate, "capId: " + capId + ": " + err.message + ": " + err.stack);
+}
 
+try{
 	editAppName(AInfo["License Type"]);
 	updateShortNotes(AInfo["Premise County"]);
 	if(appTypeArray[2] == "Temporary") {
