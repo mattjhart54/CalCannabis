59f439f8349beea82a5591dfee1981c80409ea26
Lynda Wacht <lwacht@users.noreply.github.com>
2018-03-22 14:17:30 -0600

    untest

--- INCLUDES_CUSTOM/getReqdDocs.js
+++ INCLUDES_CUSTOM/getReqdDocs.js
@@ -347,7 +347,7 @@ try{
 	//these documents are always required
 		arrReqdDocs_Own.push(governmentIssuedID);
 		//lwacht : 180322: story ????: only require fingerprint doc when live scan is available
-		var liveScanNotActive = lookup("LIVESCAN_NOT_AVAILABLE","LIVESCAN_NOT_AVAILABLE");
+		//var liveScanNotActive = lookup("LIVESCAN_NOT_AVAILABLE","LIVESCAN_NOT_AVAILABLE");
 		aa.sendMail(sysFromEmail, debugEmail, "", "INFO ONLY: getReqdDocs: " + startDate, "capId: " + capId + ": " + br + liveScanNotActive);
 		if(!matches(liveScanNotActive,true, "true")){
 			arrReqdDocs_Own.push(fingerprintApp);
