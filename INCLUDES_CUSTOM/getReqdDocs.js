d65e17199ae4dfec5655392307e3ac35cfe50181
Lynda Wacht <lwacht@users.noreply.github.com>
2018-03-22 11:17:09 -0600

    story ????

--- INCLUDES_CUSTOM/getReqdDocs.js
+++ INCLUDES_CUSTOM/getReqdDocs.js
@@ -348,7 +348,7 @@ try{
 		arrReqdDocs_Own.push(governmentIssuedID);
 		//lwacht : 180322: story ????: only require fingerprint doc when live scan is available
 		var liveScanNotActive = lookup("LIVESCAN_NOT_AVAILABLE","LIVESCAN_NOT_AVAILABLE");
-		if(!liveScanNotActive){
+		if(!matches(liveScanNotActive,true, "true")){
 			arrReqdDocs_Own.push(fingerprintApp);
 		}
 		//lwacht : 180322: story ????: end
