9d4efcd4a03025adee4f6a73a8bd3470a5115a7e
Lynda Wacht <lwacht@users.noreply.github.com>
2018-03-23 16:02:49 -0600

    new story

---
+++ SCRIPTS/EVENT/WTUA;ENFORCEMENT!CASE!NA!NA.js
@@ -0,0 +1,10 @@
+//lwacht: 180323: test: close case disposition task with whatever status activated it 
+try{
+	if(isTaskActive("Case Disposition"){
+		closeTask("Case Disposition", "Closed - " + wfStatus, "Close via WTUA:ENFORCEMENT/CASE/NA/NA", "");
+	}
+} catch(err){
+	logDebug("An error has occurred in WTUA:ENFORCEMENT/CASE/NA/NA: Close Case Disposition task:" + err.message);
+	logDebug(err.stack);
+}
+//lwacht: 180323: test: end 
