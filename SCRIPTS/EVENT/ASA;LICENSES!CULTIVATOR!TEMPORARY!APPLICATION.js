1d099718255b91d3f637624675ce9dd8bf2cea5b
Lynda Wacht <lwacht@users.noreply.github.com>
2018-04-16 16:13:12 -0600

    story 5175

--- SCRIPTS/EVENT/ASA;LICENSES!CULTIVATOR!TEMPORARY!APPLICATION.js
+++ SCRIPTS/EVENT/ASA;LICENSES!CULTIVATOR!TEMPORARY!APPLICATION.js
@@ -12,3 +12,7 @@ try{
 	logDebug(err.stack);
 	aa.sendMail(sysFromEmail, debugEmail, "", "An error has occurred in ASA:LICENSES/CULTIVATOR/TEMPORARY/APPLICATION: Add Permanent Record: "+ startDate, capId + br + err.message + br + err.stack + br + currEnv);
 }
+
+//lwacht: 171214: story 5175: create a reference contact for the temp drp and bsns contact
+//this will be in ASA:LICENSES/CULTIVATOR/*/APPLICATION, with all the other logic
+//lwacht: 171214: story 5175: end
--- SCRIPTS/EVENT/ASA;LICENSES!CULTIVATOR!~!APPLICATION.js
+++ SCRIPTS/EVENT/ASA;LICENSES!CULTIVATOR!~!APPLICATION.js
@@ -18,6 +18,203 @@ try{
 	logDebug(err.stack);
 	aa.sendMail(sysFromEmail, debugEmail, "", "A JavaScript Error occurred: ASA:Licenses/Cultivation/*/Application:  DRP Notification: " + startDate, "capId: " + capId + br + err.message + br + err.stack + br + currEnv);
 }
+
+//lwacht: 171214: story 5175: create a reference contact for the temp drp and bsns contact
+try{
+	if(!publicUser){
+		//lwacht: create reference contact and public user account for the DRP		
+		var capContactResult = aa.people.getCapContactByCapID(capId);
+		var drpExists = false;
+		var tdrpExists = false;
+		var bsnsExists = false;
+		var asopExists = false;
+		var drpEmail = false;
+		var tdrpEmail = false;
+		var bsnsEmail = false;
+		var asopEmail = false;
+		if (capContactResult.getSuccess()){
+			Contacts = capContactResult.getOutput();
+			for (yy in Contacts){
+				var thisCont = Contacts[yy].getCapContactModel();
+				var contType = thisCont.contactType;
+				showMessage=true;
+				if(contType =="Designated Responsible Party") {
+					var drpRefContNrb = thisCont.refContactNumber;
+					drpEmail = thisCont.email.toLowerCase();
+					logDebug("drpEmail: " + drpEmail);
+					var drpCont = Contacts[yy].getCapContactModel();
+					var drpAddressList = aa.address.getContactAddressListByCapContact(thisCont).getOutput();
+				}
+				if(contType =="DRP - Temporary License") {
+					var tdrpRefContNrb = thisCont.refContactNumber;
+					tdrpEmail = thisCont.email.toLowerCase();
+					logDebug("tdrpEmail: " + tdrpEmail);
+					var tdrpCont = Contacts[yy].getCapContactModel();
+					var tdrpAddressList = aa.address.getContactAddressListByCapContact(thisCont).getOutput();
+				}
+				if(contType =="Business") {
+					//var refContNrb = thisCont.refContactNumber;
+					bsnsEmail = thisCont.email.toLowerCase();
+					logDebug("bsnsEmail: " + bsnsEmail);
+					var bsnsFName = thisCont.firstName;
+					var bsnsLName = thisCont.lastName;
+					var bsnsCont = Contacts[yy].getCapContactModel();
+					var bsnsAddressList = aa.address.getContactAddressListByCapContact(thisCont).getOutput();
+				}
+				if(contType =="Agent for Service of Process") {
+					//var refContNrb = thisCont.refContactNumber;
+					var asopEmail = thisCont.email.toLowerCase();
+					logDebug("asopEmail: " + asopEmail);
+					var asopFName = thisCont.firstName;
+					var asopLName = thisCont.lastName;
+					var asopCont = Contacts[yy].getCapContactModel();
+					var asopAddressList = aa.address.getContactAddressListByCapContact(thisCont).getOutput();
+				}
+			}
+		}
+		if(tdrpEmail){
+			var qryPeople = aa.people.createPeopleModel().getOutput().getPeopleModel();
+			qryPeople.setEmail(tdrpEmail);
+			var qryResult = aa.people.getPeopleByPeopleModel(qryPeople);
+			if (!qryResult.getSuccess()){ 
+				logDebug("WARNING: error searching for people : " + qryResult.getErrorMessage());
+			}else{
+				var peopResult = qryResult.getOutput();
+				if (peopResult.length > 0){
+					tdrpExists = true;
+				}
+			}
+		}
+		if(drpEmail){
+			var qryPeople = aa.people.createPeopleModel().getOutput().getPeopleModel();
+			qryPeople.setEmail(drpEmail);
+			var qryResult = aa.people.getPeopleByPeopleModel(qryPeople);
+			if (!qryResult.getSuccess()){ 
+				logDebug("WARNING: error searching for people : " + qryResult.getErrorMessage());
+			}else{
+				var peopResult = qryResult.getOutput();
+				if (peopResult.length > 0){
+					drpExists = true;
+				}
+			}
+		}
+		if(bsnsEmail){
+			var qryPeople = aa.people.createPeopleModel().getOutput().getPeopleModel();
+			qryPeople.setEmail(bsnsEmail);
+			var qryResult = aa.people.getPeopleByPeopleModel(qryPeople);
+			if (!qryResult.getSuccess()){ 
+				logDebug("WARNING: error searching for people : " + qryResult.getErrorMessage());
+			}else{
+				var peopResult = qryResult.getOutput();
+				if (peopResult.length > 0){
+					bsnsExists = true;
+				}
+			}
+		}
+		if(asopEmail){
+			var qryPeople = aa.people.createPeopleModel().getOutput().getPeopleModel();
+			qryPeople.setEmail(asopEmail);
+			var qryResult = aa.people.getPeopleByPeopleModel(qryPeople);
+			if (!qryResult.getSuccess()){ 
+				logDebug("WARNING: error searching for people : " + qryResult.getErrorMessage());
+			}else{
+				var peopResult = qryResult.getOutput();
+				if (peopResult.length > 0){
+					asopExists = true;
+				}
+			}
+		}
+		if(drpEmail && !drpExists){
+			var arrAddr = [];
+			var peopleModel = drpCont.getPeople();
+			for (ad in drpAddressList){
+				thisAddr = drpAddressList[ad];
+				logDebug("drpAddressList: " + thisAddr.addressLine1);
+				arrAddr.push(thisAddr);
+			}
+			if(drpEmail==bsnsEmail || drpEmail==asopEmail){
+				//if the contacts share the same email address and it isn't a reference contact already, 
+				//add the address(s) to the drp that's to be created
+				if(drpEmail==bsnsEmail){
+					for (ad in bsnsAddressList){
+						thisAddr = bsnsAddressList[ad];
+						logDebug("bsnsAddressList: " + thisAddr.addressLine1);
+						arrAddr.push(thisAddr);
+					}
+				}
+				if(drpEmail==asopEmail){
+					for (ad in asopAddressList){
+						thisAddr = asopAddressList[ad];
+						logDebug("asopAddressList: " + thisAddr.addressLine1);
+						arrAddr.push(thisAddr);
+					}
+				}
+			}
+			var contactAddressModelArr = convertContactAddressModelArr(arrAddr);
+			peopleModel.setContactAddressList(contactAddressModelArr);    
+			var addResult = aa.people.editCapContactWithAttribute(drpCont);
+			if (addResult.getSuccess()){
+				logDebug("Successfully added addresses to DRP.");
+				createRefContactsFromCapContactsAndLink(capId,["Designated Responsible Party"], null, false, false, comparePeopleStandard);
+				logDebug("Successfully created DRP");
+			}else{
+				logDebug("failure: " + addResult.getErrorMessage());
+			}
+		}
+		if(appTypeArray[2]!="Temporary"){
+			if(bsnsEmail && !bsnsExists && bsnsEmail!=drpEmail){
+				createRefContactsFromCapContactsAndLink(capId,["Business"], null, false, false, comparePeopleStandard);
+				logDebug("Successfully created Business");
+			}
+			//not needed now but leaving for when they change their minds
+			//if(asopEmail && !asopExists && asopEmail!=drpEmail && asopEmail!=bsnsEmail){
+			//	createRefContactsFromCapContactsAndLink(capId,["Business"], null, false, false, comparePeopleStandard);
+			//	logDebug("Successfully created ASOP");
+			//}
+		}
+		if(appTypeArray[2]=="Temporary"){
+			if(tdrpEmail	 && !tdrpExists){
+				var arrAddr = [];
+				var peopleModel = tdrpCont.getPeople();
+				for (ad in tdrpAddressList){
+					thisAddr = tdrpAddressList[ad];
+					logDebug("tdrpAddressList: " + thisAddr.addressLine1);
+					arrAddr.push(thisAddr);
+				}
+				if(tdrpEmail==bsnsEmail){
+					//if the contacts share the same email address and it isn't a reference contact already, 
+					//add the address(s) to the drp that's to be created
+					for (ad in bsnsAddressList){
+						thisAddr = bsnsAddressList[ad];
+						logDebug("bsnsAddressList: " + thisAddr.addressLine1);
+						arrAddr.push(thisAddr);
+					}
+				}
+				var contactAddressModelArr = convertContactAddressModelArr(arrAddr);
+				peopleModel.setContactAddressList(contactAddressModelArr);    
+				var addResult = aa.people.editCapContactWithAttribute(tdrpCont);
+				if (addResult.getSuccess()){
+					logDebug("Successfully added addresses to DRP.");
+					createRefContactsFromCapContactsAndLink(capId,["Designated Responsible Party"], null, false, false, comparePeopleStandard);
+					logDebug("Successfully created DRP");
+				}else{
+					logDebug("failure: " + addResult.getErrorMessage());
+				}
+				createRefContactsFromCapContactsAndLink(capId,["DRP - Temporary License"], null, false, false, comparePeopleStandard);
+				logDebug("Successfully created Temp DRP");
+			}
+			if(bsnsEmail && !bsnsExists && bsnsEmail!=tdrpEmail){
+				createRefContactsFromCapContactsAndLink(capId,["Business"], null, false, false, comparePeopleStandard);
+				logDebug("Successfully created Business");
+			}
+		}
+	}
+}catch (err){
+	logDebug("A JavaScript Error occurred: ASA: Licenses/Cultivation/*/Application: Create DRP/Bsns/ASOP: " + err.message);
+	logDebug(err.stack);
+}
+//lwacht: 171214: story 5175: end
+
 //mhart
 //update work description with Legal Business Name
 //lwacht: don't run for temporary app
