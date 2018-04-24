8f6bf948b1269076121782a0b8428a8d66c394f3
Lynda Wacht <lwacht@users.noreply.github.com>
2018-04-24 16:10:13 -0600

    story 5411

--- INCLUDES_CUSTOM/licenseNumberToCatJson.js
+++ INCLUDES_CUSTOM/licenseNumberToCatJson.js
@@ -6,33 +6,62 @@
 | By: John Towell
 |
 | Notes   : This file should contain all CDFA specific code for gathering CAT data.
+/	180424: lwacht: story 5411: updated to conform to standards, simplify code
 /------------------------------------------------------------------------------------------------------*/
 function licenseNumberToCatJson(licenseNumber) {
-    useAppSpecificGroupName = false;
-    licenseNumber = '' + licenseNumber;
+    licenseNumber = "" + licenseNumber;
     capId = aa.cap.getCapID(licenseNumber).getOutput();
     var capScriptObj = aa.cap.getCap(capId);
     cap = capScriptObj.getOutput();
     var capModel = (capScriptObj.getOutput()).getCapModel();
-
-    var legalBusinessName = stringValue(getAppSpecific('Legal Business Name'), 100);
-    var licenseType = getLicenseType(licenseNumber, '' + getAppSpecific('License Type'));
-    var licenseStatus = getLicenseStatus('' + capModel.getCapStatus());
-    var licenseValidityStart = dateFormat(stringValue(getAppSpecific('Valid From Date')));
+	var AInfo = [];
+	loadAppspecific(AInfo);
+    var legalBusinessName = AInfo["Legal Business Name"].substr(1,100);
+	var firstThree = licenseNumber.substring(0, 3);
+	if(firstThree == "CAL" || firstThree == "TAL") {
+		var licenseType ="A-"+AInfo["License Type"];
+	} else {
+		var licenseType = "M-"+AInfo["License Type"];
+	}
+	//lwacht: 180424: story 5411/5412: only allowing 'active' and 'inactive' statuses
+	if(capModel.getCapStatus() == "Active") {
+		var licenseStatus =  "Active";
+	} else  {
+		var licenseStatus = "Inactive";
+	}
+    var licenseValidityStart = AInfo["Valid From Date"].replace("/","-");
     var vLicenseObj = new licenseObject(licenseNumber);
-    var licenseExpiration = dateFormat(stringValue(vLicenseObj.b1ExpDate));
-    var drpPhoneNumber = stringValue(getDRPInfo('phone3'), 20);
-    var facilityPhone = stringValue(getAppSpecific('Premise Phone'), 20);
-    var drpEmail = stringValue(getDRPInfo('email'), 255);
-    var premiseAddress = stringValue(getAppSpecific('Premise Address'), 100);
-    var premiseCity = stringValue(getAppSpecific('Premise City'), 40);
-    var premiseCounty = stringValue(getAppSpecific('Premise County'), 40);
-    var premiseState = stringValue(getAppSpecific('Premise State'), 40);
-    var premiseZip = stringValue(getAppSpecific('Premise Zip'), 20);
-    var drpFirstName = stringValue(getDRPInfo('firstName'), 100);
-    var drpLastName = stringValue(getDRPInfo('lastName'), 100);
-    var apn = stringValue(getAppSpecific('APN'), 75);
-    var sellersPermitNumber = stringValue(getAppSpecific('BOE Seller\'s Permit Number'), 50);
+    var licExp = vLicenseObj.b1ExpDate;
+	var pYear = licExp.getYear() + 1899;
+	var pMonth = licExp.getMonth();
+	var pDay = licenseExlicExppiration.getDate();
+	if(pMonth<12){
+		pMonth++;
+	}else{
+		pMonth=1;
+	}
+	if (pMonth > 9)
+		var mth = pMonth.toString();
+	else
+		var mth = "0" + pMonth.toString();
+	if (pDay > 9)
+		var day = pDay.toString();
+	else
+		var day = "0" + pDay.toString();
+	var licenseExpiration = day + "/" + mth + "/" + pYear.toString();
+	var contDRP = getContactByType("Designated Responsible Party",capId);
+	var drpPhoneNumber = contDRP.phone3.substr(0, 20);
+    var facilityPhone = AInfo["Premise Phone"].substr(0,20);
+    var drpEmail = contDRP.email.substr(0, 255);
+    var premiseAddress = stringValue(getAppSpecific("Premise Address"), 100);
+    var premiseCity = AInfo["Premise City"].substr(0, 40);
+    var premiseCounty = AInfo["Premise County"].substr(0, 40);
+    var premiseState = AInfo["Premise State"].substr(0, 40);
+    var premiseZip = AInfo["Premise Zip"].substr(0, 20);
+    var drpFirstName = contDRP.firstName.substr(0, 100);
+    var drpLastName = contDRP.lastName.substr(0 100);
+    var apn = AInfo["APN"].substr(0, 75);
+    var sellersPermitNumber = AInfo["BOE Seller's Permit Number"].substr(0, 50);
 
     ////////////FORMAT DATA TO JSON////////////////////////////////////////////////////
     var jsonResult = {
@@ -65,163 +94,3 @@ function licenseNumberToCatJson(licenseNumber) {
 
     return jsonResult;
 
-    /**
-     * ======================= PRIVATE FUNCTIONS ===========================
-     *
-     * Nested functions to reduce global namespace pollution
-     */
-
-
-    /**
-     * Returns the string value or empty string ("") for nulls, can also optionally truncate
-     */
-    function stringValue(value, length) {
-        var result = '' + value;
-        if("null" == result) {
-            return "";
-        } else if ("undefined" == result) {
-            return "";
-        } else {
-            if(length) {
-                return truncate(result, length);
-            } else {
-                return result;
-            }
-        }
-    }
-
-    /**
-     * Truncates the value if it is longer then length
-     */
-    function truncate(value, length) {
-        if(value.length > length) {
-            return value.substring(0, length);
-        } else {
-            return value;
-        }
-    }
-
-    /**
-     * Formats the date to CAT format YYYY-MM-DD
-     */
-    function dateFormat(value) {
-        if(value == "") {
-            return value;
-        } else {
-            var dateSplit = value.split("/");
-            return dateSplit[2] + "-" + dateZeroPad(dateSplit[0]) + "-" + dateZeroPad(dateSplit[1]);
-        }
-    }
-
-    /**
-     * Left pads the string digit with zero for single digits.
-     */
-    function dateZeroPad(value) {
-        if(value.length === 1) {
-            return "0"+value;
-        } else {
-            return value;
-        }
-    }
-
-    /**
-     * Returns the CAT license status based on this license status
-     */
-    function getLicenseStatus(licenseStatus) {
-        if(licenseStatus === 'Active') {
-            return 'Active';
-        } else  {
-            return 'Inactive';
-        }
-    }
-
-    /**
-     * Returns the CAT license type based on license number and license Type
-     */
-    function getLicenseType(licenseNumber, licenseType) {
-        var firstThree = licenseNumber.substring(0, 3);
-        if(firstThree === 'CAL' || firstThree === "TAL") {
-            return "A-"+licenseType;
-        } else {
-            return "M-"+licenseType;
-        }
-    }
-
-    /**
-     * Returns information from the DRP contact array
-     */
-    function getDRPInfo(name) {
-        var contactArray = getContactArrayLocal();
-        for (var i = 0, len = contactArray.length; i < len; i++) {
-            if (contactArray[i]['contactType'] = 'Designated Responsible Party') {
-                return '' + contactArray[i][name];
-            }
-        }
-    }
-
-    /**
-     * Similar to getContactArray() global function except it adds phone3 which we need.
-     */
-    function getContactArrayLocal() {
-        // Returns an array of associative arrays with contact attributes.  Attributes are UPPER CASE
-        // optional capid
-        // added check for ApplicationSubmitAfter event since the contactsgroup array is only on pageflow,
-        // on ASA it should still be pulled normal way even though still partial cap
-        var thisCap = capId;
-        if (arguments.length == 1) thisCap = arguments[0];
-
-        var cArray = new Array();
-
-        if (arguments.length == 0 && !cap.isCompleteCap() && controlString != "ApplicationSubmitAfter") // we are in a page flow script so use the capModel to get contacts
-        {
-            capContactArray = cap.getContactsGroup().toArray();
-        }
-        else {
-            var capContactResult = aa.people.getCapContactByCapID(thisCap);
-            if (capContactResult.getSuccess()) {
-                var capContactArray = capContactResult.getOutput();
-            }
-        }
-
-        if (capContactArray) {
-            for (yy in capContactArray) {
-                var aArray = new Array();
-                aArray["lastName"] = capContactArray[yy].getPeople().lastName;
-                aArray["refSeqNumber"] = capContactArray[yy].getCapContactModel().getRefContactNumber();
-                aArray["firstName"] = capContactArray[yy].getPeople().firstName;
-                aArray["middleName"] = capContactArray[yy].getPeople().middleName;
-                aArray["businessName"] = capContactArray[yy].getPeople().businessName;
-                aArray["contactSeqNumber"] = capContactArray[yy].getPeople().contactSeqNumber;
-                aArray["contactType"] = capContactArray[yy].getPeople().contactType;
-                aArray["relation"] = capContactArray[yy].getPeople().relation;
-                aArray["phone1"] = capContactArray[yy].getPeople().phone1;
-                aArray["phone3"] = capContactArray[yy].getPeople().phone3;
-                aArray["email"] = capContactArray[yy].getPeople().email;
-                aArray["addressLine1"] = capContactArray[yy].getPeople().getCompactAddress().getAddressLine1();
-                aArray["addressLine2"] = capContactArray[yy].getPeople().getCompactAddress().getAddressLine2();
-                aArray["city"] = capContactArray[yy].getPeople().getCompactAddress().getCity();
-                aArray["state"] = capContactArray[yy].getPeople().getCompactAddress().getState();
-                aArray["zip"] = capContactArray[yy].getPeople().getCompactAddress().getZip();
-                aArray["fax"] = capContactArray[yy].getPeople().fax;
-                aArray["notes"] = capContactArray[yy].getPeople().notes;
-                aArray["country"] = capContactArray[yy].getPeople().getCompactAddress().getCountry();
-                aArray["fullName"] = capContactArray[yy].getPeople().fullName;
-                aArray["peopleModel"] = capContactArray[yy].getPeople();
-
-                var pa = new Array();
-
-                if (arguments.length == 0 && !cap.isCompleteCap()) {
-                    var paR = capContactArray[yy].getPeople().getAttributes();
-                    if (paR) pa = paR.toArray();
-                }
-                else
-                    var pa = capContactArray[yy].getCapContactModel().getPeople().getAttributes().toArray();
-                for (xx1 in pa)
-                    aArray[pa[xx1].attributeName] = pa[xx1].attributeValue;
-
-                cArray.push(aArray);
-            }
-        }
-        return cArray;
-    }
-}
