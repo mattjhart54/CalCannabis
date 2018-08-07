a7c62eb0def546206fccab676ccc394461156380
Lynda Wacht <lwacht@users.noreply.github.com>
2018-08-07 13:53:42 -0600

    5608

--- INCLUDES_CUSTOM/deficiencySubmitted.js
+++ INCLUDES_CUSTOM/deficiencySubmitted.js
@@ -1,3 +1,17 @@
+/*===========================================
+Title: deficiencySubmitted
+Purpose: processes all child/grandchild deficiency
+	records
+Author: Lynda Wacht		
+Functional Area : child records
+Description : 
+Reviewed By: 
+Script Type : (EMSE, EB, Pageflow, Batch): EMSE
+General Purpose/Client Specific : General
+Client developed for : CDFA_CalCannabis 
+Parameters:	
+	none
+============================================== */
 function deficiencySubmitted() {
 try {
 	parentAltId = AInfo["ParentCapId"];
@@ -16,10 +30,10 @@ try {
 			parAppType = parCap.getCapType();
 			parAppTypeString = parAppType.toString();
 			parAppTypeArray = parAppTypeString.split("/");
-			if(parAppTypeArray[3]=="Application"){
-				//lwacht: 180806: 5608: only update the Admin Review when the submitted app is not an owner app
+			//lwacht: 180806: 5608: only update the Admin Review when the submitted app is not an owner app
+			if(matches(parAppTypeArray[3], "Application", "Owner Application")){
 				if(appTypeArray[2]=="Medical"){
-				//lwacht: 180806: 5608: end
+			//lwacht: 180806: 5608: end
 					var taskItemScriptModel=aa.workflow.getTask(parentCapId, "Administrative Review");
 					if(taskItemScriptModel.getSuccess()){
 						var taskItemScript = taskItemScriptModel.getOutput();
@@ -49,42 +63,52 @@ try {
 					}
 				}
 				//lwacht: 180806: 5608: only update the Owner Review when the submitted app is the last owner app
+				//this is a bit complicated because the owner deficiency is a grandchild of the annual application,
+				//so i'm getting the grandparent annual capId, then finding each of the owner child records, then,
+				//finally looking for the rest of the owner deficiency records to see if any have not been submitted.
 				if(appTypeArray[2]=="Owner"){
-					var resParCapId = getChildren("Licenses/Cultivator/Owner/Amendment",resParCapId);
-					var unSubRecd = false;
-					for(c in resParCapId){
-						var thisChild = resParCapId[c].getCustomID();
-						if(thisChild.indexOf("T")>6){
-							unSubRecd = true;
-						}
-					}
-					if(!unSubRecd){
-				//lwacht: 180806: 5608: end
-						var taskItemScriptModel=aa.workflow.getTask(parentCapId, "Owner Application Reviews");
-						if(taskItemScriptModel.getSuccess()){
-							var taskItemScript = taskItemScriptModel.getOutput();
-							if(matches(taskItemScript.disposition, "Additional Information Needed", "Incomplete Response") ){
-								var actionByUser=taskItemScript.getTaskItem().getSysUser(); // Get action by user, this is a SysUserModel 
-								var taskUpdaterModel = aa.person.getUser(actionByUser.getFirstName(),actionByUser.getMiddleName(),actionByUser.getLastName());
-								var taskUpdater = taskUpdaterModel.getOutput(); 
-								staffEmail = taskUpdater.email;
-								email(staffEmail, sysFromEmail, "Deficiency Report for " + parentAltId, "The deficiency report " + newAltId + " has been submitted.") ;
-								logDebug("Owner Amendment record processed");
-								capId = parentCapId;
-								//lwacht: 180426: story ????: reset the assigned task
-								var asgnDateOR = getAssignedDate("Owner Application Reviews");
-								activateTask("Owner Application Reviews");
-								if(asgnDateOR){
-									updateTaskAssignedDate("Owner Application Reviews", asgnDateOR);
-								}else{
-									logDebug("No assigned date found for Owner Application Reviews");
+					var grandParCapId =  getParentByCapId(parentCapId);
+					if(grandParCapId){
+						var arrOwnChild = getChildren("Licenses/Cultivator/Medical/Owner Application",grandParCapId);
+						var unSubRecd = false;
+						for(c in arrOwnChild){
+							var thisOwnChild = arrOwnChild[c];
+							var arrOwnDefChild = getChildren("Licenses/Cultivator/Owner/Amendment",thisOwnChild);
+							for (o in arrOwnDefChild){
+								var thisOwnnDefChild = arrOwnDefChild[o].getCustomID();
+								if(thisOwnnDefChild.indexOf("T")>6){
+									unSubRecd = true;
 								}
-								//lwacht: 180426: story ????: end
-								deactivateTask("Administrative Manager Review");
-								capId = currCap;
 							}
-						}else{
-							logDebug("Error occurred getting taskItemScriptModel: Owner Application Reviews: " + taskItemScriptModel.getErrorMessage());
+						}
+						if(!unSubRecd){
+				//lwacht: 180806: 5608: end
+							var taskItemScriptModel=aa.workflow.getTask(grandParCapId, "Owner Application Reviews");
+							if(taskItemScriptModel.getSuccess()){
+								var taskItemScript = taskItemScriptModel.getOutput();
+								if(matches(taskItemScript.disposition, "Additional Information Needed", "Incomplete Response") ){
+									var actionByUser=taskItemScript.getTaskItem().getSysUser(); // Get action by user, this is a SysUserModel 
+									var taskUpdaterModel = aa.person.getUser(actionByUser.getFirstName(),actionByUser.getMiddleName(),actionByUser.getLastName());
+									var taskUpdater = taskUpdaterModel.getOutput(); 
+									staffEmail = taskUpdater.email;
+									email(staffEmail, sysFromEmail, "Owner Deficiency Reports for " + grandParCapId.getCustomID(), "All owner deficiency reports for " + grandParCapId.getCustomID() + " have been submitted.") ;
+									logDebug("Owner Amendment record processed");
+									capId = grandParCapId;
+									//lwacht: 180426: story ????: reset the assigned task
+									var asgnDateOR = getAssignedDate("Owner Application Reviews");
+									activateTask("Owner Application Reviews");
+									if(asgnDateOR){
+										updateTaskAssignedDate("Owner Application Reviews", asgnDateOR);
+									}else{
+										logDebug("No assigned date found for Owner Application Reviews");
+									}
+									//lwacht: 180426: story ????: end
+									deactivateTask("Administrative Manager Review");
+									capId = currCap;
+								}
+							}else{
+								logDebug("Error occurred getting taskItemScriptModel: Owner Application Reviews: " + taskItemScriptModel.getErrorMessage());
+							}
 						}
 					}
 				}
@@ -153,5 +177,4 @@ try {
 	logDebug("An error has occurred in CTRCA:LICENSES/CULTIVATOR/*/AMENDMENT: Notify Processor: " + err.message);
 	logDebug(err.stack);
 	aa.sendMail(sysFromEmail, debugEmail, "", "An error has occurred in CTRCA:LICENSES/CULTIVATOR/*/AMENDMENT: Notify Processor: "+ startDate, capId + br + err.message+ br+ err.stack + br + currEnv);
-}
-}
+}}
