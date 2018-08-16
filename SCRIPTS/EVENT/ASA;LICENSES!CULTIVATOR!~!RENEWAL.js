37cf1d223485a01ee836ae30be81d49a762e75fd
Lynda Wacht <lwacht@users.noreply.github.com>
2018-08-16 11:31:58 -0600

    prototype

--- SCRIPTS/EVENT/ASA;LICENSES!CULTIVATOR!~!RENEWAL.js
+++ SCRIPTS/EVENT/ASA;LICENSES!CULTIVATOR!~!RENEWAL.js
@@ -3,10 +3,17 @@
 //lwacht: commenting out and putting in CTRCA
 
 try{
+	var newAltId = parentCapId.getCustomID() + "-REN2018";
+	var resAltId = aa.cap.updateCapAltID(capId,newAltId);
+	if(resAltId.getSuccess()==true){
+		logDebug("Alt ID set to " + newAltId);
+	}else{
+		logDebug("Error updating Alt ID: " +resAltId.getErrorMessage());
+	}
 	if(AInfo["Changes"]=="Y") {
 		var expDate = dateAddMonths(null,12);
 		setLicExpirationDate(parentCapId,null,expDate,"Active");
-		emailRptContact("PRA", "LCA_APP_APPROVAL_PAID", "Official License Certificate", true, capStatus, capId, "Designated Responsible Party", "altId", parCapId.getCustomID());
+		emailRptContact("PRA", "LCA_APP_APPROVAL_PAID", "Official License Certificate", true, capStatus, capId, "Designated Responsible Party", "altId", parentCapId.getCustomID());
 	}
 } catch(err){
 	logDebug("An error has occurred in ASA:LICENSES/CULTIVATOR/*/RENEWAL: Update AltId: " + err.message);
