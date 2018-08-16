340eafc2c722686e82a328e8577fe34ad81430d5
Lynda Wacht <lwacht@users.noreply.github.com>
2018-08-16 14:26:11 -0600

    prototype

--- SCRIPTS/EVENT/CTRCA;LICENSES!CULTIVATOR!~!RENEWAL.js
+++ SCRIPTS/EVENT/CTRCA;LICENSES!CULTIVATOR!~!RENEWAL.js
@@ -1,16 +1,20 @@
 //lwacht: 080816: prototype
 try{
-	var newAltId = parentCapId.getCustomID() + "-REN2018";
-	var resAltId = aa.cap.updateCapAltID(capId,newAltId);
-	if(resAltId.getSuccess()==true){
-		logDebug("Alt ID set to " + newAltId);
-	}else{
-		logDebug("Error updating Alt ID: " +resAltId.getErrorMessage());
-	}
-	if(AInfo["Changes"]=="N") {
-		var expDate = dateAddMonths(null,12);
-		setLicExpirationDate(parentCapId,null,expDate,"Active");
-		emailRptContact("PRA", "LCA_APP_APPROVAL_PAID", "Official License Certificate", true, capStatus, capId, "Designated Responsible Party", "altId", parentCapId.getCustomID());
+	parentCapId = getParentLicenseCapID(capId)
+	logDebug("parentCapId: " + parentCapId);
+	if (vLicenseID != null) {
+		var newAltId = parentCapId.getCustomID() + "-REN2018";
+		var resAltId = aa.cap.updateCapAltID(capId,newAltId);
+		if(resAltId.getSuccess()==true){
+			logDebug("Alt ID set to " + newAltId);
+		}else{
+			logDebug("Error updating Alt ID: " +resAltId.getErrorMessage());
+		}
+		if(AInfo["Changes"]=="Y") {
+			var expDate = dateAddMonths(null,12);
+			setLicExpirationDate(parentCapId,null,expDate,"Active");
+			emailRptContact("PRA", "LCA_APP_APPROVAL_PAID", "Official License Certificate", true, capStatus, capId, "Designated Responsible Party", "altId", parentCapId.getCustomID());
+		}
 	}
 } catch(err){
 	logDebug("An error has occurred in ASA:LICENSES/CULTIVATOR/*/RENEWAL: Update AltId: " + err.message);
