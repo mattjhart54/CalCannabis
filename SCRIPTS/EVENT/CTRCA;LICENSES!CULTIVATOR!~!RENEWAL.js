7dd7d26414577b1cc24934373cfb6152707aaa5c
Lynda Wacht <lwacht@users.noreply.github.com>
2018-08-16 12:44:56 -0600

    prototype

--- SCRIPTS/EVENT/CTRCA;LICENSES!CULTIVATOR!~!RENEWAL.js
+++ SCRIPTS/EVENT/CTRCA;LICENSES!CULTIVATOR!~!RENEWAL.js
@@ -1,7 +1,4 @@
-//lwacht
-//update AltId
-//lwacht: commenting out and putting in CTRCA
-/*
+//lwacht: 080816: prototype
 try{
 	var newAltId = parentCapId.getCustomID() + "-REN2018";
 	var resAltId = aa.cap.updateCapAltID(capId,newAltId);
@@ -16,8 +13,8 @@ try{
 		emailRptContact("PRA", "LCA_APP_APPROVAL_PAID", "Official License Certificate", true, capStatus, capId, "Designated Responsible Party", "altId", parentCapId.getCustomID());
 	}
 } catch(err){
-	logDebug("An error has occurred in ASA:LICENSES/CULTIVATOR/* /RENEWAL: Update AltId: " + err.message);
+	logDebug("An error has occurred in ASA:LICENSES/CULTIVATOR/*/RENEWAL: Update AltId: " + err.message);
 	logDebug(err.stack);
 	aa.sendMail(sysFromEmail, debugEmail, "", "An error has occurred in ASA:LICENSES/CULTIVATOR/* /RENEWAL: Submission: "+ startDate, capId + br + err.message+ br+ err.stack + br + currEnv);
 }
-*/
+//lwacht: 080816: prototype
