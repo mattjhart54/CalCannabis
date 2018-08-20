87fd51618fb669df0d54a446c24184ca80e7940b
Lynda Wacht <lwacht@users.noreply.github.com>
2018-08-20 17:24:24 -0600

    prototype

--- SCRIPTS/EVENT/CTRCA;LICENSES!CULTIVATOR!~!RENEWAL.js
+++ SCRIPTS/EVENT/CTRCA;LICENSES!CULTIVATOR!~!RENEWAL.js
@@ -1,6 +1,7 @@
 //lwacht: 080816: prototype
 try{
 	var parCapArr =AInfo["Parent ID"].split("-");
+	aa.sendMail(sysFromEmail, debugEmail, "", "INFO ONLY CTRCA:LICENSES/CULTIVATOR/* /RENEWAL: Submission: "+ startDate, "parCapArr"+parCapArr + br + message + br + currEnv);
 	var parCapId = aa.cap.getCapID(parCapArr[0],parCapArr[1],parCapArr[2]).getOutput();
 	if (parCapId != null) {
 		var newAltId = parCapId.getCustomID() + "-REN2018";
@@ -14,19 +15,18 @@ try{
 			var expDate = dateAddMonths(null,12);
 			var validFromDate = dateAddMonths(null,0);
 			setLicExpirationDate(parCapId,null,expDate,"Active");
-			updateAppStatus("Active", "Updated via CTRCA:LICENSES/CULTIVATOR/*/RENEWAL");
-			//emailRptContact("PRA", "LCA_APP_APPROVAL_PAID", "Official License Certificate", true, capStatus, capId, "Designated Responsible Party", "altId", parCapId.getCustomID());
+			updateAppStatus("Active", "Updated via CTRCA:LICENSES/CULTIVATOR/*/RENEWAL",parCapId);
+			editAppSpecific("Valid From Date", validFromDate,parCapId);
+			emailRptContact("ASA", "LCA_APP_APPROVAL_PAID", "Official License Certificate", true, capStatus, capId, "Designated Responsible Party", "altId", parCapId.getCustomID());
 			result = aa.cap.getProjectByMasterID(parCapId, "Renewal", "Incomplete");
 			if (result.getSuccess()) {
 				var renewCapProjArr=result.getOutput();
 				var renewCapProj = renewCapProjArr[0];
 				renewCapProj.setStatus("Complete");
 				var updRes = aa.cap.updateProject(renewCapProj);
-				editAppSpecific("Valid From Date", validFromDate);
 			}
 		}
 	}
-	aa.sendMail(sysFromEmail, debugEmail, "", "INFO ONLY CTRCA:LICENSES/CULTIVATOR/* /RENEWAL: Submission: "+ startDate, capId + br + message + br + currEnv);
 } catch(err){
 	logDebug("An error has occurred in CTRCA:LICENSES/CULTIVATOR/*/RENEWAL: Submission: " + err.message);
 	logDebug(err.stack);
