812db46d51c300d280dc00ab9599d520c477942d
eshanower <30445369+eshanower@users.noreply.github.com>
2019-03-20 09:26:28 -0700

    US 5945

--- SCRIPTS/EVENT/WTUB;LICENSES!CULTIVATOR!~!APPLICATION.js
+++ SCRIPTS/EVENT/WTUB;LICENSES!CULTIVATOR!~!APPLICATION.js
@@ -11,6 +11,9 @@ try{
 	}
 	if(wfTask == "Scientific Review" && wfStatus == "Scientific Review Completed")  {
 			editAppSpecific("App Expiry Date", "")
+// ees 20190319 US 5945 - update Record status to Scientific Review Complete.
+			updateAppStatus("Under Scientific Review","updated by script"); 
+// ees 20190319 US 5945 end
 	}
 	if("Administrative Manager Review".equals(wfTask) && "Deficiency Letter Sent".equals(wfStatus)){
 		//set due date and expiration date
