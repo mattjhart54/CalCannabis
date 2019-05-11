cb151e9ec0c4ad81b0eeba2c497e82f8be9e8e20
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2019-05-10 17:03:49 -0700

    MJH:  Added rev function to skip deactivated contacts

---
+++ INCLUDES_CUSTOM/copyContactsByType_rev.js
@@ -0,0 +1,35 @@
+
+function copyContactsByType_rev(pFromCapId, pToCapId, pContactType)
+	{
+	//Copies all contacts from pFromCapId to pToCapId
+	//where type == pContactType and the contact does not have an end date (is active)
+	if (pToCapId==null)
+		var vToCapId = capId;
+	else
+		var vToCapId = pToCapId;
+	
+	var capContactResult = aa.people.getCapContactByCapID(pFromCapId);
+	var copied = 0;
+	if (capContactResult.getSuccess())
+		{
+		var Contacts = capContactResult.getOutput();
+		for (yy in Contacts)
+			{
+			if(Contacts[yy].getCapContactModel().getContactType() == pContactType && Contacts[yy].getCapContactModel().getEndDate() != null)
+			    {
+			    var newContact = Contacts[yy].getCapContactModel();
+			    newContact.setCapID(vToCapId);
+			    aa.people.createCapContact(newContact);
+			    copied++;
+			    logDebug("Copied contact from "+pFromCapId.getCustomID()+" to "+vToCapId.getCustomID());
+			    }
+		
+			}
+		}
+	else
+		{
+		logMessage("**ERROR: Failed to get contacts: " + capContactResult.getErrorMessage()); 
+		return false; 
+		}
+	return copied;
+	} 
