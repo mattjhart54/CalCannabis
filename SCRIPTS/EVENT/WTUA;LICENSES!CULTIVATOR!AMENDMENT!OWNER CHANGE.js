294dda202bee569b5087fb06dbfd5bcedf7ece79
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2019-06-13 09:55:12 -0700

    MJH Added code to run and attach amendment letters if the contact has a preference of Postal Mail

--- SCRIPTS/EVENT/WTUA;LICENSES!CULTIVATOR!AMENDMENT!OWNER CHANGE.js
+++ SCRIPTS/EVENT/WTUA;LICENSES!CULTIVATOR!AMENDMENT!OWNER CHANGE.js
@@ -14,7 +14,7 @@ try {
 							if(licContacts[i].getCapContactModel().getContactType() == "Owner") {
 								var licCont = licContacts[i].getCapContactModel();
 								logDebug("license " + licCont.refContactNumber + " " + licCont.contactType + " " + licCont.email + " " + licCont.lastName);
-								if(ownEmail == licCont.email.toUpperCase()) {
+								if(ownEmail == licCont.email.toUpperCase() && matches(licCont.endDate, null, "", undefined)) {
 										licFnd = true;
 										break;
 								}
@@ -28,14 +28,7 @@ try {
 							}
 							var eParams = aa.util.newHashtable(); 
 							addParameter(eParams, "$$fileDateYYYYMMDD$$", fileDateYYYYMMDD);
-							var contPhone = licCont.phone1;
-							if(contPhone){
-								var fmtPhone = contPhone.substr(0,3) + "-" + contPhone.substr(3,3) +"-" + contPhone.substr(6,4);
-							}else{
-								var fmtPhone = "";
-							}
 							addParameter(eParams, "$$altId$$", capId.getCustomID());
-							addParameter(eParams, "$$contactPhone1$$", fmtPhone);
 							addParameter(eParams, "$$contactFirstName$$", licCont.firstName);
 							addParameter(eParams, "$$contactLastName$$", licCont.lastName);
 							addParameter(eParams, "$$contactEmail$$", licCont.email);
@@ -50,10 +43,33 @@ try {
 							var priChannel =  lookup("CONTACT_PREFERRED_CHANNEL",licCont.preferredChannel);
 							if(!matches(priChannel, "",null,"undefined", false)){
 								if(priChannel.indexOf("Postal") > -1 ){
-									if(OWNERS[o]["Change Status"] == "Delete")
-										var sName = createSet("Amendment Owner Deleted","Amendment Notifications", "New");
-									else
+									if(OWNERS[o]["Change Status"] == "Delete") {
+										var amendAltId = capId.getCustomID();
+										var licAltId = parentCapId.getCustomID();
+										var scriptName = "asyncRunAmendmentLetter";
+										var envParameters = aa.util.newHashMap();
+										envParameters.put("amendCap",amendAltId);
+										envParameters.put("licCap",licAltId); 
+										envParameters.put("reportName","Amendment Owner Change Removal"); 
+										envParameters.put("currentUserID",currentUserID);
+										envParameters.put("email",priEmail);
+										envParameters.put("fromEmail","calcannabislicensing@cdfa.ca.gov");
+										aa.runAsyncScript(scriptName, envParameters);
+									}
+									else {
+										var amendAltId = capId.getCustomID();
+										var licAltId = parentCapId.getCustomID();
+										var scriptName = "asyncRunAmendmentLetter";
+										var envParameters = aa.util.newHashMap();
+										envParameters.put("amendCap",amendAltId);
+										envParameters.put("licCap",licAltId); 
+										envParameters.put("reportName","Amendment Owner Change New"); 
+										envParameters.put("currentUserID",currentUserID);
+										envParameters.put("email",priEmail);
+										envParameters.put("fromEmail","calcannabislicensing@cdfa.ca.gov");
+										aa.runAsyncScript(scriptName, envParameters);
 										var sName = createSet("Amendment Owner Modified","Amendment Notifications", "New");
+									}	
 									if(sName){
 										setAddResult=aa.set.add(sName,parentCapId);
 										if(setAddResult.getSuccess()){
@@ -74,19 +90,12 @@ try {
 						cContact = getContactObj(cId,"Owner");
 						var ownAppEmail = ""+ cContact.capContact.email;
 						ownAppEmail = ownAppEmail.toUpperCase();
-						logDebug("OA email " + ownAppEmail);
+						logDebug("ownAppEmail " + ownAppEmail + " ownEmail " + ownEmail);
 						if(ownEmail == ownAppEmail) {
 							copyContactsByType_rev(cId,parentCapId,"Owner");
 							var eParams = aa.util.newHashtable(); 
 							addParameter(eParams, "$$fileDateYYYYMMDD$$", fileDateYYYYMMDD);
-							var contPhone = cContact.capContact.phone1;
-							if(contPhone){
-								var fmtPhone = contPhone.substr(0,3) + "-" + contPhone.substr(3,3) +"-" + contPhone.substr(6,4);
-							}else{
-								var fmtPhone = "";
-							}
 							addParameter(eParams, "$$altId$$", capId.getCustomID());
-							addParameter(eParams, "$$contactPhone1$$", fmtPhone);
 							addParameter(eParams, "$$contactFirstName$$", cContact.capContact.firstName);
 							addParameter(eParams, "$$contactLastName$$", cContact.capContact.lastName);
 							addParameter(eParams, "$$contactEmail$$", cContact.capContact.email);
@@ -95,10 +104,21 @@ try {
 							var priEmail = ""+cContact.capContact.getEmail();
 							var rFiles = [];
 							sendNotification(sysFromEmail,priEmail,"","LCA_AMENDMENT_OWNER_APPROVAL",eParams, rFiles,capId);
-//							emailRptContact("", "LCA_AMENDMENT_APPROVAL", "", false, capStatus, capId, "Designated Responsible Party");
 							var priChannel =  lookup("CONTACT_PREFERRED_CHANNEL",""+ cContact.capContact.getPreferredChannel());
 							if(!matches(priChannel, "",null,"undefined", false)){
 								if(priChannel.indexOf("Postal") > -1 ){
+//	Run report and attach to record for conatact with postal preference	then add to set					
+									var amendAltId = capId.getCustomID();
+									var licAltId = parentCapId.getCustomID();
+									var scriptName = "asyncRunAmendmentLetter";
+									var envParameters = aa.util.newHashMap();
+									envParameters.put("amendCap",amendAltId);
+									envParameters.put("licCap",licAltId); 
+									envParameters.put("reportName","Amendment Owner Change New"); 
+									envParameters.put("currentUserID",currentUserID);
+									envParameters.put("email",priEmail);
+									envParameters.put("fromEmail","calcannabislicensing@cdfa.ca.gov");
+									aa.runAsyncScript(scriptName, envParameters);
 									var sName = createSet("Amendment New Owner","Amendment Notifications", "New");
 									if(sName){
 										setAddResult=aa.set.add(sName,parentCapId);
@@ -128,13 +148,7 @@ try {
 			var eParams = aa.util.newHashtable(); 
 			addParameter(eParams, "$$fileDateYYYYMMDD$$", fileDateYYYYMMDD);
 			var contPhone = priContact.capContact.phone1;
-			if(contPhone){
-				var fmtPhone = contPhone.substr(0,3) + "-" + contPhone.substr(3,3) +"-" + contPhone.substr(6,4);
-			}else{
-				var fmtPhone = "";
-			}
 			addParameter(eParams, "$$altId$$", capId.getCustomID());
-			addParameter(eParams, "$$contactPhone1$$", fmtPhone);
 			addParameter(eParams, "$$contactFirstName$$", priContact.capContact.firstName);
 			addParameter(eParams, "$$contactLastName$$", priContact.capContact.lastName);
 			addParameter(eParams, "$$contactEmail$$", priContact.capContact.email);
@@ -142,7 +156,6 @@ try {
 			var priEmail = ""+priContact.capContact.getEmail();
 			var rFiles = [];
 			sendNotification(sysFromEmail,priEmail,"","LCA_AMENDMENT_APPROVAL",eParams, rFiles,capId);
-//			emailRptContact("", "LCA_AMENDMENT_APPROVAL", "", false, capStatus, capId, "Designated Responsible Party");
 			var priChannel =  lookup("CONTACT_PREFERRED_CHANNEL",""+ priContact.capContact.getPreferredChannel());
 			if(!matches(priChannel, "",null,"undefined", false)){
 				if(priChannel.indexOf("Postal") > -1 ){
@@ -158,48 +171,47 @@ try {
 				}
 			}
 		}
-	}
+		}
 	if(wfStatus == "Amendment Rejected") {
 	//  Send rejected email notification to DRP
-			var priContact = getContactObj(parentCapId,"Designated Responsible Party");
-			if(priContact){
-				var eParams = aa.util.newHashtable(); 
-				addParameter(eParams, "$$fileDateYYYYMMDD$$", fileDateYYYYMMDD);
-				var contPhone = priContact.capContact.phone1;
-				if(contPhone){
-					var fmtPhone = contPhone.substr(0,3) + "-" + contPhone.substr(3,3) +"-" + contPhone.substr(6,4);
-				}else{
-					var fmtPhone = "";
-				}
-				TInfo = [];
-				loadTaskSpecific(TInfo);
-				addParameter(eParams, "$$rejectReason$$", TInfo["Rejection Reason"]);
-				addParameter(eParams, "$$altId$$", capId.getCustomID());
-				addParameter(eParams, "$$contactPhone1$$", fmtPhone);
-				addParameter(eParams, "$$contactFirstName$$", priContact.capContact.firstName);
-				addParameter(eParams, "$$contactLastName$$", priContact.capContact.lastName);
-				addParameter(eParams, "$$contactEmail$$", priContact.capContact.email);
-				addParameter(eParams, "$$parentId$$", parentCapId.getCustomID());
-				var priEmail = ""+priContact.capContact.getEmail();
-				var rFiles = [];
-				sendNotification(sysFromEmail,priEmail,"","LCA_AMENDMENT_REJECTED",eParams, rFiles,capId);
-		//		emailRptContact("", "LCA_AMENDMENT_APPROVAL", "", false, capStatus, capId, "Designated Responsible Party");
-				var priChannel =  lookup("CONTACT_PREFERRED_CHANNEL",""+ priContact.capContact.getPreferredChannel());
-				if(!matches(priChannel, "",null,"undefined", false)){
-					if(priChannel.indexOf("Postal") > -1 ){
-						var sName = createSet("Amendment Approval","Amendment Notifications", "New");
-						if(sName){
-							setAddResult=aa.set.add(sName,parentCapId);
-							if(setAddResult.getSuccess()){
-								logDebug(capId.getCustomID() + " successfully added to set " +sName);
-							}else{
-								logDebug("Error adding record to set " + sName + ". Error: " + setAddResult.getErrorMessage());
-							}
+		var priContact = getContactObj(parentCapId,"Designated Responsible Party");
+		if(priContact){
+			var eParams = aa.util.newHashtable(); 
+			addParameter(eParams, "$$fileDateYYYYMMDD$$", fileDateYYYYMMDD);
+			var contPhone = priContact.capContact.phone1;
+			if(contPhone){
+				var fmtPhone = contPhone.substr(0,3) + "-" + contPhone.substr(3,3) +"-" + contPhone.substr(6,4);
+			}else{
+				var fmtPhone = "";
+			}
+			TInfo = [];
+			loadTaskSpecific(TInfo);
+			addParameter(eParams, "$$rejectReason$$", TInfo["Rejection Reason"]);
+			addParameter(eParams, "$$altId$$", capId.getCustomID());
+			addParameter(eParams, "$$contactPhone1$$", fmtPhone);
+			addParameter(eParams, "$$contactFirstName$$", priContact.capContact.firstName);
+			addParameter(eParams, "$$contactLastName$$", priContact.capContact.lastName);
+			addParameter(eParams, "$$contactEmail$$", priContact.capContact.email);
+			addParameter(eParams, "$$parentId$$", parentCapId.getCustomID());
+			var priEmail = ""+priContact.capContact.getEmail();
+			var rFiles = [];
+			sendNotification(sysFromEmail,priEmail,"","LCA_AMENDMENT_REJECTED",eParams, rFiles,capId);
+			var priChannel =  lookup("CONTACT_PREFERRED_CHANNEL",""+ priContact.capContact.getPreferredChannel());
+			if(!matches(priChannel, "",null,"undefined", false)){
+				if(priChannel.indexOf("Postal") > -1 ){
+					var sName = createSet("Amendment Rejcted","Amendment Notifications", "New");
+					if(sName){
+						setAddResult=aa.set.add(sName,parentCapId);
+						if(setAddResult.getSuccess()){
+							logDebug(capId.getCustomID() + " successfully added to set " +sName);
+						}else{
+							logDebug("Error adding record to set " + sName + ". Error: " + setAddResult.getErrorMessage());
 						}
 					}
 				}
 			}
 		}
+	}	
 } catch(err){
 	logDebug("An error has occurred in WTUA:LICENSES/CULTIVATOR/AMENDMENT/OWNER CHANGE: Amendmeth Approved/Rejected " + err.message);
 	logDebug(err.stack);
