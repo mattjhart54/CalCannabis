95479651486dd10b5805e21a33093db3cfc13bbb
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2019-06-19 15:59:47 -0700

    MJH Changed logic to look for Owner Applications as child of an Owner Change record then as a child of an Application Record

--- SCRIPTS/EVENT/ASA;LICENSES!CULTIVATOR!AMENDMENT!OWNER INFORMATION.js
+++ SCRIPTS/EVENT/ASA;LICENSES!CULTIVATOR!AMENDMENT!OWNER INFORMATION.js
@@ -24,128 +24,134 @@ try {
 		logDebug("Updated amendment record AltId to " + newAltId + ".");
 	else 
 		logDebug("Error renaming amendment record " + capId);
-// Get Owner Application record
-	appRecs = getChildren("Licenses/Cultivator/*/Application",parentId);
-	var appFnd = false;
-	for(a in appRecs) {
-		appId = appRecs[a];
-		appCap = aa.cap.getCap(appRecs[a]).getOutput();
-		typeResult = appCap.getCapType();
-		typeString = typeResult.toString();
-		typeArray = typeString.split("/");
-		if(matches(typeArray[2],"Medical","Adult Use")) {
-			appFnd = true;
-			break;
-		}
-	}
-	if(appFnd) {
-		ownRecs = getChildren("Licenses/Cultivator/Medical/Owner Application",appId);
-		for(o in ownRecs) {
-			ownId = ownRecs[o];
+// Get Owner Change records then get child Owner Application records
+	var ownAppFnd = false;
+	OCRecs = getChildren("Licenses/Cultivator/Amendment/Owner Change",parentId);
+	for(oc in OCRecs) {
+		ocId = OCRecs[oc];
+		ownApps = getChildren("Licenses/Cultivator/Medical/Owner Application",ocId);
+		for(oa in ownApps) {
+			ownId = ownApps[oa];
 			var ownContact = getContactObj(ownId,"Owner");
 			if(ownContact) {
 				if(AInfo["Owner Email"].toUpperCase() == ownContact.capContact.email.toUpperCase() && AInfo["Owner First Name"].toUpperCase() == ownContact.capContact.firstName.toUpperCase() &&
 				   AInfo["Owner Last Name"].toUpperCase() == ownContact.capContact.lastName.toUpperCase()) {
-	// Copy custom fields from the license record to the parent record
-						var ownAmendRecs = getChildren("Licenses/Cultivator/Amendment/Owner Information",ownId);
-						if(ownAmendRecs != null && ownAmendRecs.length > 0) {
-							for(oa in ownAmendRecs) {
-								ownId = ownAmendRecs[oa];
-							}
-							holdId = capId;
-							capId = ownId;
-							PInfo = new Array;
-							loadAppSpecific(PInfo);
-							capId = holdId;
-				//			editAppSpecific("Percent Ownership",PInfo["Percent Update"]);
-				//			editAppSpecific("Date Owner Acquired Interest",PInfo["DOAI Update"]);
-							editAppSpecific("CA State issued ID #",PInfo["CSI Update"]);
-							editAppSpecific("Other Government issued ID #",PInfo["OGI Update"]);
-							editAppSpecific("ATI Code",PInfo["ATI Update"]);
-							editAppSpecific("Convicted of a Crime",PInfo["COC Update"]);
-							editAppSpecific("License Revoked",PInfo["LR Update"]);
-							editAppSpecific("Subject to Fines",PInfo["SF Update"]);
-							editAppSpecific("Disciplinary Actions",PInfo["DA Update"]);
-							copyASITables(ownId,capId,"DEFICIENCIES","ATTAHMENTS");
-							editAppName(AInfo["Owner Email"] + "-" + AInfo["Owner First Name"] + " " +  AInfo["Owner Last Name"]);
-						}
-						else {
-							holdId = capId;
-							capId = ownId;
-							PInfo = new Array;
-							loadAppSpecific(PInfo);
-							capId = holdId;
-			//				editAppSpecific("Percent Ownership",PInfo["Percent Ownership"]);
-			//				editAppSpecific("Date Owner Acquired Interest",PInfo["Date Owner Acquired Interest"]);
-							editAppSpecific("CA State issued ID #",PInfo["CA State issued ID #"]);
-							editAppSpecific("Other Government issued ID #",PInfo["Other Government issued ID #"]);
-							editAppSpecific("ATI Code",PInfo["ATI Code"]);
-							editAppSpecific("Convicted of a Crime",PInfo["Convicted of a Crime"]);
-							editAppSpecific("License Revoked",PInfo["License Revoked"]);
-							editAppSpecific("Subject to Fines",PInfo["Subject to Fines"]);
-							editAppSpecific("Disciplinary Actions",PInfo["Disciplinary Actions"]);
-							copyASITables(ownId,capId,"DEFICIENCIES","ATTAHMENTS");
-							editAppName(AInfo["Owner Email"] + "-" + AInfo["Owner First Name"] + " " +  AInfo["Owner Last Name"]);
-						}
-	// Link Amendment record to License reord as a child
-				var ownerAltId = ownId.getCustomID();
-				addParent(ownerAltId);
-	// Copy the Owner contact from the License Application to the Amendment record			
-				copyContactsByType_rev(ownId,capId,"Owner");
-//				copyContactsByType_rev(parentId,capId,"Designated Responsible Party");		
-				
-				
-	//  Send email notification to DRP
-						var priContact = getContactObj(capId,"Designated Responsible Party");
-						if(priContact){
-							var eParams = aa.util.newHashtable(); 
-							addParameter(eParams, "$$fileDateYYYYMMDD$$", fileDateYYYYMMDD);
-							var contPhone = priContact.capContact.phone1;
-							if(contPhone){
-								var fmtPhone = contPhone.substr(0,3) + "-" + contPhone.substr(3,3) +"-" + contPhone.substr(6,4);
-								}else{
-							var fmtPhone = "";
-							}
-							addParameter(eParams, "$$altId$$", newAltId);
-							addParameter(eParams, "$$contactPhone1$$", fmtPhone);
-							addParameter(eParams, "$$contactFirstName$$", priContact.capContact.firstName);
-							addParameter(eParams, "$$contactLastName$$", priContact.capContact.lastName);
-							addParameter(eParams, "$$contactEmail$$", priContact.capContact.email);
-							addParameter(eParams, "$$parentId$$", parentAltId);
-							var rFiles = [];
-							var priEmail = ""+priContact.capContact.getEmail();
-							sendNotification(sysFromEmail,priEmail,"","LCA_AMENDMENT_SUBMISSION",eParams, rFiles,capId)
-	//	emailRptContact("", "LCA_AMENDMENT_SUBMISSION", "", false, capStatus, capId, "Designated Responsible Party");
-							var priChannel =  lookup("CONTACT_PREFERRED_CHANNEL",""+ priContact.capContact.getPreferredChannel());
-							if(!matches(priChannel, "",null,"undefined", false)){
-								if(priChannel.indexOf("Postal") > -1 ){
-									var sName = createSet("Amendment Submission","Amendment Notifications", "New");
-									if(sName){
-										setAddResult=aa.set.add(sName,capId);
-										if(setAddResult.getSuccess()){
-											logDebug(capId.getCustomID() + " successfully added to set " +sName);
-										}else{
-											logDebug("Error adding record to set " + sName + ". Error: " + setAddResult.getErrorMessage());
-										}
-									}
-								}
-							}
-						}
-				}
-				else {
-					showMessage = true;
-					comment("No matching Owner contact found on the Owner Application record");
+					ownAppFnd = true;
+					break;
 				}
 			}
-			else {
-				showMessage = true;
-				comment("No Owner contact found on the Owner application record");
+		}
+	}
+	if(!ownAppFnd) {
+// Get Owner Application record
+		appRecs = getChildren("Licenses/Cultivator/*/Application",parentId);
+		var appFnd = false;
+		for(a in appRecs) {
+			appId = appRecs[a];
+			appCap = aa.cap.getCap(appRecs[a]).getOutput();
+			typeResult = appCap.getCapType();
+			typeString = typeResult.toString();
+			typeArray = typeString.split("/");
+			if(matches(typeArray[2],"Medical","Adult Use")) {
+				appFnd = true;
+				break;
+			}
+		}
+		if(appFnd) {
+			ownRecs = getChildren("Licenses/Cultivator/Medical/Owner Application",appId);
+			ownAppFnd = false;
+			for(o in ownRecs) {
+				ownId = ownRecs[o];
+				var ownContact = getContactObj(ownId,"Owner");
+				if(ownContact) {
+					if(AInfo["Owner Email"].toUpperCase() == ownContact.capContact.email.toUpperCase() && AInfo["Owner First Name"].toUpperCase() == ownContact.capContact.firstName.toUpperCase() &&
+						AInfo["Owner Last Name"].toUpperCase() == ownContact.capContact.lastName.toUpperCase()) {
+						ownAppFnd = true;
+						break;
+					}
+				}
+			}
+		}
+	}
+	if(ownAppFnd) {
+	// Copy custom fields from the owner application record to the amendment record
+		var ownAmendRecs = getChildren("Licenses/Cultivator/Amendment/Owner Information",ownId);
+		if(ownAmendRecs != null && ownAmendRecs.length > 0) {
+			for(oa in ownAmendRecs) {
+				ownId = ownAmendRecs[oa];
+			}
+			holdId = capId;
+			capId = ownId;
+			PInfo = new Array;
+			loadAppSpecific(PInfo);
+			capId = holdId;
+	//		editAppSpecific("Percent Ownership",PInfo["Percent Update"]);
+	//		editAppSpecific("Date Owner Acquired Interest",PInfo["DOAI Update"]);
+			editAppSpecific("CA State issued ID #",PInfo["CSI Update"]);
+			editAppSpecific("Other Government issued ID #",PInfo["OGI Update"]);
+			editAppSpecific("ATI Code",PInfo["ATI Update"]);
+			editAppSpecific("Convicted of a Crime",PInfo["COC Update"]);
+			editAppSpecific("License Revoked",PInfo["LR Update"]);
+			editAppSpecific("Subject to Fines",PInfo["SF Update"]);
+			editAppSpecific("Disciplinary Actions",PInfo["DA Update"]);
+			copyASITables(ownId,capId,"DEFICIENCIES","ATTAHMENTS");
+			editAppName(AInfo["Owner Email"] + "-" + AInfo["Owner First Name"] + " " +  AInfo["Owner Last Name"]);
+		}
+		else {
+			holdId = capId;
+			capId = ownId;
+			PInfo = new Array;
+			loadAppSpecific(PInfo);
+			capId = holdId;
+	//		editAppSpecific("Percent Ownership",PInfo["Percent Ownership"]);
+	//		editAppSpecific("Date Owner Acquired Interest",PInfo["Date Owner Acquired Interest"]);
+			editAppSpecific("CA State issued ID #",PInfo["CA State issued ID #"]);
+			editAppSpecific("Other Government issued ID #",PInfo["Other Government issued ID #"]);
+			editAppSpecific("ATI Code",PInfo["ATI Code"]);
+			editAppSpecific("Convicted of a Crime",PInfo["Convicted of a Crime"]);
+			editAppSpecific("License Revoked",PInfo["License Revoked"]);
+			editAppSpecific("Subject to Fines",PInfo["Subject to Fines"]);
+			editAppSpecific("Disciplinary Actions",PInfo["Disciplinary Actions"]);
+			copyASITables(ownId,capId,"DEFICIENCIES","ATTAHMENTS");
+			editAppName(AInfo["Owner Email"] + "-" + AInfo["Owner First Name"] + " " +  AInfo["Owner Last Name"]);
+		}
+// Link Amendment record to License reord as a child
+		var ownerAltId = ownId.getCustomID();
+		addParent(ownerAltId);
+// Copy the Owner contact from the License Application to the Amendment record			
+		copyContactsByType_rev(ownId,capId,"Owner");
+//  Send email notification to Owner
+		var priContact = getContactObj(capId,"Owner");
+		if(priContact){
+			var eParams = aa.util.newHashtable(); 
+			addParameter(eParams, "$$fileDateYYYYMMDD$$", fileDateYYYYMMDD);
+			addParameter(eParams, "$$altId$$", newAltId);
+			addParameter(eParams, "$$contactFirstName$$", priContact.capContact.firstName);
+			addParameter(eParams, "$$contactLastName$$", priContact.capContact.lastName);
+			addParameter(eParams, "$$contactEmail$$", priContact.capContact.email);
+			addParameter(eParams, "$$parentId$$", parentAltId);
+			var rFiles = [];
+			var priEmail = ""+priContact.capContact.getEmail();
+			sendNotification(sysFromEmail,priEmail,"","LCA_AMENDMENT_SUBMISSION",eParams, rFiles,capId)
+			var priChannel =  lookup("CONTACT_PREFERRED_CHANNEL",""+ priContact.capContact.getPreferredChannel());
+			if(!matches(priChannel, "",null,"undefined", false)){
+				if(priChannel.indexOf("Postal") > -1 ){
+					var sName = createSet("Amendment Submission","Amendment Notifications", "New");
+					if(sName){
+						setAddResult=aa.set.add(sName,capId);
+						if(setAddResult.getSuccess()){
+							logDebug(capId.getCustomID() + " successfully added to set " +sName);
+						}else{
+							logDebug("Error adding record to set " + sName + ". Error: " + setAddResult.getErrorMessage());
+						}
+					}
+				}
 			}
 		}
 	}
 	else {
 		showMessage = true;
-		comment("No License application record found");
+		comment("No matching Owner application record found");
 	}
 }catch(err){
 	logDebug("An error has occurred in ASA:LICENSES/CULTIVATOR/AMENDMENT/Owner Information: " + err.message);
