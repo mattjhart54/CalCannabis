385e59bcfcc271c613794a070b74675dcd039cbb
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2019-07-30 11:26:12 -0700

    MJH Script to check that there are only 1 Business and ASOP contact types

---
+++ SCRIPTS/EVENT/WTUB;LICENSES!CULTIVATOR!AMENDMENT!ADMINISTRATIVE.js
@@ -0,0 +1,28 @@
+try {
+	var amendContactResult = aa.people.getCapContactByCapID(capId);
+	if (amendContactResult.getSuccess()){
+		var amendContacts = amendContactResult.getOutput();
+		var cntBusiness = 0;
+		var cntASOP = 0;
+		for (a in amendContacts){
+			if(amendContacts[a].getCapContactModel().getContactType()== "Business") 
+				++cntBusiness;
+			if(amendContacts[a].getCapContactModel().getContactType()== "Agent for Service of Process") 
+				++cntASOP;	
+		}
+		if(cntBusiness != 1) {
+			cancel = true;
+			showmessage = true;
+			comment("There must be one and only one Business contact");
+		}
+		if(cntASOP != 1) {
+			cancel = true;
+			showmessage = true;
+			comment("There must be one and only one Agent for Service Process contact");
+		}
+	}
+			
+} catch(err){
+	logDebug("An error has occurred in WTUB:LICENSES/CULTIVATOR/AMENDMENT/ADMINISTRTIVE: Check Number of contacts " + err.message);
+	logDebug(err.stack);
+}
