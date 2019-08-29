732056800b7fe4eb0274042e845137fb4fa1b2c1
isallam <isallam@gmail.com>
2019-08-28 20:55:47 -0400

    ensure contacts are in submitted appl

--- SCRIPTS/EVENT/CTRCB;LICENSES!CULTIVATOR!~!APPLICATION.js
+++ SCRIPTS/EVENT/CTRCB;LICENSES!CULTIVATOR!~!APPLICATION.js
@@ -30,12 +30,46 @@ try{
 }
 */
 
-/*lwacht: testing to see if it works
-try{
-	aa.sendMail(sysFromEmail, debugEmail, "", "INFO ONLY: CTRCB:LICENSES/CULTIVATOR/* /APPLICATION: Convert Assoc Forms: "+ startDate, capId + br + message + br + currEnv);
+
+
+// IAS User Story Prod Defect 6135 - record app, the business, DRP, and ASOP contacts are missing.
+
+try {
+	var applContactResult = aa.people.getCapContactByCapID(capId);
+	if (applContactResult.getSuccess()){
+		var applContacts = applContactResult.getOutput();
+		var cntDRP = false;
+		var cntBusiness =false;
+		var cntASOP = false;
+		
+		for (a in applContacts){
+			if(applContacts[a].getCapContactModel().getContactType()== "Designated Responsible Party") 
+				cntDRP=true;
+			if(applContacts[a].getCapContactModel().getContactType()== "Business") 
+				cntBusiness=true;
+			if(applContacts[a].getCapContactModel().getContactType()== "Agent for Service of Process") 
+				cntASOP=true;	
+		}
+		
+		if(cntDRP = false) {
+			cancel=true;
+			showMessage=true;
+			comment("No required Designated Responsible Party contact has been entered on the application.  Please add before submitting the application");
+		}
+		if(cntBusiness = false) {
+			cancel=true;
+			showMessage=true;
+			comment("There must be one and only one Business contact");
+		}
+		if(cntASOP = false) {
+			cancel=true;
+			showMessage=true;
+			comment("There must be one and only one Agent for Service Process contact");
+		}
+		
+	}
+			
 } catch(err){
-	logDebug("An error has occurred in CTRCB:LICENSES/CULTIVATOR/* /APPLICATION: Convert Assoc Forms: " + err.message);
+	logDebug("An error has occurred in CTRCB;LICENSES!CULTIVATOR!~!APPLICATION.js: Check Number of contacts " + err.message);
 	logDebug(err.stack);
-	aa.sendMail(sysFromEmail, debugEmail, "", "An error has occurred in CTRCB:LICENSES/CULTIVATOR/* /APPLICATION: Convert Assoc Forms: "+ startDate, capId + br + err.message + br + err.stack + br + currEnv);
 }
-*/
