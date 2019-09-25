7e2c3e11a5575195481979cfc383adecff2c0a62
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2019-09-25 14:14:14 -0700

    MJH Script to activate Renewal Review task when the renewal fees are paid

---
+++ SCRIPTS/EVENT/PRA;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
@@ -0,0 +1,9 @@
+try{
+	if(balanceDue<=0 && !isTaskComplete("Renewal Review")){
+		activateTask("Renewal Review")
+		updateAppStatus("Submitted", "Updated via PRA:LICENSES/CULTIVATOR/*/Renewal.");
+	}
+}catch(err){
+	logDebug("An error has occurred in PRA:LICENSES/CULTIVATOR/*/Renewal: Renewal Fees Paid: " + err.message);
+	logDebug(err.stack);
+}
