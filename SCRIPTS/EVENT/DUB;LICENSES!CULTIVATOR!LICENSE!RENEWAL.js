7b6c87f3d0c7d0def9c7eca6b94684aba5d90ea9
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2019-10-31 16:08:10 -0700

    MJH Prevent documents from being uploaded on a License Renewal record.

---
+++ SCRIPTS/EVENT/DUB;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
@@ -0,0 +1,11 @@
+try{
+	if(documentUploadedFrom == "ACA"){
+		showMessage = true;
+		cancel = true;
+		message = "No documents are allowed to be attached to a License Renewal record";
+	}
+}catch(err){
+	logDebug("An error has occurred in DUB:LICENSES/CULTIVATOR/LICENSE/RENEWAL: No Documents: " + err.message);
+	logDebug(err.stack);
+	aa.sendMail(sysFromEmail, debugEmail, "", "An error has occurred in DUB:LICENSES/CULTIVATOR/LICENSE/RENEWAL: No Documents: "+ startDate, capId + br+ err.message+ br+ err.stack + br + "DOCUMENT ARRAY: " + documentModel);
+}
