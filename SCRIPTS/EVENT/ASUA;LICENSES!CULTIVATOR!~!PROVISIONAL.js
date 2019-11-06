c31c167b8121fbaff0588e60ba38d6ac7be936c4
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2019-11-06 10:40:26 -0800

    MJH Changed code to select statuses to add record to CAT set.

--- SCRIPTS/EVENT/ASUA;LICENSES!CULTIVATOR!~!PROVISIONAL.js
+++ SCRIPTS/EVENT/ASUA;LICENSES!CULTIVATOR!~!PROVISIONAL.js
@@ -1,7 +1,7 @@
 //Sends license to CAT when status becomes 'Inactive'
 try{
 	if (appTypeArray[2] != "Temporary") {
-		if(matches(appStatus, "Cancelled","Expired","Revoked","Surrendered", "Suspended", "Inactive")){
+		if(matches(appStatus, "Active","Cancelled","Expired","Revoked","Surrendered","Inactive")){
 			var capId = aa.cap.getCapID(capIDString).getOutput();
 			addToCat(capId);
 		}
