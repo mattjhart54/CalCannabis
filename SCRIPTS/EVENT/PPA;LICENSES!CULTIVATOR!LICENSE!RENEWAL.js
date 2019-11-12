5ac71eab7174e7d0f1a3134567769fb43b4a9c8d
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2019-11-12 14:44:58 -0800

    MJH  Removed code to update the record status

--- SCRIPTS/EVENT/PPA;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
+++ SCRIPTS/EVENT/PPA;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
@@ -1,7 +1,7 @@
 try{
 	if(balanceDue<=0 && !isTaskComplete("Renewal Review")){
 		activateTask("Renewal Review")
-		updateAppStatus("Submitted", "Updated via PRA:LICENSES/CULTIVATOR/*/Renewal.");
+//		updateAppStatus("Submitted", "Updated via PRA:LICENSES/CULTIVATOR/*/Renewal.");
 	}
 }catch(err){
 	logDebug("An error has occurred in PRA:LICENSES/CULTIVATOR/*/Renewal: Renewal Fees Paid: " + err.message);
