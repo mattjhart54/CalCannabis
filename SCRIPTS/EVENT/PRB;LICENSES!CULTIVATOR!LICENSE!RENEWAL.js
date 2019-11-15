b9319e9667932c604b90a815b0e8b86617bc3576
isallam <isallam@gmail.com>
2019-11-15 18:45:22 -0500

    fix PRB in error message

--- SCRIPTS/EVENT/PRB;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
+++ SCRIPTS/EVENT/PRB;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
@@ -4,6 +4,6 @@ try{
 		updateAppStatus("Fee Paid", "Updated via PRA:LICENSES/CULTIVATOR/*/Renewal.");
 	}
 }catch(err){
-	logDebug("An error has occurred in PRB:LICENSES/CULTIVATOR/*/Renewal: Renewal Fees Paid: " + err.message);
+	logDebug("An error has occurred in PRB:LICENSES/CULTIVATOR/*/Renewal: Renewal Fees Paid:  " + err.message);
 	logDebug(err.stack);
 }
