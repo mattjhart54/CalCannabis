413b95a7fd81c464c6529b4ef3c51c4b653a2efb
isallam <isallam@gmail.com>
2019-11-18 16:07:39 -0500

    Reverted to submitted

--- SCRIPTS/EVENT/PRB;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
+++ SCRIPTS/EVENT/PRB;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
@@ -2,8 +2,7 @@ try{
 	if(balanceDue<=0 && !isTaskComplete("Renewal Review")){
 		activateTask("Renewal Review")
 		updateAppStatus("Submitted", "Updated via PRB:LICENSES/CULTIVATOR/*/Renewal.");
-		
-		
+				
 	}
 }catch(err){
 	logDebug("An error has occurred in PRB:LICENSES/CULTIVATOR/*/Renewal: Renewal Fees Paid:  " + err.message);
