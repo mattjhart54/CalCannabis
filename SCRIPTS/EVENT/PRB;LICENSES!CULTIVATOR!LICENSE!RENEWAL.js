5e21ee2ac6b86fd98a16b158c66c4cdc123279c1
isallam <isallam@gmail.com>
2019-11-18 15:52:29 -0500

    Submitted status

--- SCRIPTS/EVENT/PRB;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
+++ SCRIPTS/EVENT/PRB;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
@@ -1,11 +1,9 @@
 try{
-	if(balanceDue>0 && !isTaskComplete("Renewal Review")){
+	if(balanceDue<=0 && !isTaskComplete("Renewal Review")){
 		activateTask("Renewal Review")
-		updateAppStatus("Payment Pending", "Updated via PRB:LICENSES/CULTIVATOR/*/Renewal.");
-		
-		}else{
-		
 		updateAppStatus("Submitted", "Updated via PRB:LICENSES/CULTIVATOR/*/Renewal.");
+		
+		
 	}
 }catch(err){
 	logDebug("An error has occurred in PRB:LICENSES/CULTIVATOR/*/Renewal: Renewal Fees Paid:  " + err.message);
