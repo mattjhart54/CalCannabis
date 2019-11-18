bf77686541e7337002a5a1be2dcc572b0276942a
isallam <isallam@gmail.com>
2019-11-18 14:34:43 -0500

    Status"Payment Pending" added

--- SCRIPTS/EVENT/PRB;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
+++ SCRIPTS/EVENT/PRB;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
@@ -1,7 +1,11 @@
 try{
-	if(balanceDue<=0 && !isTaskComplete("Renewal Review")){
+	if(balanceDue>0 && !isTaskComplete("Renewal Review")){
 		activateTask("Renewal Review")
-		updateAppStatus("Fee Paid", "Updated via PRA:LICENSES/CULTIVATOR/*/Renewal.");
+		updateAppStatus("Payment Pending", "Updated via PRB:LICENSES/CULTIVATOR/*/Renewal.");
+		
+		}else{
+		
+		updateAppStatus("Submitted", "Updated via PRB:LICENSES/CULTIVATOR/*/Renewal.");
 	}
 }catch(err){
 	logDebug("An error has occurred in PRB:LICENSES/CULTIVATOR/*/Renewal: Renewal Fees Paid:  " + err.message);
