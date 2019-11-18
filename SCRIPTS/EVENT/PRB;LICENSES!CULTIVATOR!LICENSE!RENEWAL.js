f1f7520cbc73188576ec4e488cf401345ed4b461
isallam <isallam@gmail.com>
2019-11-18 17:30:26 -0500

    disabled

--- SCRIPTS/EVENT/PRB;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
+++ SCRIPTS/EVENT/PRB;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
@@ -1,10 +1,10 @@
-/*try{
-	if(balanceDue<=0 && !isTaskComplete("Renewal Review")){
-		activateTask("Renewal Review")
-		updateAppStatus("Submitted", "Updated via PRB:LICENSES/CULTIVATOR/*/Renewal.");
+//try{
+//	if(balanceDue<=0 && !isTaskComplete("Renewal Review")){
+//		activateTask("Renewal Review")
+//		updateAppStatus("Submitted", "Updated via PRB:LICENSES/CULTIVATOR/*/Renewal.");
 				
-	}
-}catch(err){
-	logDebug("An error has occurred in PRB:LICENSES/CULTIVATOR/*/Renewal: Renewal Fees Paid:  " + err.message);
-	logDebug(err.stack);
-}*/
+//	}
+//}catch(err){
+//	logDebug("An error has occurred in PRB:LICENSES/CULTIVATOR/*/Renewal: Renewal Fees Paid:  " + err.message);
+//	logDebug(err.stack);
+//}
