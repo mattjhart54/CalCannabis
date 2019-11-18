1152f8e18321b18aec840be7c0d158714e392b6b
isallam <isallam@gmail.com>
2019-11-18 17:19:46 -0500

    no if

--- SCRIPTS/EVENT/ASA;LICENSES!CULTIVATOR!~!RENEWAL.js
+++ SCRIPTS/EVENT/ASA;LICENSES!CULTIVATOR!~!RENEWAL.js
@@ -61,12 +61,11 @@ try{
 				}
 			}
 			
-			if(balanceDue<=0 && !isTaskComplete("Renewal Review")){
+			
 		
 		updateAppStatus("Pending Payment", "Updated via ASA:LICENSES/CULTIVATOR/*/Renewal.");
 		
-		
-	}
+
 				
 		//5. Set B1PERMIT.B1_ACCESS_BY_ACA to "N" for partial CAP to not allow that it is searched by ACA user.
 	//		aa.cap.updateAccessByACA(partialCapId, "N");
