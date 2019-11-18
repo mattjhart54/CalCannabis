ebb661a2081ef444eaa72c6e937cfc78357091d6
isallam <isallam@gmail.com>
2019-11-18 17:26:19 -0500

    disabled script

--- SCRIPTS/EVENT/PRB;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
+++ SCRIPTS/EVENT/PRB;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
@@ -1,4 +1,4 @@
-try{
+/*try{
 	if(balanceDue<=0 && !isTaskComplete("Renewal Review")){
 		activateTask("Renewal Review")
 		updateAppStatus("Submitted", "Updated via PRB:LICENSES/CULTIVATOR/*/Renewal.");
@@ -7,4 +7,4 @@ try{
 }catch(err){
 	logDebug("An error has occurred in PRB:LICENSES/CULTIVATOR/*/Renewal: Renewal Fees Paid:  " + err.message);
 	logDebug(err.stack);
-}
+}*/
