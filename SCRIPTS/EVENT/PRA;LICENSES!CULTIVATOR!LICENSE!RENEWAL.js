ca4936c6bd68a32f970fc480b5ef2d53604a4dd9
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2019-11-19 11:26:14 -0800

    Update PRA;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
    MJH Changed code to set record status to Submitted when fees paid in full

--- SCRIPTS/EVENT/PRA;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
+++ SCRIPTS/EVENT/PRA;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
@@ -1,8 +1,11 @@
 try{
-	if(balanceDue<=0 && !isTaskComplete("Renewal Review")){
-		activateTask("Renewal Review")
+	if(balanceDue<=0){
 		updateAppStatus("Submitted", "Updated via PRA:LICENSES/CULTIVATOR/*/Renewal.");
+		if(!isTaskComplete("Renewal Review")){
+			activateTask("Renewal Review")
+		}
 	}
+
 }catch(err){
 	logDebug("An error has occurred in PRA:LICENSES/CULTIVATOR/*/Renewal: Renewal Fees Paid: " + err.message);
 	logDebug(err.stack);
