c7e1851df28d8164d7c91166b4aa4f25298365a2
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2019-11-19 11:24:47 -0800

    Update PPA;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
    MJH Changed code to always set record status to submitted when fees paid in full

--- SCRIPTS/EVENT/PPA;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
+++ SCRIPTS/EVENT/PPA;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
@@ -1,7 +1,9 @@
 try{
-	if(balanceDue<=0 && !isTaskComplete("Renewal Review")){
-		activateTask("Renewal Review")
-    	updateAppStatus("Submitted", "Updated via PPA:LICENSES/CULTIVATOR/*/Renewal.");
+	if(balanceDue<=0){
+		updateAppStatus("Submitted", "Updated via PRA:LICENSES/CULTIVATOR/*/Renewal.");
+		if(!isTaskComplete("Renewal Review")){
+			activateTask("Renewal Review")
+		}
 	}
 }catch(err){
 	logDebug("An error has occurred in PPA:LICENSES/CULTIVATOR/*/Renewal: Renewal Fees Paid: " + err.message);
