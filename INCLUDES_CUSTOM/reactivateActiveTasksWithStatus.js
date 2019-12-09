cfbeb1cb1d42150b173eb74e7e6db39844d0647e
jshear28 <57968271+jshear28@users.noreply.github.com>
2019-12-09 14:34:27 -0500

    6315 - Reactivating Renewal Review task when License Manager task status equals Revisions Required

---
+++ INCLUDES_CUSTOM/reactivateActiveTasksWithStatus.js
@@ -0,0 +1,20 @@
+function reactivateActiveTasksWithStatus(uStatus,processName) {
+
+    var workflowResult = aa.workflow.getTasks(capId);
+    if (workflowResult.getSuccess())
+        wfObj = workflowResult.getOutput();
+    else
+        { logMessage("**ERROR: Failed to get workflow object: " + s_capResult.getErrorMessage()); return false; }
+    
+    for (i in wfObj)
+        {
+        fTask = wfObj[i];
+        if (fTask.getProcessCode().equals(processName) || processName == null)
+            if (fTask.getDisposition() == uStatus) {
+                activateTask(fTask.getTaskDescription());
+                updateTask(fTask.getTaskDescription(),"","","");
+                
+            }
+
+        }
+}
