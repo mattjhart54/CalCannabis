8a9ef79771b78db90fa015678c3cb22b97809841
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2020-01-23 15:21:07 -0800

    MJH Added code to fast track renewal

--- SCRIPTS/EVENT/CTRCA;LICENSES!CULTIVATOR!~!RENEWAL.js
+++ SCRIPTS/EVENT/CTRCA;LICENSES!CULTIVATOR!~!RENEWAL.js
@@ -76,36 +76,140 @@ try{
 		aa.sendMail(sysFromEmail, debugEmail, "", "A JavaScript Error occurred: CTRCA:Licenses/Cultivation/License/Renewal: Get Fee: " + startDate, "fee description: " + feeDesc + br + "capId: " + capId + br + currEnv);
 		logDebug("An error occurred retrieving fee item: " + feeDesc);
 	}
-	
-//  Send email notification to DRP
-	var priContact = getContactObj(capId,"Designated Responsible Party");
-	if(priContact){
-		var eParams = aa.util.newHashtable(); 
-		addParameter(eParams, "$$altId$$", newAltId);
-		addParameter(eParams, "$$contactFirstName$$", priContact.capContact.firstName);
-		addParameter(eParams, "$$contactLastName$$", priContact.capContact.lastName);
-		addParameter(eParams, "$$contactEmail$$", priContact.capContact.email);
-		addParameter(eParams, "$$parentId$$", vLicenseAltId);
-		var rFiles = [];
-		var priEmail = ""+priContact.capContact.getEmail();
-		
-		sendNotification(sysFromEmail,priEmail,"","LCA_RENEWAL_SUBMISSION",eParams, rFiles,capId)
+// Check License Cases to see if renewal can be fast tracked
+	var licenseId = AInfo["License Number"];
+	var licId = aa.cap.getCapID(licenseId);
+	licId = licId.getOutput();
+	childIds  = getChildren("Licenses/Cultivator/License Case/*",licId);
+	holdId = capId;
+	var caseReview = false
+	for(c in childIds) {
+		capId = childIds[c];
+		cCap = aa.cap.getCap(capId).getOutput();
+		cStatus = cCap.getCapStatus();
+		cInfo = new Array;
+		loadAppSpecific(cInfo);
+		logDebug(cInfo["Case Renewal Type"] + " - " + cStatus);
+		if(cInfo["Case Renewal Type"] == "Renewal Manual Review") {
+			if(!matches(cStatus, "Resolved", "Closed")) {
+				caseReview = true;
+				break;
+			}
+		}
+	}	
+	capid = holdId;
+	if(caseReview) {	
+		var renewalCapProject;
+		var vExpDate;
+		var vNewExpDate;
+		var vLicenseObj;
+		licAltId = licId.getCustomID();
+		altId = capId.getCustomID();
+		if (licId != null) {
+	// Get current expiration date.
+			vLicenseObj = new licenseObject(null, licId);
+			vExpDate = vLicenseObj.b1ExpDate;
+			vExpDate = new Date(vExpDate);
+	// Extend license expiration by 1 year
+			vNewExpDate = new Date(vExpDate.getFullYear() + 1, vExpDate.getMonth(), vExpDate.getDate());
+	// Update license expiration date
+			logDebug("Updating Expiration Date to: " + vNewExpDate);
+			vLicenseObj.setExpiration(dateAdd(vNewExpDate,0));
+	// Set license record expiration and status to active
+			vLicenseObj.setStatus("Active");
+			updateAppStatus("Active","License Renewed",licId);
+	// Update the Cultivation Type on the license record
+			if(AInfo["Designation Change"] == "Yes") {
+				editAppSpecific("Cultivator Type",AInfo["Designation Type"],licId);
+				editAppName(AInfo["Designation Type"] + " - " + AInfo["License Type"],licId);
+			}
+	//Set renewal to complete, used to prevent more than one renewal record for the same cycle
+			renewalCapProject = getRenewalCapByParentCapIDForIncomplete(licId);
+			if (renewalCapProject != null) {
+				renewalCapProject.setStatus("Complete");
+				renewalCapProject.setRelationShip("R");  // move to related records
+				aa.cap.updateProject(renewalCapProject);
+			}
+			
+	//Run Official License Certificate and Annual/Provisional Renewal Approval Email and Set the DRP		
+			if (AInfo["License Issued Type"] == "Provisional")
+				var approvalLetter = "Provisional Renewal Approval";
+			else
+				var approvalLetter = "Approval Letter Renewal";
+			var scriptName = "asyncRunOfficialLicenseRpt";
+			var envParameters = aa.util.newHashMap();
+			envParameters.put("licType", "");
+			envParameters.put("appCap",altId);
+			envParameters.put("licCap",licAltId); 
+			envParameters.put("reportName","Official License Certificate");
+			envParameters.put("approvalLetter", approvalLetter);
+			envParameters.put("emailTemplate", "LCA_RENEWAL_APPROVAL");
+			envParameters.put("reason", "");
+			envParameters.put("currentUserID",currentUserID);
+			envParameters.put("contType","Designated Responsible Party");
+			envParameters.put("fromEmail","calcannabislicensing@cdfa.ca.gov");
+			aa.runAsyncScript(scriptName, envParameters);
+			
+			var priContact = getContactObj(capId,"Designated Responsible Party");
+		// If DRP preference is Postal add license record to Annual/Provisional Renewal A set
+			if(priContact){
+				var priChannel =  lookup("CONTACT_PREFERRED_CHANNEL",""+ priContact.capContact.getPreferredChannel());
+				if(!matches(priChannel, "",null,"undefined", false)){
+					if(priChannel.indexOf("Postal") > -1 ){
+						
+						if (AInfo['License Issued Type'] == "Provisional") {
+							var sName = createSet("PROVISIONAL_LICENSE_RENEWAL_ISSUED","License Notifications", "New");
+						}
+						if (AInfo['License Issued Type'] == "Annual"){
+							var sName = createSet("ANNUAL_LICENSE_RENEWAL_ISSUED","License Notifications", "New");
+						}
+						if(sName){
+							setAddResult=aa.set.add(sName,licId);
+							if(setAddResult.getSuccess()){
+								logDebug(capId.getCustomID() + " successfully added to set " +sName);
+							}else{
+								logDebug("Error adding record to set " + sName + ". Error: " + setAddResult.getErrorMessage());
+							}
+						}
+					}
+				}
+			}
+	// Add record to the CAT set
+			addToCat(licId);
+		}
 
-		var priChannel =  lookup("CONTACT_PREFERRED_CHANNEL",""+ priContact.capContact.getPreferredChannel());
-		if(!matches(priChannel, "",null,"undefined", false)){
-			if(priChannel.indexOf("Postal") > -1 ){
-				var sName = createSet("Renewal Submission","Renewal Notifications", "New");
-				if(sName){
-					setAddResult=aa.set.add(sName,capId);
-					if(setAddResult.getSuccess()){
-						logDebug(capId.getCustomID() + " successfully added to set " +sName);
-					}else{
-						logDebug("Error adding record to set " + sName + ". Error: " + setAddResult.getErrorMessage());
+	}
+	else {
+		//  Send email notification to DRP
+		var priContact = getContactObj(capId,"Designated Responsible Party");
+		if(priContact){
+			var eParams = aa.util.newHashtable(); 
+			addParameter(eParams, "$$altId$$", newAltId);
+			addParameter(eParams, "$$contactFirstName$$", priContact.capContact.firstName);
+			addParameter(eParams, "$$contactLastName$$", priContact.capContact.lastName);
+			addParameter(eParams, "$$contactEmail$$", priContact.capContact.email);
+			addParameter(eParams, "$$parentId$$", vLicenseAltId);
+			var rFiles = [];
+			var priEmail = ""+priContact.capContact.getEmail();
+			
+			sendNotification(sysFromEmail,priEmail,"","LCA_RENEWAL_SUBMISSION",eParams, rFiles,capId)
+	
+			var priChannel =  lookup("CONTACT_PREFERRED_CHANNEL",""+ priContact.capContact.getPreferredChannel());
+			if(!matches(priChannel, "",null,"undefined", false)){
+				if(priChannel.indexOf("Postal") > -1 ){
+					var sName = createSet("Renewal Submission","Renewal Notifications", "New");
+					if(sName){
+						setAddResult=aa.set.add(sName,capId);
+						if(setAddResult.getSuccess()){
+							logDebug(capId.getCustomID() + " successfully added to set " +sName);
+						}else{
+							logDebug("Error adding record to set " + sName + ". Error: " + setAddResult.getErrorMessage());
+						}
 					}
 				}
 			}
 		}
-	}
+	} 
 
 } catch(err){
 	logDebug("An error has occurred in CTRCA:LICENSES/CULTIVATOR/*/RENEWAL: Submission: " + err.message);
