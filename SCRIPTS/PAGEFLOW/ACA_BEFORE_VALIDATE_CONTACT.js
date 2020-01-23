29c5088a57195df457e33106eb825f1dedaa244f
jshear28 <57968271+jshear28@users.noreply.github.com>
2020-01-23 10:38:50 -0500

    Unit Test: show debug window

--- SCRIPTS/PAGEFLOW/ACA_BEFORE_VALIDATE_CONTACT.js
+++ SCRIPTS/PAGEFLOW/ACA_BEFORE_VALIDATE_CONTACT.js
@@ -17,8 +17,8 @@
 |     will no longer be considered a "Master" script and will not be supported in future releases.  If
 |     changes are made, please add notes above.
 /------------------------------------------------------------------------------------------------------*/
-var showMessage = false; // Set to true to see results in popup window
-var showDebug = false; // Set to true to see debug messages in popup window
+var showMessage = true; // Set to true to see results in popup window
+var showDebug = true; // Set to true to see debug messages in popup window
 var useAppSpecificGroupName = false; // Use Group name when populating App Specific Info Values
 var useTaskSpecificGroupName = false; // Use Group name when populating Task Specific Info Values
 var cancel = false;
@@ -201,9 +201,12 @@ try {
 			}
 		}
 		//jshear: 200123: story 6306: Check for Smart Chars
-
+		logDebug(myObj);
+		logDebug(invalidChar);
 		for (x in myObj){
-			if (myObj.hasOwnProperty(x)){	
+			logDebug("within1");
+			if (myObj.hasOwnProperty(x)){
+				logDebug("within2");				
 				var smartChar = isUnicode(String(myObj[x]));
 				if (smartChar){
 					invalidChar = true;
@@ -211,8 +214,9 @@ try {
 				}
 			}
 		}
-
+		
 		if (invalidChar){
+			logDebug("within3");
 			cancel = true;
 			showMessage = true;
 			logMessage(smartCharMessage);
