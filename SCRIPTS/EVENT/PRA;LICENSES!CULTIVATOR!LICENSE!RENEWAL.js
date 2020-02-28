7122c884a5a8be2c76540be822625e8f1f131c14
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2020-02-28 11:14:00 -0800

    MJH:  added logic to only run code if the record status is blank or Renewal Fee Due

--- SCRIPTS/EVENT/PRA;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
+++ SCRIPTS/EVENT/PRA;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
@@ -1,64 +1,64 @@
 try{
 	if(balanceDue<=0){
-		if (getAppStatus() != "Submitted"){
+		if (matches(getAppStatus(), null, "", undefined, "Renewal Fee Due")){
 			updateAppStatus("Submitted", "Updated via PRA:LICENSES/CULTIVATOR/*/Renewal.");
-		}
-		if(!isTaskComplete("Annual Renewal Review") && !isTaskComplete("Provisional Renewal Review")){
-			if (AInfo["License Issued Type"] == "Provisional") {
-				activateTask("Provisional Renewal Review");
-				deactivateTask("Annual Renewal Review");
-			}else{
-				activateTask("Annual Renewal Review");
-				deactivateTask("Provisional Renewal Review");
+
+			if(!isTaskComplete("Annual Renewal Review") && !isTaskComplete("Provisional Renewal Review")){
+				if (AInfo["License Issued Type"] == "Provisional") {
+					activateTask("Provisional Renewal Review");
+					deactivateTask("Annual Renewal Review");
+				}else{
+					activateTask("Annual Renewal Review");
+					deactivateTask("Provisional Renewal Review");
+				}
 			}
-		}
-		//	6316: Add cond If Parent record of Provisional license does not have science Amendment with Status of "Approved for Provisional Renewl" year of last renewal
-		if (AInfo['License Issued Type'] == "Provisional"){
-			var vLicenseID = getParentLicenseCapID(capId);
-			var vIDArray = String(vLicenseID).split("-");
-			var vLicenseID = aa.cap.getCapID(vIDArray[0],vIDArray[1],vIDArray[2]).getOutput();
-			var scienceArr = getChildren("Licenses/Cultivator/Amendment/Science",vLicenseID);
-			var issueDate = getAppSpecific("Valid From Date",vLicenseID);
-			var approvedRen = false;
-			if (scienceArr) {
-				if (scienceArr.length > 0) {
-					for (x in scienceArr){
-						var scienceCap = scienceArr[x];
-						var workflowResult = aa.workflow.getTasks(scienceCap);
-						if (workflowResult.getSuccess()){
-							wfObj = workflowResult.getOutput();		
-							for (i in wfObj) {
-								fTask = wfObj[i];
-								var status = fTask.getDisposition();
-								var taskDesc = fTask.getTaskDescription();
-								if(status != null && taskDesc != null && status.equals("Approved for Provisional Renewal")){
-									var taskDate = fTask.getStatusDate()
-									var taskDateMMDDYYYY = dateFormatted(taskDate.getMonth()+1, taskDate.getDate(), taskDate.getYear()+1900, "MM/DD/YYYY");
-									var issueDateObj = new Date(issueDate);
-									var taskDateObj = new Date(taskDateMMDDYYYY);
-									var thisLic = new licenseObject(null,vLicenseID);
-									var licExpDateObj = new Date(thisLic.b1ExpDate);
-									licExpDateObj.setFullYear(licExpDateObj.getFullYear() - 1);
-									var diffDays = parseInt((taskDateObj - licExpDateObj) / (1000 * 60 * 60 * 24));
-									if(diffDays >= 0){
-										approvedRen = true;
-									}
-								}	
-							}
-						}else {
-							logDebug("**ERROR: Failed to get workflow object: "+wfObj );
-						}
+			//	6316: Add cond If Parent record of Provisional license does not have science Amendment with Status of "Approved for Provisional Renewl" year of last renewal
+			if (AInfo['License Issued Type'] == "Provisional"){
+				var vLicenseID = getParentLicenseCapID(capId);
+				var vIDArray = String(vLicenseID).split("-");
+				var vLicenseID = aa.cap.getCapID(vIDArray[0],vIDArray[1],vIDArray[2]).getOutput();
+				var scienceArr = getChildren("Licenses/Cultivator/Amendment/Science",vLicenseID);
+				var issueDate = getAppSpecific("Valid From Date",vLicenseID);
+				var approvedRen = false;
+				if (scienceArr) {
+					if (scienceArr.length > 0) {
+						for (x in scienceArr){
+							var scienceCap = scienceArr[x];
+							var workflowResult = aa.workflow.getTasks(scienceCap);
+							if (workflowResult.getSuccess()){
+								wfObj = workflowResult.getOutput();		
+								for (i in wfObj) {
+									fTask = wfObj[i];
+									var status = fTask.getDisposition();
+									var taskDesc = fTask.getTaskDescription();
+									if(status != null && taskDesc != null && status.equals("Approved for Provisional Renewal")){
+										var taskDate = fTask.getStatusDate()
+										var taskDateMMDDYYYY = dateFormatted(taskDate.getMonth()+1, taskDate.getDate(), taskDate.getYear()+1900, "MM/DD/YYYY");
+										var issueDateObj = new Date(issueDate);
+										var taskDateObj = new Date(taskDateMMDDYYYY);
+										var thisLic = new licenseObject(null,vLicenseID);
+										var licExpDateObj = new Date(thisLic.b1ExpDate);
+										licExpDateObj.setFullYear(licExpDateObj.getFullYear() - 1);
+										var diffDays = parseInt((taskDateObj - licExpDateObj) / (1000 * 60 * 60 * 24));
+										if(diffDays >= 0){
+											approvedRen = true;
+										}
+									}	
+								}
+							}else {
+								logDebug("**ERROR: Failed to get workflow object: "+wfObj );
+							}	
+						}	
+					}	
+				}	
+				if (!approvedRen){
+					if	(!appHasCondition("Application Condition","Applied","Provisional Renewal Missing Science Amendment",null)){
+						addStdCondition("Application Condition", "Provisional Renewal Missing Science Amendment");
 					}
 				}
 			}
-			if (!approvedRen){
-				if	(!appHasCondition("Application Condition","Applied","Provisional Renewal Missing Science Amendment",null)){
-					addStdCondition("Application Condition", "Provisional Renewal Missing Science Amendment");
-				}
-			}
-		}
-	}
-	if(!publicUser) {
+		
+			if(!publicUser) {
 		// Check License Cases to see if renewal can be fast tracked
 				var licenseId = AInfo["License Number"];
 				var licId = aa.cap.getCapID(licenseId);
@@ -86,7 +86,7 @@ try{
 					}
 				}	
 				capId = holdId;
-			// Fast track license if qualified and fees paid
+		// Fast track license if qualified and fees paid
 				if(!caseReview && balanceDue <= 0) {	
 					var renewalCapProject;
 					var vExpDate;
@@ -95,40 +95,40 @@ try{
 					licAltId = licId.getCustomID();
 					altId = capId.getCustomID();
 					if (licId != null) {
-			// Get current expiration date.
+		// Get current expiration date.
 						vLicenseObj = new licenseObject(null, licId);
 						vExpDate = vLicenseObj.b1ExpDate;
 						vExpDate = new Date(vExpDate);
-			// Extend license expiration by 1 year
+		// Extend license expiration by 1 year
 						vNewExpDate = new Date(vExpDate.getFullYear() + 1, vExpDate.getMonth(), vExpDate.getDate());
-			// Update license expiration date
+		// Update license expiration date
 						logDebug("Updating Expiration Date to: " + vNewExpDate);
 						vLicenseObj.setExpiration(dateAdd(vNewExpDate,0));
-			// Set license record expiration and status to active
+		// Set license record expiration and status to active
 						vLicenseObj.setStatus("Active");
 						updateAppStatus("Active","License Renewed",licId);
-			// Update the Cultivation Type on the license record
+		// Update the Cultivation Type on the license record
 						if(AInfo["Designation Change"] == "Yes") {
 							editAppSpecific("Cultivator Type",AInfo["Designation Type"],licId);
 							editAppName(AInfo["Designation Type"] + " - " + AInfo["License Type"],licId);
 						}
-			//Set renewal to complete, used to prevent more than one renewal record for the same cycle
+		//Set renewal to complete, used to prevent more than one renewal record for the same cycle
 						renewalCapProject = getRenewalCapByParentCapIDForIncomplete(licId);
 						if (renewalCapProject != null) {
 							renewalCapProject.setStatus("Complete");
 							renewalCapProject.setRelationShip("R");  // move to related records
 							aa.cap.updateProject(renewalCapProject);
 						}
-			// Update the workflow on the Renewal record to approved
+		// Update the workflow on the Renewal record to approved
 						if (AInfo["License Issued Type"] == "Provisional") {
 							closeTask("Provisional Renewal Review","Approved","Renewal Fast Tracked","");
 						}else{
 							closeTask("Annual Renewal Review","Approved","Renewal Fast Tracked","");
 						}
-			//			updateAppStatus("Approved","Renewal Fast Tracked");
+		//				updateAppStatus("Approved","Renewal Fast Tracked");
 						editAppSpecific("Fast Track","CHECKED");
-					
-			//Run Official License Certificate and Annual/Provisional Renewal Approval Email and Set the DRP		
+				
+		//Run Official License Certificate and Annual/Provisional Renewal Approval Email and Set the DRP		
 						if (AInfo["License Issued Type"] == "Provisional")
 							var approvalLetter = "Provisional Renewal Approval";
 						else
@@ -146,9 +146,9 @@ try{
 						envParameters.put("contType","Designated Responsible Party");
 						envParameters.put("fromEmail","calcannabislicensing@cdfa.ca.gov");
 						aa.runAsyncScript(scriptName, envParameters);
-					
+							
 						var priContact = getContactObj(capId,"Designated Responsible Party");
-				// If DRP preference is Postal add license record to Annual/Provisional Renewal A set
+		// If DRP preference is Postal add license record to Annual/Provisional Renewal A set
 						if(priContact){
 							var priChannel =  lookup("CONTACT_PREFERRED_CHANNEL",""+ priContact.capContact.getPreferredChannel());
 							if(!matches(priChannel, "",null,"undefined", false)){
@@ -170,11 +170,13 @@ try{
 								}
 							}
 						}
-				// Add record to the CAT set
+			// Add record to the CAT set
 						addToCat(licId);
 					}
 				}
 			}	
+		}
+	}
 }catch(err){
 	logDebug("An error has occurred in PRA:LICENSES/CULTIVATOR/*/Renewal: Renewal Fees Paid: " + err.message);
 	logDebug(err.stack);
