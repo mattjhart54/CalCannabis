f96cf778742f2fc754adcabe095f95b463711670
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2020-03-11 12:48:25 -0700

    MJH Changed code to call addFee and invoiceFee instead of UpdateFee.  Added custom function addFee to the script.

--- SCRIPTS/BATCH/BATCH_LICENSE_EXPIRATION.js
+++ SCRIPTS/BATCH/BATCH_LICENSE_EXPIRATION.js
@@ -323,7 +323,9 @@ try{
 					holdId = capId;
 					capId = renCapId;
 					if (!feeExists(thisFee.feeCode)){
-						updateFee(thisFee.feeCode,"LIC_CC_REN", "FINAL", 1, "Y", "N");
+//						updateFee(thisFee.feeCode,"LIC_CC_REN", "FINAL", 1, "Y", "N");
+						addFeeT(thisFee.feeCode,"LIC_CC_REN", "FINAL", 1, "Y",capId);
+						invoiceFee(thisFee.feeCode,"FINAL");
 					}
 					capId = holdId;
 				}else{
@@ -459,3 +461,45 @@ try{
 	logDebug("ERROR: createExpirationSet: " + err.message + " In " + batchJobName);
 	logDebug("Stack: " + err.stack);
 }}
+function addFeeT(fcode, fsched, fperiod, fqty, finvoice) // Adds a single fee, optional argument: fCap
+{
+	// Updated Script will return feeSeq number or null if error encountered (SR5112)
+	var feeCap = capId;
+	var feeCapMessage = "";
+	var feeSeq_L = new Array(); // invoicing fee for CAP in args
+	var paymentPeriod_L = new Array(); // invoicing pay periods for CAP in args
+	var feeSeq = null;
+	if (arguments.length > 5) {
+		feeCap = arguments[5]; // use cap ID specified in args
+		feeCapMessage = " to specified CAP";
+	}
+
+	assessFeeResult = aa.finance.createFeeItem(feeCap, fsched, fcode, fperiod, fqty);
+	if (assessFeeResult.getSuccess()) {
+		feeSeq = assessFeeResult.getOutput();
+		logMessage("Successfully added Fee " + fcode + ", Qty " + fqty + feeCapMessage);
+		logDebug("The assessed fee Sequence Number " + feeSeq + feeCapMessage);
+
+		if (finvoice == "Y" && arguments.length == 5) // use current CAP
+		{
+			feeSeqList.push(feeSeq);
+			paymentPeriodList.push(fperiod);
+		}
+		if (finvoice == "Y" && arguments.length > 5) // use CAP in args
+		{
+			feeSeq_L.push(feeSeq);
+			paymentPeriod_L.push(fperiod);
+			var invoiceResult_L = aa.finance.createInvoice(feeCap, feeSeq_L, paymentPeriod_L);
+			if (invoiceResult_L.getSuccess())
+				logMessage("Invoicing assessed fee items" + feeCapMessage + " is successful.");
+			else
+				logDebug("**ERROR: Invoicing the fee items assessed" + feeCapMessage + " was not successful.  Reason: " + invoiceResult.getErrorMessage());
+		}
+//		updateFeeItemInvoiceFlag(feeSeq, finvoice);
+	} else {
+		logDebug("**ERROR: assessing fee (" + fcode + "): " + assessFeeResult.getErrorMessage());
+		feeSeq = null;
+	}
+	return feeSeq;
+
+}
