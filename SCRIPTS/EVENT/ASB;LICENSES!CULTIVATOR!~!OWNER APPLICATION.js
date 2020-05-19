a903e95c4ad4a8e49cb015e9983cf6b50189ed6d
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2020-05-19 08:29:44 -0700

    MJH:  Added code to check for owner contact brfore application can be submitted

--- SCRIPTS/EVENT/ASB;LICENSES!CULTIVATOR!~!OWNER APPLICATION.js
+++ SCRIPTS/EVENT/ASB;LICENSES!CULTIVATOR!~!OWNER APPLICATION.js
@@ -20,3 +20,28 @@ try{
 	logDebug(err.stack);
 	aa.sendMail(sysFromEmail, debugEmail, "", "A JavaScript Error occurred: ASB:Licenses/Cultivation/*/OwnerApplication: Doc check: " + startDate, "capId: " + capId + ": " + br + err.message + br + err.stack + br + currEnv);
 }
+try {
+	if(publicUser){
+		var cntOwn = false;
+		var envContactList = aa.env.getValue("ContactList");
+		logDebug("Contact list " + envContactList);
+		var capContactArray = envContactList.toArray();
+		if (capContactArray){
+			for (yy in capContactArray){
+				p = capContactArray[yy].getPeople();
+				cType = p.getContactType();
+				if(cType == "Owner"){ 
+					cntOwn=true;
+				}
+			}
+		}
+		if(!cntOwn) {
+			cancel=true;
+			showMessage=true;
+			comment("No required Owner contact has been entered on the application.  Please add before submitting the application");
+		}
+	}	
+} catch(err){
+	logDebug("An error has occurred in ASB;LICENSES!CULTIVATOR!~!OWNER APPLICATION.js: Check Owner contacts " + err.message);
+	logDebug(err.stack);
+}
