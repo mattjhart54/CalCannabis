79bbe885b6254b35085713d2700a1edf1854566b
Joe Espena <jmespena@gmail.com>
2020-08-11 15:15:20 -0700

    Add files via upload

--- SCRIPTS/EVENT/WTUA;ENFORCEMENT!CASE!TAG REQUEST!NA.js
+++ SCRIPTS/EVENT/WTUA;ENFORCEMENT!CASE!TAG REQUEST!NA.js
@@ -3,11 +3,11 @@ try {
 		if (typeof(TAQREQUEST) == "object") {
 			for(r in TAQREQUEST) {
 				licId = TAQREQUEST[r]["License Number"];
-				if (matches(TAQREQUEST["Increase Type"],"Permitted")) {
+				if (matches(TAQREQUEST[r]["Increase Type"],"Permitted")) {
 					editAppSpecific(licId,"Current Plant Tags",TAQREQUEST[r]["New Plant Tags"]);
 					editAppSpecific(licId,"Current Package Tags",TAQREQUEST[r]["New Package Tags"]);
 				}
-				if (matches(TAQREQUEST["Increase Type"],"One-Time")) {
+				if (matches(TAQREQUEST[r]["Increase Type"],"One-Time")) {
 					editAppSpecific(licId,"One-Time Increase Plant Tags",TAQREQUEST[r]["One-Time Plant Tags"]);
 					editAppSpecific(licId,"One-Time Increase Package Tags",TAQREQUEST[r]["One-Time Package Tags"]);
 					editAppSpecific(licId,"One-Time Increase Expiration Date",TAQREQUEST[r]["One-Time Expiration Date"]);
