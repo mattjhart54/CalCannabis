5336b585e17480b4c1c3f0ca810c53bfa5819013
Joe Espena <jmespena@gmail.com>
2020-08-12 09:58:02 -0700

    Update WTUA;ENFORCEMENT!CASE!TAG REQUEST!NA.js

--- SCRIPTS/EVENT/WTUA;ENFORCEMENT!CASE!TAG REQUEST!NA.js
+++ SCRIPTS/EVENT/WTUA;ENFORCEMENT!CASE!TAG REQUEST!NA.js
@@ -1,17 +1,18 @@
 try {
 	if(wfStatus == "Approved") {
-		if (typeof(TAQREQUEST) == "object") {
-			for(r in TAQREQUEST) {
-				licenseNumber = TAQREQUEST[r]["License Number"];
+		var tagRequest = loadASITable("TAQ REQUEST",capId); 
+		if (typeof(tagRequest) == "object") {
+			for(r in tagRequest) {
+				licenseNumber = tagRequest[r]["License Number"];
 				licenseCapId = aa.cap.getCapID(licenseNumber);
-				if (matches(TAQREQUEST[r]["Increase Type"],"Permitted")) {
-					editAppSpecific("Current Plant Tags",TAQREQUEST[r]["New Plant Tags"],licenseCapId);
-					editAppSpecific("Current Package Tags",TAQREQUEST[r]["New Package Tags"],licenseCapId);
+				if (matches(tagRequest[r]["Increase Type"],"Permitted")) {
+					editAppSpecific("Current Plant Tags",tagRequest[r]["New Plant Tags"],licenseCapId);
+					editAppSpecific("Current Package Tags",tagRequest[r]["New Package Tags"],licenseCapId);
 				}
-				if (matches(TAQREQUEST[r]["Increase Type"],"One-Time")) {
-					editAppSpecific("One-Time Increase Plant Tags",TAQREQUEST[r]["One-Time Plant Tags"],licenseCapId);
-					editAppSpecific("One-Time Increase Package Tags",TAQREQUEST[r]["One-Time Package Tags"],licenseCapId);
-					editAppSpecific("One-Time Increase Expiration Date",TAQREQUEST[r]["One-Time Expiration Date"],licenseCapId);
+				if (matches(tagRequest[r]["Increase Type"],"One-Time")) {
+					editAppSpecific("One-Time Increase Plant Tags",tagRequest[r]["One-Time Plant Tags"],licenseCapId);
+					editAppSpecific("One-Time Increase Package Tags",tagRequest[r]["One-Time Package Tags"],licenseCapId);
+					editAppSpecific("One-Time Increase Expiration Date",tagRequest[r]["One-Time Expiration Date"],licenseCapId);
 				}
 				// link the tag request record as a child to the license record
 			}	
