c63043789141a6826ccbfee3c412bc46b37a0c39
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2020-09-02 15:16:24 -0700

    MJH: Changed script to start by getting invoices then feeitems

--- SCRIPTS/EVENT/IFA;LICENSES!CULTIVATOR!~!RENEWAL.js
+++ SCRIPTS/EVENT/IFA;LICENSES!CULTIVATOR!~!RENEWAL.js
@@ -2,33 +2,26 @@ try {
 	var feeDesc = AInfo["License Type"] + " - Late Fee";
 	var thisFee = getFeeDefByDesc("LIC_CC_REN", feeDesc);
 	if(thisFee){
-		var feeResult = aa.fee.getFeeItems(capId, thisFee.feeCode, null);
-		if (feeResult.getSuccess()) {
-			var feeObjArr = feeResult.getOutput();
-		} else {
-			logDebug("**ERROR: getting fee items: " + capContResult.getErrorMessage());
-		}
-		for(f in feeObjArr) {
-			feeSeq = feeObjArr[f].getFeeSeqNbr();
-			feeStatus = feeObjArr[f].getFeeitemStatus();
-			feeItemDesc = feeObjArr[f].getFeeDescription();
-		//	logDebug("fee status " + feeStatus + " fee Desc " + feeItemDesc + " " + feeDesc); 
-			if (feeStatus == "CREDITED" && feeDesc == feeItemDesc) {
-				var invoiceResult = aa.finance.getFeeItemInvoiceByFeeNbr(capId, feeSeq, null);
-				if (invoiceResult.getSuccess()) {
-					var invoiceItem = invoiceResult.getOutput();
-					for (i in invoiceItem) {
-						iDate = invoiceItem[i].getApplyDate();
-						iStatus = invoiceItem[i].getFeeitemStatus();
-						var feeDesc = AInfo["License Type"] + " - Late Fee";
+		var invoiceResult = aa.finance.getInvoiceByCapID(capId,null);
+		if (invoiceResult.getSuccess()) {
+			var invoiceItem = invoiceResult.getOutput();
+			for (i in invoiceItem) {
+	//			describeObject(invoiceItem[i]);
+				iDate = new Date 
+				iDate.setTime(invoiceItem[i].getInvDate().getEpochMilliseconds());
+				iNbr = invoiceItem[i].getInvNbr();
+				var feeResult = aa.finance.getFeeItemInvoiceByInvoiceNbr(capId,iNbr,null);
+				if(feeResult.getSuccess()) {
+					feeItem = feeResult.getOutput();
+					for(f in feeItem) {
+						var feeStatus = feeItem[f].getFeeitemStatus();
+						var feeItemDesc = feeItem[f].getFeeDescription();
 						invDate =  dateFormatted(iDate.getMonth()+1,iDate.getDate(),iDate.getYear(),"YY-MM-DD");
 						cDate = new Date();
 						curDate =  dateFormatted(cDate.getMonth()+1,cDate.getDate(),cDate.getYear(),"YY-MM-DD");
-						logDebug("Inv " + invoiceItem[i].getInvoiceNbr() + " status " + iStatus + "inv date " + invDate + " cur date " + curDate);
-						if(curDate == invDate && iStatus == "CREDITED" ) {
-				//			var rParams = aa.util.newHashMap(); 
-				//			rParams.put("altId", capId.getCustomID());
-				//			emailRptContact("IFA", "LCA_BALANCE_DUE", "Balance Due Report", true,"","","Designated Responsible Party","altId",capId.getCustomID());
+						logDebug("Inv " + iNbr + " status " + feeStatus + "inv date " + invDate + " cur date " + curDate);
+						if(curDate == invDate && feeStatus == "CREDITED" && feeDesc == feeItemDesc) {
+							logDebug("Run Report");
 							var scriptName = "asyncRunBalanceDueRpt";
 							var envParameters = aa.util.newHashMap();
 							envParameters.put("recNum",capId.getCustomID()); 
@@ -38,8 +31,8 @@ try {
 							envParameters.put("fromEmail","calcannabislicensing@cdfa.ca.gov");
 							aa.runAsyncScript(scriptName, envParameters);
 						}
-					} 
-				}
+					}
+				} 
 			}
 		}
 	}
