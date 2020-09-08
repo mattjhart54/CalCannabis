048012469d3df8771b0c6ba8cc4caf9ec7244f16
jshear28 <57968271+jshear28@users.noreply.github.com>
2020-09-08 09:58:52 -0400

    remove debugs

--- SCRIPTS/EVENT/FAA;LICENSES!CULTIVATOR!~!RENEWAL.js
+++ SCRIPTS/EVENT/FAA;LICENSES!CULTIVATOR!~!RENEWAL.js
@@ -10,9 +10,7 @@ try{
 			cDate = new Date();
 			curDateFormatted =  dateFormatted(cDate.getMonth()+1,cDate.getDate(),cDate.getFullYear(),"YY-MM-DD");
 			feeDateFormatted = dateFormatted(feeDate.getMonth(),feeDate.getDayOfMonth(),feeDate.getYear(),"YY-MM-DD");
-			logDebug("feeStatus: " + feeStatus + " feeDateFormatted: " + feeDateFormatted);
-			if(curDateFormatted == feeDateFormatted && feeStatus == "INVOICED"){
-				logDebug("within");
+			if(curDateFormatted == feeDateFormatted && feeStatus == "INVOICED"){;
 				var scriptName = "asyncRunBalanceDueRpt";
 				var envParameters = aa.util.newHashMap();
 				envParameters.put("altId",capId.getCustomID()); 
