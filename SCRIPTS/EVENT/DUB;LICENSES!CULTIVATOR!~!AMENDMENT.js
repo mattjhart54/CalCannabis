42ed64aaf34c6379f9a7207ce363e822e1f75e1c
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2020-09-24 13:46:12 -0700

    MJH Scriot to prevent documents from being uploaded in ACA if the deficienccy record or its parent record is Disqualified.

---
+++ SCRIPTS/EVENT/DUB;LICENSES!CULTIVATOR!~!AMENDMENT.js
@@ -0,0 +1,16 @@
+try{
+	if(documentUploadedFrom == "ACA"){
+		pId = getParent();
+		pCap = aa.cap.getCap(pId).getOutput();
+		pStatus = pCap.getCapStatus();
+		if(pStatus == "Disquaified" || capStatus == "Disqualified") {
+			cancel = true;		
+			showMessage = true;
+			comment("This application has been placed on hold. Please contact CalCannabis Cultivation Licensing by calling (833) CALGROW (225-4769) or by sending an email to calcannabis@cdfa.ca.gov.");
+		}
+	}
+}catch(err){
+	logDebug("An error has occurred in DUB:LICENSES/CULTIVATOR/AMENDMENT/SCIENCE: No Documents: " + err.message);
+	logDebug(err.stack);
+	aa.sendMail(sysFromEmail, debugEmail, "", "An error has occurred in DUB:LICENSES/CULTIVATOR/*/AMENDMENT: Amendment Disqualified: "+ startDate, capId + br+ err.message+ br+ err.stack);
+}
