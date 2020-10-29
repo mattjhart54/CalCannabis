6762fdc5c636f7bf06eee55cf334d6e19f642f9b
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2020-10-29 09:43:12 -0700

    Update ASIUA;LICENSES!CULTIVATOR!~!APPLICATION.js

--- SCRIPTS/EVENT/ASIUA;LICENSES!CULTIVATOR!~!APPLICATION.js
+++ SCRIPTS/EVENT/ASIUA;LICENSES!CULTIVATOR!~!APPLICATION.js
@@ -132,3 +132,33 @@ try{
 	logDebug(err.stack);
 	aa.sendMail(sysFromEmail, debugEmail, "", "A JavaScript Error occurred: ASIUA:Licenses/Cultivation/*/Application: Send Local Auth: " + startDate, "capId: " + capId + ": " + err.message + ": " + err.stack);
 }
+try {
+	if(LAKEANDSTREAMBEDALTERATION.length>0) {
+		var tblLSA = loadASITable("LAKE AND STREAMBED ALTERATION");
+		var addRow = false;
+		for(r in LAKEANDSTREAMBEDALTERATION) {
+			if(LAKEANDSTREAMBEDALTERATION[r]["New Row"] == "CHECKED") {
+				addRow = true;
+				tblLSA[r]["New Row"] = "UNCHECKED";
+				thisLSA = LAKEANDSTREAMBEDALTERATION[r];
+				thisLSA["New Row"] = "UNCHECKED";
+				thisLSA["Covered Activity"] = "";
+				thisLSA["LSA Detail Latitude"] = "";
+				thisLSA["LSA Detail Longitude"] = "";
+				thisLSA["Covered Activity"] = "";
+				thisLSA["APN"] = "";
+				thisLSA["APN Latitude"] = "";
+				thisLSA["APN Longitude"] = "";
+				thisLSA["Adjacent APN"] = "";	
+				tblLSA.push(thisLSA);
+			}
+		}
+		if(addRow) {
+			removeASITable("LAKE AND STREAMBED ALTERATION");
+			addASITable("LAKE AND STREAMBED ALTERATION",tblLSA);
+		}
+	}
+}catch (err){
+	logDebug("A JavaScript Error occurred: ASIUA:Licenses/Cultivation/*/Application: Update LSA table: " + err.message);
+	logDebug(err.stack);
+}
