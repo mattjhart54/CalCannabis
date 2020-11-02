35867a6a764dd146cf30cd7337e04323599aa1b6
jshear28 <57968271+jshear28@users.noreply.github.com>
2020-11-02 18:30:16 -0500

    testing

--- SCRIPTS/EVENT/DUB;LICENSES!CULTIVATOR!LICENSE!LICENSE.js
+++ SCRIPTS/EVENT/DUB;LICENSES!CULTIVATOR!LICENSE!LICENSE.js
@@ -1,8 +1,10 @@
 try{
-	if(documentUploadedFrom == "ACA"){
+	//if(documentUploadedFrom == "ACA"){
 		var cancelUpload = false;
 		for(var index = 0; index < documentModelArray.size(); index++) {
+			logDebug("Cat: " + String(documentModelArray.get(index).getDocCategory());
 			if (String(documentModelArray.get(index).getDocCategory()) =! "Delegate Contact"){
+				logDebug("within");
 				cancelUpload = true;
 			}
 		}
@@ -11,7 +13,7 @@ try{
 			showMessage = true;
 			comment("To upload additional documents, please submit a new amendment. For further questions please contact CalCannabis at 1-833-CALGROW (225-4769) or by sending an email to calcannabis@cdfa.ca.gov.");
 		}
-	}
+	//}
 }catch(err){
 	logDebug("An error has occurred in DUB:LICENSES/CULTIVATOR/LICENSE/LICENSE: No Documents: " + err.message);
 	logDebug(err.stack);
