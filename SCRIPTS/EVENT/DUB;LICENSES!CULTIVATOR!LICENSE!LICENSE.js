16deb0f6b53607a2b4a64927ed6175eec29a85da
jshear28 <57968271+jshear28@users.noreply.github.com>
2020-11-02 18:03:32 -0500

    testing

--- SCRIPTS/EVENT/DUB;LICENSES!CULTIVATOR!LICENSE!LICENSE.js
+++ SCRIPTS/EVENT/DUB;LICENSES!CULTIVATOR!LICENSE!LICENSE.js
@@ -1,13 +1,14 @@
 try{
-	if(documentUploadedFrom == "ACA"){
+	//if(documentUploadedFrom == "ACA"){
 		for (x in documentModelArray){
+			logDebug(documentModelArray[x].getDocType());
 			if (documentModelArray[x].getDocType() =! "Delegate Contact"){
 				cancel = true;		
 				showMessage = true;
 				comment("To upload additional documents, please submit a new amendment. For further questions please contact CalCannabis at 1-833-CALGROW (225-4769) or by sending an email to calcannabis@cdfa.ca.gov.");
 			}
 		}
-	}
+	//}
 }catch(err){
 	logDebug("An error has occurred in DUB:LICENSES/CULTIVATOR/LICENSE/LICENSE: No Documents: " + err.message);
 	logDebug(err.stack);
