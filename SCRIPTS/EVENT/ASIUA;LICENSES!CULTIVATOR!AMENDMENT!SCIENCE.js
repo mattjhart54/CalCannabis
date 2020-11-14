4616f3afcff3e9fcefbbfdc184927cf5bf9c99a3
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2020-11-13 18:21:32 -0800

    Create ASIUA;LICENSES!CULTIVATOR!AMENDMENT!SCIENCE.js
    MJH Added code to add row to Lake and Streambed table

---
+++ SCRIPTS/EVENT/ASIUA;LICENSES!CULTIVATOR!AMENDMENT!SCIENCE.js
@@ -0,0 +1,30 @@
+try {
+	if(LAKEANDSTREAMBEDALTERATION.length>0) {
+		var tblLSA = loadASITable("LAKE AND STREAMBED ALTERATION");
+		var addRow = false;
+		for(r in LAKEANDSTREAMBEDALTERATION) {
+			if(LAKEANDSTREAMBEDALTERATION[r]["New Row"] == "CHECKED") {
+				addRow = true;
+				tblLSA[r]["New Row"] = "UNCHECKED";
+				thisLSA = LAKEANDSTREAMBEDALTERATION[r];
+				thisLSA["New Row"] = "UNCHECKED";
+				thisLSA["Covered Activity"] = "";
+				thisLSA["LSA Detail Latitude"] = "";
+				thisLSA["LSA Detail Longitude"] = "";
+				thisLSA["Covered Activity"] = "";
+				thisLSA["APN"] = "";
+				thisLSA["APN Latitude"] = "";
+				thisLSA["APN Longitude"] = "";
+				thisLSA["Adjacent APN"] = "";	
+				tblLSA.push(thisLSA);
+			}
+		}
+		if(addRow) {
+			removeASITable("LAKE AND STREAMBED ALTERATION");
+			addASITable("LAKE AND STREAMBED ALTERATION",tblLSA);
+		}
+	}
+}catch (err){
+	logDebug("A JavaScript Error occurred: ASIUA:Licenses/Cultivation/*/Application: Update LSA table: " + err.message);
+	logDebug(err.stack);
+}
