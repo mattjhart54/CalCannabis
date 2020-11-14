4b2312770ae0a856d194509c296aec7d3fd88156
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2020-11-13 18:34:03 -0800

    Update ASIUA;LICENSES!CULTIVATOR!~!LICENSE.js
    MJH Removed code to edit app type alias

--- SCRIPTS/EVENT/ASIUA;LICENSES!CULTIVATOR!~!LICENSE.js
+++ SCRIPTS/EVENT/ASIUA;LICENSES!CULTIVATOR!~!LICENSE.js
@@ -1,25 +1,4 @@
 try {
-	if(appTypeArray[2] != AInfo["Cultivator Type"]) {
-		if(AInfo["Cultivator Type"] == "Medical") {
-			typeUpdated= editAppTypeAlias("Medicinal Cannabis Cultivator License","Medical");
-			if(typeUpdated)
-				logDebug("App Type Alias Succesfully Changed");
-			else
-				logDebug("App Type Alias Update Failed");
-		}
-		else {
-			typeUpdated= editAppTypeAlias("Adult-Use Cannabis Cultivator License","Adult Use");
-			if(typeUpdated)
-				logDebug("App Type Alias Succesfully Changed");
-			else
-				logDebug("App Type Alias Update Failed");
-		}
-	}
-} catch(err){
-	logDebug("An error has occurred in ASIU:LICENSES/CULTIVATOR/*/LICENSE: Update App Type " + err.message);
-	logDebug(err.stack);
-}
-try {
 	if(LAKEANDSTREAMBEDALTERATION.length>0) {
 		var tblLSA = loadASITable("LAKE AND STREAMBED ALTERATION");
 		var addRow = false;
