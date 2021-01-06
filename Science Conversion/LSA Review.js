925e59b6c7fb6d0db0362f141627a7b471bc199b
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2021-01-06 11:36:16 -0800

    Create LSA Review.js

---
+++ Science Conversion/LSA Review.js
@@ -0,0 +1,133 @@
+/*------------------------------------------------------------------------------------------------------/
+| Program: BATCH_LSA Review Update
+| Client:  CDFA_CalCannabis
+|
+| Version 1.0 - Base Version. 
+|
+|  
+/------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------------------------------------/
+|
+| START: USER CONFIGURABLE PARAMETERS
+|
+/------------------------------------------------------------------------------------------------------*/
+var emailText = "";
+var errLog = "";
+var debugText = "";
+var showDebug = false;	
+var showMessage = false;
+var message = "";
+var maxSeconds = 7 * 60;
+var br = "<br>";
+
+/*------------------------------------------------------------------------------------------------------/
+|
+| END: USER CONFIGURABLE PARAMETERS
+|
+/------------------------------------------------------------------------------------------------------*/
+sysDate = aa.date.getCurrentDate();
+batchJobResult = aa.batchJob.getJobID()
+batchJobName = "" + aa.env.getValue("BatchJobName");
+wfObjArray = null;
+
+eval(getMasterScriptText("INCLUDES_ACCELA_FUNCTIONS"));
+eval(getScriptText("INCLUDES_BATCH"));
+eval(getMasterScriptText("INCLUDES_CUSTOM"));
+eval(getScriptText("LSA REVIEW DATA"));
+
+override = "function logDebug(dstr){ if(showDebug) { aa.print(dstr); emailText+= dstr + \"<br>\"; } }";
+eval(override);
+
+function getScriptText(vScriptName){
+	vScriptName = vScriptName.toUpperCase();
+	var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();
+	var emseScript = emseBiz.getScriptByPK(aa.getServiceProviderCode(),vScriptName,"ADMIN");
+	return emseScript.getScriptText() + "";
+}
+
+function getMasterScriptText(vScriptName) {
+    vScriptName = vScriptName.toUpperCase();
+    var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();
+    var emseScript = emseBiz.getMasterScript(aa.getServiceProviderCode(), vScriptName);
+    return emseScript.getScriptText() + "";
+}
+
+showDebug = true;
+batchJobID = 0;
+if (batchJobResult.getSuccess())
+  {
+  batchJobID = batchJobResult.getOutput();
+  logDebug("Batch Job " + batchJobName + " Job ID is " + batchJobID);
+  }
+else
+  logDebug("Batch job ID not found " + batchJobResult.getErrorMessage());
+
+var emailAddress = "mhart@trustvip.com";
+var sysFromEmail = "calcannabislicensing@cdfa.ca.gov";
+var useAppSpecificGroupName = false;
+var startDate = new Date();
+var startTime = startDate.getTime(); // Start timer
+var currentUserID = "ADMIN";
+var systemUserObj = aa.person.getUser("ADMIN").getOutput();
+
+var AInfo = new Array();
+var LSA = LSAReviewData();
+/*------------------------------------------------------------------------------------------------------/
+| <===========Main=Loop================>
+|
+/-----------------------------------------------------------------------------------------------------*/
+
+logDebug("Start of Job");
+
+try {
+	mainProcess();
+	logDebug("End of Job: Elapsed Time : " + elapsed() + " Seconds");
+	if (emailAddress.length) {
+		aa.sendMail(sysFromEmail, emailAddress, "", batchJobName + " Results", emailText);
+		if(errLog != "") {
+			aa.sendMail(sysFromEmail, emailAddress, "", batchJobName + " Errors", errLog);
+		}
+	}
+} catch (err) {
+	logDebug("ERROR: BATCH_LSA Review Update: " + err.message + " In " + batchJobName + " Line " + err.lineNumber);
+	logDebug("Stack: " + err.stack);
+}
+
+/*------------------------------------------------------------------------------------------------------/
+| <===========END=Main=Loop================>
+/-----------------------------------------------------------------------------------------------------*/
+
+function mainProcess() {
+try{ 
+	var recCnt = 0;
+	var rejCnt = 0;
+	for (i in LSA) {
+		if(!matches(LSA[i]["License_Number"],null,"",undefined)) {
+			capId =aa.cap.getCapID(LSA[i]["License_Number"]).getOutput();
+			if (!capId) {
+				logDebug("LSA Review row for " + LSA[i]["License_Number"] + " not processed as License record not found");
+				rejCnt++;
+				continue;
+			}
+		}
+		else {
+			capId =aa.cap.getCapID(LSA[i]["Application_Number"]).getOutput();
+			if (!capId) {
+				logDebug("LSA Review row for " + LSA[i]["Application_Number"] + " not processed as Application record not found");
+				rejCnt++;
+				continue;
+			}
+		}
+		recCnt++;
+		editAppSpecific("LSA Review Status",LSA[i]["CDFW_Review_Status"]);
+		editAppSpecific("APN Matches Premises",LSA[i]["APN_Matches_Premises"]);
+		editAppSpecific("APN_Matches_Adjacent_Parcel",LSA[i]["APN_Matches_Adjacent_Parcel"]);
+		editAppSpecific("Notes",LSA[i]["Notes"]);
+	}
+	logDebug("Total Records Processed : " + LSA.length);
+	logDebug("Total Records Rejected: " + rejCnt);
+	logDebug("Total Records Converted: " + recCnt);
+}catch (err){
+	logDebug("ERROR: BATCH_LSA: " + err.message + " In " + batchJobName);
+	logDebug("Stack: " + err.stack);
+}}
