b941b821c5ecffbed31fff22be569236914ddf4a
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2021-02-16 14:33:22 -0800

    Update WTUA;LICENSES!CULTIVATOR!AMENDMENT!SCIENCE.js

--- SCRIPTS/EVENT/WTUA;LICENSES!CULTIVATOR!AMENDMENT!SCIENCE.js
+++ SCRIPTS/EVENT/WTUA;LICENSES!CULTIVATOR!AMENDMENT!SCIENCE.js
@@ -69,6 +69,7 @@ try {
 		}
 		ignoreTableArray = [];
 		removeASITable("APN SPATIAL INFORMATION",parentCapId);
+		removeASITable("LAKE AND STREAMBED ALTERATION",parentCapId);
 		if (matches(getAppSpecific("Small Retail Water Supplier Review Status-NEW"),"Complete","N/A")){
 			removeASITable("SMALL RETAIL WATER SUPPLIERS",parentCapId);
 		}else{
@@ -93,11 +94,6 @@ try {
 			removeASITable("WATER RIGHTS",parentCapId);
 		}else{
 			ignoreTableArray.push("WATER RIGHTS");
-		}
-		if (matches(getAppSpecific("LSA Review Status-NEW"),"Complete","N/A")){
-			removeASITable("LAKE AND STREAMBED ALTERATION",parentCapId);
-		}else{
-			ignoreTableArray.push("LAKE AND STREAMBED ALTERATION");
 		}
 		if (typeof(PREMISESADDRESSES) == "object"){
 			if(PREMISESADDRESSES.length > 0){
