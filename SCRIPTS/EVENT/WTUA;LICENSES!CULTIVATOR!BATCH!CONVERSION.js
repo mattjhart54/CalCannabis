2ccb35b6211c0b24a517e0f6e83b729beaef7bd2
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2022-06-15 11:06:39 -0700

    Create WTUA;LICENSES!CULTIVATOR!BATCH!CONVERSION.js

---
+++ SCRIPTS/EVENT/WTUA;LICENSES!CULTIVATOR!BATCH!CONVERSION.js
@@ -0,0 +1,17 @@
+try {
+
+  if(wfStatus == "Processed"){
+  		if(AInfo["Pause Renewal"] == "Check to stop renewal notices") 
+  			fieldUpdate = "CHECKED"
+	  	else
+	  		fieldUpdate = "UNCHECKED";
+	  for(i in LICENSES) {
+      licRec = LICENSES[i]["License Number"];
+      licRecId = aa.cap.getCapID(licRec).getOutput();
+	  	editAppSpecific("Pause Renewal Notices for License Conversion",fieldUpdate,licRecId)
+	  }
+  }
+}catch(err){
+	logDebug("An error has occurred in WTUA:LICENSES/CULTIVATOR/Batch/Conversion: " + err.message);
+	logDebug(err.stack);
+}
