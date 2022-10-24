a1df1efccb2daf3bbc915f1a2016ac37f57a9e78
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2022-10-24 16:34:55 -0700

    Update WTUB;LICENSES!CULTIVATOR~CONVERSION REQUEST!~.js

--- SCRIPTS/EVENT/WTUB;LICENSES!CULTIVATOR~CONVERSION REQUEST!~.js
+++ SCRIPTS/EVENT/WTUB;LICENSES!CULTIVATOR~CONVERSION REQUEST!~.js
@@ -1,7 +1,7 @@
 try {
 	var licType = AInfo["Proposed License Type"];		
 	if(licType.substring(0,5) == "Large") {
-		if(matches(AInfo["Canopy SF-NEW"],null,"",undefined) && matches(AInfo["Canopy SF],null,"",undefined)) {
+		if(matches(AInfo["Canopy SF-NEW"],null,"",undefined) && matches(AInfo["Canopy SF"],null,"",undefined)) {
 			showMessage = true;
 			cancel = true;
 			comment("There must be a value entered in the Canopy Square Footage field to process this conversion request")
