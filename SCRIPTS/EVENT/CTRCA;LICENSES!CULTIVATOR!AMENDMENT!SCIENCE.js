5e73fa0b3dc1a30bc7b5ad63036e4de5c40f7fdf
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2022-11-16 08:17:22 -0800

    Update CTRCA;LICENSES!CULTIVATOR!AMENDMENT!SCIENCE.js

--- SCRIPTS/EVENT/CTRCA;LICENSES!CULTIVATOR!AMENDMENT!SCIENCE.js
+++ SCRIPTS/EVENT/CTRCA;LICENSES!CULTIVATOR!AMENDMENT!SCIENCE.js
@@ -50,7 +50,7 @@ try {
 	PInfo = new Array;
 	loadAppSpecific(PInfo);
 	capId = holdId;
-	editAppSpecific("Submitted License Type" , AInfo["Proposed License Type");
+	editAppSpecific("Submitted License Type" , AInfo["Proposed License Type"]);
 	editAppSpecific("License Issued Type", PInfo["License Issued Type"]);
 	editAppSpecific("Premise State",PInfo["Premise State"]);
 	editAppSpecific("Medium Validation",PInfo["Medium Validation"]);
