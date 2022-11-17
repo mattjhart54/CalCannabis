1094a7b50b4320516a6f2d928f4d5b9d54108963
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2022-11-17 13:29:30 -0800

    Update ASA;LICENSES!CULTIVATOR!CONVERSION REQUEST!~.js

--- SCRIPTS/EVENT/ASA;LICENSES!CULTIVATOR!CONVERSION REQUEST!~.js
+++ SCRIPTS/EVENT/ASA;LICENSES!CULTIVATOR!CONVERSION REQUEST!~.js
@@ -30,7 +30,7 @@ try {
 			vLicenseObj = new licenseObject(null,capId);
 			vCurrExpDate = vLicenseObj.b1ExpDate;
 			capId = holdId;
-			
+			editAppSPecific("Submitted License Type", AInfo["Proposed License Type"])
 			editAppSpecific("License Expiration Date", vCurrExpDate);
 			editAppSpecific("Legal Business Name", PInfo["Legal Business Name"]);
 			editAppSpecific("License Issued Type", PInfo["License Issued Type"]);
