c65c4b5522d9ad5d66ca533ddeadfd8a16a65ff7
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2023-01-17 09:39:29 -0800

    7337

---
+++ SCRIPTS/BATCH/BATCH_DELETE_OWNER_ROW.js
@@ -0,0 +1,132 @@
+/*------------------------------------------------------------------------------------------------------/
+| Program: PERMIT_ONEYEAR_INSPECTION.js  Trigger: Batch
+| Client:
+|
+| Version 1.0 - Base Version. 03/05/2013 - Jaime Shear
+|
+/------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------------------------------------/
+|
+| START: USER CONFIGURABLE PARAMETERS
+|
+/------------------------------------------------------------------------------------------------------*/
+emailText = "";
+maxSeconds = 4.5 * 60;		// number of seconds allowed for batch processing, usually < 5*60
+message = "";
+br = "<br>";
+/*------------------------------------------------------------------------------------------------------/
+| BEGIN Includes
+/------------------------------------------------------------------------------------------------------*/
+SCRIPT_VERSION = 2.0
+
+eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS"));
+eval(getScriptText("INCLUDES_CUSTOM"));
+eval(getScriptText("INCLUDES_ACCELA_GLOBALS"));
+eval(getScriptText("INCLUDES_BATCH"));
+
+
+function getScriptText(vScriptName) {
+vScriptName = vScriptName.toUpperCase();
+var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();
+var emseScript = emseBiz.getScriptByPK(aa.getServiceProviderCode(), vScriptName, "ADMIN");
+return emseScript.getScriptText() + "";
+}
+
+/*------------------------------------------------------------------------------------------------------/
+|
+| END: USER CONFIGURABLE PARAMETERS
+|
+/------------------------------------------------------------------------------------------------------*/
+showDebug = "Y";
+
+sysDate = aa.date.getCurrentDate();
+batchJobResult = aa.batchJob.getJobID()
+batchJobName = "" + aa.env.getValue("BatchJobName");
+wfObjArray = null;
+
+
+batchJobID = 0;
+if (batchJobResult.getSuccess())
+  {
+  batchJobID = batchJobResult.getOutput();
+  logDebug("Batch Job " + batchJobName + " Job ID is " + batchJobID);
+  }
+else
+  logDebug("Batch job ID not found " + batchJobResult.getErrorMessage());
+
+
+/*----------------------------------------------------------------------------------------------------/
+|
+| Start: BATCH PARAMETERS
+|
+/------------------------------------------------------------------------------------------------------*/
+
+var altID = getParam("recordNumber");							// Hardcoded dates.   Use for testing only
+var eMail = getParam("emailAddress");								// ""
+var firstName = getParam("emailAddress");
+var lastName = getParam("emailAddress");
+
+
+/*----------------------------------------------------------------------------------------------------/
+|
+| End: BATCH PARAMETERS
+|
+/------------------------------------------------------------------------------------------------------*/
+var startDate = new Date();
+var timeExpired = false;
+
+
+var mailFrom = lookup("ACA_EMAIL_TO_AND_FROM_SETTING","RENEW_LICENSE_AUTO_ISSUANCE_MAILFROM");
+var acaSite = lookup("ACA_CONFIGS","ACA_SITE");
+acaSite = acaSite.substr(0,acaSite.toUpperCase().indexOf("/ADMIN"));
+
+var systemUserObj = aa.person.getUser("ADMIN").getOutput();
+
+var AInfo = new Array();
+
+if (appGroup == "*")
+	appGroup = "";
+
+if (appTypeType == "*")
+	appTypeType = "";
+
+if (appSubtype == "*")
+	appSubtype = "";
+
+if (appCategory == "*")
+	appCategory = "";
+
+//logDebug("Historical Date Check: " + dateCheck);
+
+/*------------------------------------------------------------------------------------------------------/
+| <===========Main=Loop================>
+|
+/-----------------------------------------------------------------------------------------------------*/
+
+logDebug("Start of Job");
+logDebug("********************************");
+
+if (!timeExpired) mainProcess();
+
+logDebug("End of Job: Elapsed Time : " + elapsed() + " Seconds");
+
+if (emailAddress.length)
+	aa.sendMail("noreply@accela.com", emailAddress, "", batchJobName + " Results", emailText);
+
+
+/*------------------------------------------------------------------------------------------------------/
+| <===========END=Main=Loop================>
+/-----------------------------------------------------------------------------------------------------*/
+function mainProcess() {	
+
+capId = getApplication(altID);
+var tblOwner = loadASITable("OWNERS",capId);
+
+for (row in tblOwner){
+	if(tblODefic[row]["First Name"]==firstName && tblODefic[row]["Last Name"]==lastName && tblODefic[row]["Email Address"]==eMail){
+		var capIDModel = aa.cap.getCapIDModel(capId.getID1(), capId.getID2(), capId.getID3()).getOutput();
+		deletedAppSpecificTableInfors("PERMIT INFO", capIDModel, row);
+	}
+}
+
+}
