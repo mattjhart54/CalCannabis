ef9e3b2f96d3f9c02d8748f1f54dcbbb2f1b13b3
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2023-04-19 13:36:16 -0700

    Update ASA;LICENSES!CULTIVATOR!CONVERSION REQUEST!~.js
    MJH: Added code to update work desc with Legal Business Name and short notes with Premise County

--- SCRIPTS/EVENT/ASA;LICENSES!CULTIVATOR!CONVERSION REQUEST!~.js
+++ SCRIPTS/EVENT/ASA;LICENSES!CULTIVATOR!CONVERSION REQUEST!~.js
@@ -34,6 +34,8 @@ try {
 			editAppSpecific("Submitted License Type", AInfo["Proposed License Type"])
 			editAppSpecific("License Expiration Date", vCurrExpDate);
 			editAppSpecific("Legal Business Name", PInfo["Legal Business Name"]);
+			updateWorkDesc(PInfo["Legal Business Name"]);
+			updateShortNotes(PInfo["Premise County"]);
 			editAppSpecific("License Issued Type", PInfo["License Issued Type"]);
 			editAppSpecific("Premise Address",PInfo["Premise Address"]);
 			editAppSpecific("Premise City",PInfo["Premise City"]);
