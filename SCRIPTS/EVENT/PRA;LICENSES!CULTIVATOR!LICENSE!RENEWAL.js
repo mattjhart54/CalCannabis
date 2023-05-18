4492f2e11d23d2ceb2eab58c55dd142616760d70
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2023-05-18 09:51:13 -0700

    Update PRA;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js

--- SCRIPTS/EVENT/PRA;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
+++ SCRIPTS/EVENT/PRA;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
@@ -69,6 +69,7 @@ try{
 						}else {
 							if (vCapStatus != "Inactive"){
 								updateAppStatus("Active","License Renewed",licId);
+							}
 						}
 		// Update the Cultivation Type on the license record
 						if(AInfo["Designation Change"] == "Yes") {
