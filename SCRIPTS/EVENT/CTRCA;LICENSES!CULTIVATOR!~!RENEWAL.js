431c37d0dce45eeb41087dd30161181619c569b3
jshear28 <57968271+jshear28@users.noreply.github.com>
2023-08-14 10:29:20 -0400

    7603

--- SCRIPTS/EVENT/CTRCA;LICENSES!CULTIVATOR!~!RENEWAL.js
+++ SCRIPTS/EVENT/CTRCA;LICENSES!CULTIVATOR!~!RENEWAL.js
@@ -444,6 +444,28 @@ try{
 			logDebug("Error adding record to set " + srName + ". Error: " + setAddResult.getErrorMessage());
 		}
 	}
+	
+	//remove conditions after documents are uploaded
+	try{
+		var cType = "Renewal Required Documents";
+		var capCondResult = aa.capCondition.getCapConditions(capId,cType);
+		if (!capCondResult.getSuccess()){
+			logDebug("**WARNING: error getting cap conditions : " + capCondResult.getErrorMessage()) ; 
+		}else{
+			var ccs = capCondResult.getOutput();
+			for (pc1 in ccs){
+				var rmCapCondResult = aa.capCondition.deleteCapCondition(capId,ccs[pc1].getConditionNumber()); 
+				if (rmCapCondResult.getSuccess())
+					logDebug("Successfully removed condition to CAP : " + capId + "  (" + cType + ") ");
+				else
+					logDebug( "**ERROR: removing condition  (" + cType + "): " + rmCapCondResult.getErrorMessage());
+			}
+		}
+	} catch(err){
+		logDebug("An error has occurred in CTRCA:LICENSES/CULTIVATOR/*/RENEWAL: Remove Conditions: " + err.message);
+		logDebug(err.stack);
+		aa.sendMail(sysFromEmail, debugEmail, "", "An error has occurred in CTRCA:LICENSES/CULTIVATOR/*/RENEWAL: Remove Conditions: "+ startDate, capId + br + err.message+ br+ err.stack + br + currEnv);
+	}
 
 } catch(err){
 	logDebug("An error has occurred in CTRCA:LICENSES/CULTIVATOR/*/RENEWAL: Submission: " + err.message);
