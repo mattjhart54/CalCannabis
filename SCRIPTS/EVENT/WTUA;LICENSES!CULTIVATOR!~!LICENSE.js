f3a4a7697d70ffe54ec886af962ad98a0d4dca83
jshear28 <57968271+jshear28@users.noreply.github.com>
2023-10-13 13:55:17 -0400

    Update WTUA;LICENSES!CULTIVATOR!~!LICENSE.js

--- SCRIPTS/EVENT/WTUA;LICENSES!CULTIVATOR!~!LICENSE.js
+++ SCRIPTS/EVENT/WTUA;LICENSES!CULTIVATOR!~!LICENSE.js
@@ -1,8 +1,15 @@
 //User Story 7153 - Send Inactive Record to CATUPDATE
 try{
-	if(matches(taskStatus("License Status"),"Inactive","Active")){
+	if(matches(taskStatus("License Status"),"Inactive","Active","Limited Operations")){
 		var capId = aa.cap.getCapID(capIDString).getOutput();
 		addToCat(capId);
+	}
+	
+	//User Story 7703 - Satisfy 'Suspension Lift Notice' Condition when workflow status is 'Limited Operations'
+	if(wfTask == "License Status" && wfStatus == "Limited Operations") {
+		if(appHasCondition("Application Condition", "Applied", "Suspension Lift Notice", null)) {
+			editCapConditionStatus("Application Condition", "Suspension Lift Notice", "Condition Met", "Not Applied");
+		}
 	}
 }catch(err){
 	logDebug("An error has occurred in WTUA:LICENSES/CULTIVATOR/*/LICENSE: Adding to CAT Set: " + err.message);
