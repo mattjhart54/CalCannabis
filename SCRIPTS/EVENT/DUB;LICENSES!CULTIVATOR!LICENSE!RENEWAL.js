038f2d2bc8fa378edf1f7f63aaae10152a96e451
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2023-10-19 16:52:36 -0700

    Update DUB;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js

--- SCRIPTS/EVENT/DUB;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
+++ SCRIPTS/EVENT/DUB;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
@@ -1,29 +1,34 @@
 try{
 	if(documentUploadedFrom == "ACA"){
+		AInfo= [];
 		loadAppSpecific4ACA(AInfo);
-		docsMissing = true;
+		docMissing = true;
 		docAttached = false;
 		capIdString = capId.getID1() + "-" + capId.getID2() + "-" + capId.getID3();
 		submittedDocList = aa.document.getDocumentListByEntity(capIdString,"TMP_CAP").getOutput().toArray();
-		for (var i in submittedDocList ){
-			logDebug("Document " + submittedDocList[i].getDocCategory());
-			if(AInfo["License Change"] == "Yes") {
-				if(submittedDocList[i].getDocCategory() == "Cultivation Plan - Detailed Premises Diagram")
-					docsMissing = false;
-			}else {
-				if(submittedDocList[i].getDocCategory() == "Cultivation Plan - Detailed Premises Diagram")
-					docAttached = true;
+		logDebug("Document Cnt " + submittedDocList.length + " License Change " + AInfo["License Change"]);
+		if(submittedDocList.length > 0) {
+			for (var i in submittedDocList ){
+				logDebug("Document " + submittedDocList[i].getDocCategory());
+				licChange = getAppSpecific("License Change")   
+				if(AInfo["License Change"] == "Yes") {
+					if(submittedDocList[i].getDocCategory() == "Cultivation Plan - Detailed Premises Diagram")
+						docsMissing = false;
+				}else {
+					if(submittedDocList[i].getDocCategory() == "Cultivation Plan - Detailed Premises Diagram")
+						docAttached = true;
+				}
 			}
 		}
-		if(docMissing = false) {
+				
+		if(docMissing == true) {
 			showMessage = true;
 			comment("A cultivation license size change has been requested and a revised premises diagram is required. Please upload the premises diagram.");
 		}
-		if(docAttached = true) {
+		if(docAttached == true) {
 			showMessage = true;
 			comment("A cultivation license size change was not selected and a premises diagram is not required. For further questions, please contact the Department of Cannabis Control by calling 1-844-61-CA-DCC (1-844-612-2322) or by sending an email to licensing@cannabis.ca.gov.");
 		}
-	}
 }catch(err){
 	logDebug("An error has occurred in DUB:LICENSES/CULTIVATOR/LICENSE/RENEWAL: No Documents: " + err.message);
 	logDebug(err.stack);
