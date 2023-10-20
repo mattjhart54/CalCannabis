f81f090ddb294989e7b9c61d5c7e77bfaaf8f085
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2023-10-19 22:45:10 -0700

    Update DUB;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js

--- SCRIPTS/EVENT/DUB;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
+++ SCRIPTS/EVENT/DUB;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
@@ -29,6 +29,7 @@ try{
 			showMessage = true;
 			comment("A cultivation license size change was not selected and a premises diagram is not required. For further questions, please contact the Department of Cannabis Control by calling 1-844-61-CA-DCC (1-844-612-2322) or by sending an email to licensing@cannabis.ca.gov.");
 		}
+	}
 }catch(err){
 	logDebug("An error has occurred in DUB:LICENSES/CULTIVATOR/LICENSE/RENEWAL: No Documents: " + err.message);
 	logDebug(err.stack);
