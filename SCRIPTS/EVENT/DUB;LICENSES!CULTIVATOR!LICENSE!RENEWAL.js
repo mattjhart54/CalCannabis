dcc51c8fde681fcdfddb00aee23f89f25e97d18d
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2023-10-24 12:04:34 -0700

    Update DUB;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js

--- SCRIPTS/EVENT/DUB;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
+++ SCRIPTS/EVENT/DUB;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
@@ -1,18 +1,13 @@
 try{
-//	if(documentUploadedFrom == "ACA"){
+	if(documentUploadedFrom == "ACA"){
 		AInfo= [];
 		loadAppSpecific(AInfo);
 		docAttached = false;
-		capIdString = capId.getID1() + "-" + capId.getID2() + "-" + capId.getID3();
-		submittedDocList = aa.document.getDocumentListByEntity(capIdString,"TMP_CAP").getOutput().toArray();
-		logDebug("Document Cnt " + submittedDocList.length + " License Change " + AInfo["License Change"]);
-		if(submittedDocList.length > 0) {
-			for (var i in submittedDocList ){
-				logDebug("Document " + submittedDocList[i].getDocCategory());
-				if(AInfo["License Change"] != "Yes") {
-					if(submittedDocList[i].getDocCategory() == "Cultivation Plan - Detailed Premises Diagram")
-						docAttached = true;
-				}
+		for(var index = 0; index < documentModelArray.size(); index++) {
+			logDebug("Document " + String(documentModelArray.get(index).getDocCategory()));
+			if(AInfo["License Change"] != "Yes") {
+				if (String(documentModelArray.get(index).getDocCategory()) == "Cultivation Plan - Detailed Premises Diagram")
+					docAttached = true;
 			}
 		}
 		if(docAttached == true) {
@@ -20,10 +15,10 @@ try{
 			showMessage = true;
 			comment("A cultivation license size change was not selected and a premises diagram is not required. For further questions, please contact the Department of Cannabis Control by calling 1-844-61-CA-DCC (1-844-612-2322) or by sending an email to licensing@cannabis.ca.gov.");
 		}
-	cancel = true;
-	showMessage = true;
-	comment("License Change " + AInfo["License Change"] + " Number of Docs " + submittedDocList.length);
-//	}
+		cancel = true;
+		showMessage = true;
+		comment("License Change " + AInfo["License Change"] + " Number of Docs " + documentModelArray.size());
+	}
 }catch(err){
 	logDebug("An error has occurred in DUB:LICENSES/CULTIVATOR/LICENSE/RENEWAL: No Documents: " + err.message);
 	logDebug(err.stack);
