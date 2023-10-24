6a5986ce16b5186cde603345d4492e65713be7f6
jshear28 <57968271+jshear28@users.noreply.github.com>
2023-10-23 20:12:31 -0400

    Update PRB;LICENSES!CULTIVATOR!~!APPLICATION.js

--- SCRIPTS/EVENT/PRB;LICENSES!CULTIVATOR!~!APPLICATION.js
+++ SCRIPTS/EVENT/PRB;LICENSES!CULTIVATOR!~!APPLICATION.js
@@ -91,14 +91,14 @@ try {
 		var renYear = expDateObj.getFullYear();
 	
 		var transferPermitID = new asiTableValObj("LICENSE RENEWAL HISTORY", licCapId, "N");
-		toRow["Renewal Year"] = String(renYear);
-		toRow["License Expiration"] = String(expDate);
+		toRow["Renewal Year"] = "" + String(renYear);
+		toRow["License Expiration"] = "" + String(expDate);
 		toRow["License Status"] = "Active";
 		toRow["Limited Operation"] = "";
-		toRow["License Type"] = AInfo['License Type']; 
-		toRow["Canopy Square Feet"] = AInfo['Canopy SF'];
-		toRow["Canopy Plant Count"] = AInfo['Canopy Plant Count'];
-		toRow["Canopy Square Footage Limit"] = AInfo['Canopy SF Limit'];
+		toRow["License Type"] = "" + AInfo['License Type']; 
+		toRow["Canopy Square Feet"] = "" + AInfo['Canopy SF'];
+		toRow["Canopy Plant Count"] = "" + AInfo['Canopy Plant Count'];
+		toRow["Canopy Square Footage Limit"] = "" + AInfo['Canopy SF Limit'];
 		
 		LICENSERENEWALHISTORY.push(toRow);
 		addASITable("LICENSE RENEWAL HISTORY", LICENSERENEWALHISTORY, licCapId);
