f79dac2caa498bee3340eb6285fad28477289d9a
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2023-10-25 08:07:56 -0700

    Update DUB;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js

--- SCRIPTS/EVENT/DUB;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
+++ SCRIPTS/EVENT/DUB;LICENSES!CULTIVATOR!LICENSE!RENEWAL.js
@@ -1,10 +1,9 @@
 try{
-//	if(documentUploadedFrom == "ACA"){
+	if(documentUploadedFrom == "ACA"){
 		AInfo= [];
 		loadAppSpecific(AInfo);
 		docAttached = false;
 		for(var index = 0; index < documentModelArray.size(); index++) {
-			logDebug("Document " + String(documentModelArray.get(index).getDocCategory()));
 			if(AInfo["License Change"] != "Yes") {
 				if (String(documentModelArray.get(index).getDocCategory()) == "Cultivation Plan - Detailed Premises Diagram")
 					docAttached = true;
@@ -16,10 +15,7 @@ try{
 			comment("A cultivation license size change was not selected and a premises diagram is not required. For further questions, please contact the Department of Cannabis Control by calling 1-844-61-CA-DCC (1-844-612-2322) or by sending an email to licensing@cannabis.ca.gov.");
 		}
 		
-//	}
-	cancel = true;
-	showMessage = true;
-	comment("License Change " + AInfo["License Change"] + " # of Docs " + documentModelArray.size());
+	}
 }catch(err){
 	logDebug("An error has occurred in DUB:LICENSES/CULTIVATOR/LICENSE/RENEWAL: No Documents: " + err.message);
 	logDebug(err.stack);
