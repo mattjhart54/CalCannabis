abe89323aac15f32942c2b11a39d201ce512efb2
jshear28 <57968271+jshear28@users.noreply.github.com>
2023-10-27 08:06:38 -0400

    Update renewalProcess.js

--- INCLUDES_CUSTOM/renewalProcess.js
+++ INCLUDES_CUSTOM/renewalProcess.js
@@ -66,7 +66,7 @@ function renewalProcess(rAltId, event, fees){
 			savedCapStatus = getAppSpecific("Saved License Status",licId);
 			limitedOp = AInfo['Limited Operation'] == "Yes";
 			if(limitedOp){
-				editAppSpecific("Limited Operation","Yes",licId);
+				editAppSpecific("Limited Operations","Yes",licId);
 				if(!appHasCondition("Application Condition","Applied","Suspension Lift Notice","Notice",licId)){
 					addStdCondition("Application Condition","Suspension Lift Notice","Condition Met","Not Applied",licId);
 				}
