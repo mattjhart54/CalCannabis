e4a3b8c3cd0c258fb553bdab2741df034972382b
jshear28 <57968271+jshear28@users.noreply.github.com>
2023-11-13 07:52:17 -0500

    Update renewalProcessLC.js

--- INCLUDES_CUSTOM/renewalProcessLC.js
+++ INCLUDES_CUSTOM/renewalProcessLC.js
@@ -159,12 +159,13 @@ function renewalProcessLC() {
 							var histRow = new Array();
 				
 							var renYear = vNewExpDate.getFullYear();
+							var newExpStatus = aa.cap.getCap(licId).getOutput().getCapStatus();
 							var expDateForamatted = dateFormatted(vNewExpDate.getMonth()+1, vNewExpDate.getDate(), vNewExpDate.getFullYear(), "MM/DD/YYYY");
 				
 							var transferPermitID = new asiTableValObj("LICENSE RENEWAL HISTORY", parentCapId, "N");
 							histRow["Renewal Year"] = "" + String(renYear);
 							histRow["License Expiration"] = "" + String(expDateForamatted);
-							histRow["License Status"] = "" + vCapStatus;
+							histRow["License Status"] = "" + newExpStatus;
 							histRow["Limited Operation"] = "" + AInfo['Limited Operation'];
 							histRow["License Type"] = "" + String(licType); 
 							histRow["Canopy Square Feet"] = "" + (getAppSpecific("Canopy SF",parentCapId) || "");
