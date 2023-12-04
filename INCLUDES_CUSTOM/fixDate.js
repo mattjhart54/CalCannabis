7d1f8a77f268f1b23177b55d3d2bd1cf30c65d07
evontrapp <36080657+evontrapp@users.noreply.github.com>
2023-12-04 12:06:12 -0800

    US 7740

--- INCLUDES_CUSTOM/fixDate.js
+++ INCLUDES_CUSTOM/fixDate.js
@@ -1,6 +1,6 @@
 function fixDate(dateObj) {
 	// date object with getClass assumes that this is an Accela Date object
-	if (thisDate.getClass) {
+	if (dateObj.getClass) {
 		return new Date(dateObj.getEpochMilliseconds());
 	} else {
 		logDebug("Date is not an Accela Date object");
