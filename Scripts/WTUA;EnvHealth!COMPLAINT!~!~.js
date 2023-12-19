09360b0598733a44997224a2ea0f4716a4d5be48
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2023-12-19 13:23:41 -0800

    Delete Scripts directory

--- Scripts/WTUA;EnvHealth!COMPLAINT!~!~.js
+++
@@ -1,6 +0,0 @@
-facilityId = AInfo["Facility ID"]; 
-if ( wfTask == "Complaint Closed" && wfStatus == "Closed" && AInfo["Existing Facility"]=='Yes' )
-{
-existingFacility = aa.cap.getCapID(facilityId).getOutput(); 
-aa.cap.createAppHierarchy(existingFacility, capId);
-}
--- Scripts/readMe.js
+++
@@ -1,3 +0,0 @@
-/*
-	interface files go here
-*/
