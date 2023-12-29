80411ec615c9f7a45836074d92da9b8a20cb0180
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2023-12-28 17:54:43 -0800

    Delete Scripts directory

--- Scripts/CONTACTEDITAFTER.js
+++
@@ -1,1 +0,0 @@
-aa.env.setValue("ScriptReturnCode","0");aa.print("Edit Daily Contact Start:");var contactModel = aa.env.getValue("Contact");aa.people.editContactByCapContact(contactModel);aa.print("Edit Daily Contact End:");aa.env.setValue("ScriptReturnMessage", "Edit Daily Contact successful");
--- Scripts/PUBLISHCOMMENTSBEFORE.js
+++
@@ -1,1 +0,0 @@
-var DocumentID= aa.env.getValue("DocumentID");var DocumentModel =aa.env.getValue("DocumentModel");var associationModel= aa.env.getValue("DocumentEntityAssociationModel");aa.env.setValue("ScriptReturnMessage", "Prepare to publish document comments..")aa.env.setValue("ScriptReturnCode","0");
