7ea16d46b45304e2f4a9ac678f6a5f2251e743c3
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2024-02-21 16:48:10 -0800

    Update ACA_ONLOAD_CLC_FEES.js

--- SCRIPTS/PAGEFLOW/ACA_ONLOAD_CLC_FEES.js
+++ SCRIPTS/PAGEFLOW/ACA_ONLOAD_CLC_FEES.js
@@ -313,3 +313,15 @@ try{
 	logDebug("An error has occurred in FeeDef: " + err.message);
 	logDebug(err.stack);
 }}
+function maskTheMoneyNumber(val) {
+   //set regex pattern for mask
+    var pattern;
+    pattern = /\B(?=(\d{3})+(?!\d))/g; //add commas to number
+	if (val == null || val == '') {
+        return '';
+    }
+	var strVal = new String(val.toString());
+    var cleanVal = strVal.replace(/,/g, ""); //remove commas before masking
+    var maskedVal = cleanVal.replace(pattern, ",")
+	return maskedVal;
+}
