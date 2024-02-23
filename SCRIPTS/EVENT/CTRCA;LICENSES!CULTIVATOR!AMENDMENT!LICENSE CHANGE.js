daceedea050d08a1f714ed5af061aabb9b3a4314
jshear28 <57968271+jshear28@users.noreply.github.com>
2024-02-23 12:31:08 -0500

    Update CTRCA;LICENSES!CULTIVATOR!AMENDMENT!LICENSE CHANGE.js

--- SCRIPTS/EVENT/CTRCA;LICENSES!CULTIVATOR!AMENDMENT!LICENSE CHANGE.js
+++ SCRIPTS/EVENT/CTRCA;LICENSES!CULTIVATOR!AMENDMENT!LICENSE CHANGE.js
@@ -78,6 +78,7 @@ try{
 	var expDateForamatted = dateFormatted(vNewExpDate.getMonth()+1, vNewExpDate.getDate(), vNewExpDate.getFullYear(), "MM/DD/YYYY");
 
 	histRow["Renewal Year"] = "" + String(renYear);
+	histRow["Record number of source"] = "" + newAltId;
 	histRow["License Expiration"] = "" + String(expDateForamatted);
 	histRow["License Status"] = "" + newExpStatus;
 	histRow["Limited Operation"] = "" + AInfo['Limited Operation'];
