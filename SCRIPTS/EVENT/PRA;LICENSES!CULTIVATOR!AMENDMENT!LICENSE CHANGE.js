7b9a70a929e60dd08fba3593704ffff5e38ff7da
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2024-03-02 09:55:23 -0800

    Update PRA;LICENSES!CULTIVATOR!AMENDMENT!LICENSE CHANGE.js

--- SCRIPTS/EVENT/PRA;LICENSES!CULTIVATOR!AMENDMENT!LICENSE CHANGE.js
+++ SCRIPTS/EVENT/PRA;LICENSES!CULTIVATOR!AMENDMENT!LICENSE CHANGE.js
@@ -82,7 +82,7 @@ try{
 		envParameters.put("licCap",parentCapId.getCustomID());
 		envParameters.put("reportName","Official License Certificate");
 		envParameters.put("approvalLetter", "");
-		envParameters.put("emailTemplate", "LCA_CCL_FEE_PAID");
+		envParameters.put("emailTemplate", "LCA_CLC_FEE_PAID");
 		envParameters.put("reason", "");
 		envParameters.put("currentUserID",currentUserID);
 		envParameters.put("contType","Designated Responsible Party");
