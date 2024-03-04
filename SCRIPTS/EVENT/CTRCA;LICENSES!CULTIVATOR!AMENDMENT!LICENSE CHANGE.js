3e7c4fef04f580b3cc94584649e97a58fff07d87
sumpatel640 <160267547+sumpatel640@users.noreply.github.com>
2024-03-04 12:51:07 -0500

    Update CTRCA;LICENSES!CULTIVATOR!AMENDMENT!LICENSE CHANGE.js

--- SCRIPTS/EVENT/CTRCA;LICENSES!CULTIVATOR!AMENDMENT!LICENSE CHANGE.js
+++ SCRIPTS/EVENT/CTRCA;LICENSES!CULTIVATOR!AMENDMENT!LICENSE CHANGE.js
@@ -176,14 +176,14 @@ try{
 				}
 			 }
 			feeNotification = "LCA_CLC_NO_FEE";
-			refundAmount = AInfo["Net Due/Refund"];
+			refundAmount = "$" + AInfo["Net Due/Refund"];
 		}
 		envParameters.put("reportName","Official License Certificate");
 		envParameters.put("appCap",newAltId);
 		envParameters.put("licCap",pAltId);
 		envParameters.put("licType",licType);
 		envParameters.put("emailTemplate", feeNotification);
-		envParameters.put("refundAmount", refundAmount);
+		envParameters.put("refundAmount", Math.abs(refundAmount));
 		envParameters.put("currentUserID",currentUserID);
 		envParameters.put("contType","Designated Responsible Party");
 		envParameters.put("fromEmail",sysFromEmail);
