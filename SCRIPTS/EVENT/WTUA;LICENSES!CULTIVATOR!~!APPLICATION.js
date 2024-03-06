9900ef26273281a07692b3fcae2e9875316225b5
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2024-03-06 13:01:08 -0800

    Update WTUA;LICENSES!CULTIVATOR!~!APPLICATION.js

--- SCRIPTS/EVENT/WTUA;LICENSES!CULTIVATOR!~!APPLICATION.js
+++ SCRIPTS/EVENT/WTUA;LICENSES!CULTIVATOR!~!APPLICATION.js
@@ -523,7 +523,7 @@ try{
 				lType = lookup("LIC_CC_LICENSE_TYPE", licType);
 				if(!matches(lType,"", null, undefined)){
 					licTbl = lType.split(";");
-					var base = parseInt(licTbl[3]);
+					var base = parseInt(licTbl[3] - 1);
 					feeDesc = licType + " - Per 2,000 sq ft over " + maskTheMoneyNumber(base);
 					logDebug("feeDesc " + feeDesc);
 					thisFee = getFeeDefByDesc("LIC_CC_CONVERSION", feeDesc);
