01781d6927f2f149d234de346a502f27e2cfba34
allysonscottDCC <allyson.scott@cannabis.ca.gov>
2024-04-24 08:40:44 -0700

    Update ASIUA;LICENSES!CULTIVATOR!AMENDMENT!OWNER CHANGE.js

--- SCRIPTS/EVENT/ASIUA;LICENSES!CULTIVATOR!AMENDMENT!OWNER CHANGE.js
+++ SCRIPTS/EVENT/ASIUA;LICENSES!CULTIVATOR!AMENDMENT!OWNER CHANGE.js
@@ -12,7 +12,7 @@ try{
 			var sysDate = aa.date.getCurrentDate();
 			var sysDateMMDDYYYY = dateFormatted(sysDate.getMonth(), sysDate.getDayOfMonth(), sysDate.getYear(), "MM/DD/YYYY");
 			var acaSite = getACABaseUrl();   
-			addParameter(eParams, "$$acaURL$$", acaSite);
+			addParameter(emailParameters, "$$acaURL$$", acaSite);
 			
 			addParameter(emailParameters, "$$AltID$$", capId.getCustomID());
 			addParameter(emailParameters, "$$ParentAltID$$", capId.getCustomID());
