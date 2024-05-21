d62f687a69c73efa7db8f027c6f4b7aa8c5c679d
mattjhart54 <31482365+mattjhart54@users.noreply.github.com>
2024-05-21 14:55:30 -0700

    Update ASB;LICENSES!CULTIVATOR!CONVERSION REQUEST!~.js
    MJH Story 7953 - Removed check for public user so script will run for conversion applications submitted in both ACA and AV

--- SCRIPTS/EVENT/ASB;LICENSES!CULTIVATOR!CONVERSION REQUEST!~.js
+++ SCRIPTS/EVENT/ASB;LICENSES!CULTIVATOR!CONVERSION REQUEST!~.js
@@ -1,16 +1,16 @@
 try {
-	if(publicUser){
-		if(typeof(LICENSERECORDSFORCONVERSION) == "object"){
-			if(LICENSERECORDSFORCONVERSION.length > 0){
-				var addedRow = true;
-			}
+// MJH Stpry 7953 - Removed check for public user so script will run for convesrion applications submitted in both ACA and AV
+	
+	if(typeof(LICENSERECORDSFORCONVERSION) == "object"){
+		if(LICENSERECORDSFORCONVERSION.length > 0){
+			var addedRow = true;
 		}
+	}
 			
-		if(!addedRow){
-			cancel=true; 
-			showMessage=true; 
-			comment("Conversion record can not be submitted without a secondary license record");
-		}
+	if(!addedRow){
+		cancel=true; 
+		showMessage=true; 
+		comment("Conversion record can not be submitted without a secondary license record");
 	}		
 }catch(err){
 	logDebug("An error has occurred in ASB:LICENSES/CULTIVATOR/CONVERSION REQUEST/*: " + err.message);
